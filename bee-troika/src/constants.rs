233fea6d61177933506144ca2c5eaf177a4ef3e2
Alex6323 <Al3k5@protonmail.com>
2019-07-06 23:26:32 +0200

    Revert "Added bee-trinary crate"
    This reverts commit 6f30d6bc6c4c45b586e22c9bfbff873a8fc2d1ff.

--- bee-troika/src/constants.rs
+++ bee-troika/src/constants.rs
@@ -6,3 +6,9 @@ pub const COLUMNS: usize = 9;
 pub const SLICES: usize = 27;
 
 pub const SLICE_SIZE: usize = COLUMNS * ROWS;
+
+#[cfg(feature = "troika")]
+pub const STATE_SIZE: usize = COLUMNS * ROWS * SLICES;
+
+#[cfg(feature = "troika")]
+pub const NUM_SBOXES: usize = COLUMNS * ROWS * SLICES / 3;
--- bee/src/main.rs
+++ bee/src/main.rs
@@ -1,5 +1,3 @@
-//! Entry point for the Bee node software.
-
 #![deny(warnings, bad_style, unsafe_code)]
 
 mod constants;
