48ff982c4f6faefed34b65ab362fc3a62ba2159a
Christian Poveda <christian.poveda@iota.org>
2020-08-18 15:18:33 -0500

    address style review

--- bee-crypto/benches/batched_hash.rs
+++ bee-crypto/benches/batched_hash.rs
@@ -9,9 +9,8 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
-use bee_ternary::{T1B1Buf, TryteBuf};
-
 use bee_crypto::ternary::sponge::{BatchHasher, CurlPRounds, BATCH_SIZE};
+use bee_ternary::{T1B1Buf, TryteBuf};
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
--- bee-crypto/src/ternary/sponge/batched_curl/mod.rs
+++ bee-crypto/src/ternary/sponge/batched_curl/mod.rs
@@ -107,7 +107,7 @@ impl BatchHasher {
 
         for i in 0..length {
             let low = (self.bct_hashes.lo()[i] >> index) & 1;
-            // This is safe as the previous acces to `self.lo` took care of checking the index.
+            // This is safe as the previous access to `self.lo` took care of checking the index.
             let hi = (unsafe { *self.bct_hashes.hi().get_unchecked(i) } >> index) & 1;
 
             let trit = match (low, hi) {
