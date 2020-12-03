0e7b5f2e34a2a7ee5a899d29fa14b6a55709e832
/alex/ <Al3k5@protonmail.com>
2020-12-03 18:22:16 +0100

    Disabling REST API

--- bee-node/src/node.rs
+++ bee-node/src/node.rs
@@ -386,8 +386,9 @@ impl<B: Backend> NodeBuilder<BeeNode<B>> for BeeNodeBuilder<B> {
 
         this = this.with_worker::<VersionCheckerWorker>();
 
-        info!("Initializing REST API...");
-        this = bee_rest_api::init::<BeeNode<B>>(RestApiConfig::build().finish(), config.network_id.clone(), this).await; // TODO: Read config from file
+        // info!("Initializing REST API...");
+        // this = bee_rest_api::init::<BeeNode<B>>(RestApiConfig::build().finish(), config.network_id.clone(),
+        // this).await; // TODO: Read config from file
 
         let mut node = BeeNode {
             workers: Map::new(),
