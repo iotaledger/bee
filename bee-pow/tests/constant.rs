e0bc13b9d1de595e89a3a1e67c60ffe4e26c52b0
Thibault Martinez <thibault@iota.org>
2020-12-10 16:13:36 +0100

    Constant nonce provider test

--- bee-pow/tests/constant.rs
+++ bee-pow/tests/constant.rs
@@ -2,17 +2,19 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use bee_pow::{
-    providers::{MinerBuilder, Provider, ProviderBuilder},
+    providers::{ConstantBuilder, MinerBuilder, Provider, ProviderBuilder},
     score::compute_pow_score,
 };
 use bee_test::rand::bytes::random_bytes;
 
 #[test]
-fn miner() {
+fn constant() {
     let miner = MinerBuilder::new().with_num_workers(4).finish();
     let mut bytes = random_bytes(256);
-
-    let nonce = miner.nonce(&bytes[0..248], 4000f64).unwrap();
+    let constant = ConstantBuilder::new()
+        .with_value(miner.nonce(&bytes[0..248], 4000f64).unwrap())
+        .finish();
+    let nonce = constant.nonce(&bytes[0..248], 4000f64).unwrap();
     bytes[248..].copy_from_slice(&nonce.to_le_bytes());
 
     assert!(compute_pow_score(&bytes) >= 4000f64);
