2dc480fba24bbcee1dd83580e2431931cc415048
Thibault Martinez <thibault@iota.org>
2020-12-17 18:14:05 +0100

    Fix milestone storage tests

--- bee-storage/bee-storage-rocksdb/tests/milestone_index_to_milestone.rs
+++ bee-storage/bee-storage-rocksdb/tests/milestone_index_to_milestone.rs
@@ -7,7 +7,7 @@ use bee_storage::{
     storage::Backend,
 };
 use bee_storage_rocksdb::{config::RocksDBConfigBuilder, storage::Storage};
-use bee_test::rand::milestone::random_milestone;
+use bee_test::rand::milestone::{random_milestone, random_milestone_index};
 
 use futures::stream::StreamExt;
 
@@ -22,8 +22,8 @@ async fn access() {
     let config = RocksDBConfigBuilder::default().with_path(DB_DIRECTORY.into()).finish();
     let storage = Storage::start(config).await.unwrap();
 
+    let index = random_milestone_index();
     let milestone = random_milestone();
-    let index = milestone.index();
 
     assert!(!Exist::<MilestoneIndex, Milestone>::exist(&storage, &index)
         .await
@@ -63,8 +63,7 @@ async fn access() {
     let mut batch = Storage::batch_begin();
 
     for _ in 0usize..10usize {
-        let milestone = random_milestone();
-        let index = milestone.index();
+        let (index, milestone) = (random_milestone_index(), random_milestone());
         Insert::<MilestoneIndex, Milestone>::insert(&storage, &index, &milestone)
             .await
             .unwrap();
@@ -74,8 +73,7 @@ async fn access() {
     let mut milestones = HashMap::new();
 
     for _ in 0usize..10usize {
-        let milestone = random_milestone();
-        let index = milestone.index();
+        let (index, milestone) = (random_milestone_index(), random_milestone());
         Batch::<MilestoneIndex, Milestone>::batch_insert(&storage, &mut batch, &index, &milestone).unwrap();
         milestones.insert(index, milestone);
     }
