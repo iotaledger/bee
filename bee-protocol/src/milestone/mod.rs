629d020dd46767321206309b1e4ff627d3e53b24
Thibault Martinez <thibault@iota.org>
2020-12-17 02:33:13 +0100

    Impl Packable for Milestone

--- bee-protocol/src/milestone/mod.rs
+++ bee-protocol/src/milestone/mod.rs
@@ -7,7 +7,18 @@ pub(crate) mod key_range;
 
 pub use index::MilestoneIndex;
 
+use bee_common::packable::{Packable, Read, Write};
 use bee_message::MessageId;
+
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("I/O error {0}")]
+    IO(#[from] std::io::Error),
+    #[error("MessageId error {0}")]
+    MessageId(<MessageId as Packable>::Error),
+}
 
 #[derive(Clone, Debug)]
 pub struct Milestone {
@@ -26,5 +37,28 @@ impl Milestone {
 
     pub fn message_id(&self) -> &MessageId {
         &self.message_id
+    }
+}
+
+impl Packable for Milestone {
+    type Error = Error;
+
+    fn packed_len(&self) -> usize {
+        self.index.packed_len() + self.message_id.packed_len()
+    }
+
+    fn pack<W: Write>(&self, writer: &mut W) -> Result<(), Self::Error> {
+        self.index.pack(writer)?;
+        self.message_id.pack(writer).map_err(Error::MessageId)
+    }
+
+    fn unpack<R: Read + ?Sized>(reader: &mut R) -> Result<Self, Self::Error>
+    where
+        Self: Sized,
+    {
+        Ok(Self {
+            index: MilestoneIndex::unpack(reader)?,
+            message_id: MessageId::unpack(reader).map_err(Error::MessageId)?,
+        })
     }
 }
