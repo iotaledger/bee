da1481d15d0c11095270e734c9e0a6f1076dc045
Thibault Martinez <thibault@iota.org>
2020-12-18 17:56:22 +0100

    Index to MessageId storage tests

--- bee-protocol/src/worker/peer/packet_handler.rs
+++ bee-protocol/src/worker/peer/packet_handler.rs
@@ -200,11 +200,7 @@ mod tests {
         // Create a new packet handler
         let (sender_shutdown, receiver_shutdown) = oneshot::channel::<()>();
         let (sender, receiver) = mpsc::unbounded_channel::<Vec<u8>>();
-        let mut msg_handler = PacketHandler::new(
-            receiver,
-            receiver_shutdown.fuse(),
-            "127.0.0.1:8080".parse().unwrap(),
-        );
+        let mut msg_handler = PacketHandler::new(receiver, receiver_shutdown.fuse(), "127.0.0.1:8080".parse().unwrap());
         // Create the task that does the checks of the test.
         let handle = spawn(async move {
             // The packets are expected to be filled with zeroes except for the packet length
@@ -294,11 +290,7 @@ mod tests {
         let (sender_shutdown, receiver_shutdown) = oneshot::channel::<()>();
         let (sender, receiver) = mpsc::unbounded_channel::<Vec<u8>>();
 
-        let mut msg_handler = PacketHandler::new(
-            receiver,
-            receiver_shutdown.fuse(),
-            "127.0.0.1:8080".parse().unwrap(),
-        );
+        let mut msg_handler = PacketHandler::new(receiver, receiver_shutdown.fuse(), "127.0.0.1:8080".parse().unwrap());
 
         let handle = spawn(async move {
             let expected_bytes = vec![0u8; msg_len];
---
+++ bee-storage/bee-storage-rocksdb/tests/index_to_message_id.rs
@@ -0,0 +1,104 @@
+// Copyright 2020 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use bee_message::{payload::indexation::HashedIndex, MessageId};
+use bee_storage::{
+    access::{AsStream, Batch, BatchBuilder, Delete, Exist, Fetch, Insert},
+    storage::Backend,
+};
+use bee_storage_rocksdb::{config::RocksDBConfigBuilder, storage::Storage};
+use bee_test::rand::message::{random_indexation, random_message_id};
+
+use futures::stream::StreamExt;
+
+use std::collections::HashMap;
+
+const DB_DIRECTORY: &str = "./tests/database/index_to_message_id";
+
+#[tokio::test]
+async fn access() {
+    let _ = std::fs::remove_dir_all(DB_DIRECTORY);
+
+    let config = RocksDBConfigBuilder::default().with_path(DB_DIRECTORY.into()).finish();
+    let storage = Storage::start(config).await.unwrap();
+
+    let index = random_indexation().hash();
+    let message_id = random_message_id();
+
+    assert!(
+        !Exist::<(HashedIndex, MessageId), ()>::exist(&storage, &(index, message_id))
+            .await
+            .unwrap()
+    );
+    assert!(Fetch::<HashedIndex, Vec<MessageId>>::fetch(&storage, &index)
+        .await
+        .unwrap()
+        .unwrap()
+        .is_empty());
+
+    Insert::<(HashedIndex, MessageId), ()>::insert(&storage, &(index, message_id), &())
+        .await
+        .unwrap();
+
+    assert!(
+        Exist::<(HashedIndex, MessageId), ()>::exist(&storage, &(index, message_id))
+            .await
+            .unwrap()
+    );
+    assert_eq!(
+        Fetch::<HashedIndex, Vec<MessageId>>::fetch(&storage, &index)
+            .await
+            .unwrap()
+            .unwrap(),
+        vec![message_id]
+    );
+
+    Delete::<(HashedIndex, MessageId), ()>::delete(&storage, &(index, message_id))
+        .await
+        .unwrap();
+
+    assert!(
+        !Exist::<(HashedIndex, MessageId), ()>::exist(&storage, &(index, message_id))
+            .await
+            .unwrap()
+    );
+    assert!(Fetch::<HashedIndex, Vec<MessageId>>::fetch(&storage, &index)
+        .await
+        .unwrap()
+        .unwrap()
+        .is_empty());
+
+    let mut batch = Storage::batch_begin();
+
+    for _ in 0usize..10usize {
+        let (index, message_id) = (random_indexation().hash(), random_message_id());
+        Insert::<(HashedIndex, MessageId), ()>::insert(&storage, &(index, message_id), &())
+            .await
+            .unwrap();
+        Batch::<(HashedIndex, MessageId), ()>::batch_delete(&storage, &mut batch, &(index, message_id)).unwrap();
+    }
+
+    let mut message_ids = HashMap::new();
+
+    for _ in 0usize..10usize {
+        let (index, message_id) = (random_indexation().hash(), random_message_id());
+        Batch::<(HashedIndex, MessageId), ()>::batch_insert(&storage, &mut batch, &(index, message_id), &()).unwrap();
+        message_ids.insert(index, message_id);
+    }
+
+    storage.batch_commit(batch, true).await.unwrap();
+
+    let mut stream = AsStream::<(HashedIndex, MessageId), ()>::stream(&storage)
+        .await
+        .unwrap();
+    let mut count = 0;
+
+    while let Some(((index, message_id), _)) = stream.next().await {
+        assert_eq!(message_ids.get(&index).unwrap(), &message_id);
+        count += 1;
+    }
+
+    assert_eq!(count, message_ids.len());
+
+    let _ = std::fs::remove_dir_all(DB_DIRECTORY);
+}
