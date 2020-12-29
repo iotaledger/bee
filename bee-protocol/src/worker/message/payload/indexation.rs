61a8f04b1ff6dced928bdae66fc2b2fbfb2fb908
Thibault Martinez <thibault@iota.org>
2020-12-30 00:29:34 +0100

    Preparing IndexationPayloadWorker for storage

--- bee-protocol/src/worker/message/payload/indexation.rs
+++ bee-protocol/src/worker/message/payload/indexation.rs
@@ -1,16 +1,18 @@
 // Copyright 2020 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::{tangle::MsTangle, worker::TangleWorker};
+
 use bee_common::shutdown_stream::ShutdownStream;
 use bee_common_pt2::{node::Node, worker::Worker};
-use bee_message::MessageId;
+use bee_message::{payload::Payload, MessageId};
 
 use async_trait::async_trait;
 use futures::stream::StreamExt;
 use log::info;
 use tokio::sync::mpsc;
 
-use std::convert::Infallible;
+use std::{any::TypeId, convert::Infallible};
 
 #[derive(Debug)]
 pub(crate) struct IndexationPayloadWorkerEvent(pub(crate) MessageId);
@@ -27,16 +29,32 @@ where
     type Config = ();
     type Error = Infallible;
 
+    fn dependencies() -> &'static [TypeId] {
+        vec![TypeId::of::<TangleWorker>()].leak()
+    }
+
     async fn start(node: &mut N, _config: Self::Config) -> Result<Self, Self::Error> {
         let (tx, rx) = mpsc::unbounded_channel();
+        let tangle = node.resource::<MsTangle<N::Backend>>();
 
         node.spawn::<Self, _, _>(|shutdown| async move {
             info!("Running.");
 
             let mut receiver = ShutdownStream::new(shutdown, rx);
 
-            while let Some(IndexationPayloadWorkerEvent(_message_id)) = receiver.next().await {
-                // TODO store
+            while let Some(IndexationPayloadWorkerEvent(ref message_id)) = receiver.next().await {
+                if let Some(message) = tangle.get(message_id).await {
+                    let indexation = match message.payload() {
+                        Some(Payload::Indexation(indexation)) => indexation,
+                        Some(Payload::Transaction(transaction)) => match transaction.essence().payload() {
+                            Some(Payload::Indexation(indexation)) => indexation,
+                            _ => continue,
+                        },
+                        _ => continue,
+                    };
+                    let _hash = indexation.hash();
+                    // TODO store
+                }
             }
 
             info!("Stopped.");
