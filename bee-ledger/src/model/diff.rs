298e78da2e976ed08d3a8986a31e387791db9b32
Thibault Martinez <thibault@iota.org>
2021-01-06 01:01:29 +0100

    New ledger Diff type

---
+++ bee-ledger/src/model/diff.rs
@@ -0,0 +1,73 @@
+// Copyright 2020 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::error::Error;
+
+use bee_common::packable::{Packable, Read, Write};
+use bee_message::MessageId;
+
+#[derive(Debug)]
+pub struct Diff {
+    spent_outputs: Vec<MessageId>,
+    created_outputs: Vec<MessageId>,
+}
+
+impl Diff {
+    pub fn new(spent_outputs: Vec<MessageId>, created_outputs: Vec<MessageId>) -> Self {
+        Self {
+            spent_outputs,
+            created_outputs,
+        }
+    }
+
+    pub fn spent_outputs(&self) -> &[MessageId] {
+        &self.spent_outputs
+    }
+
+    pub fn created_outputs(&self) -> &[MessageId] {
+        &self.created_outputs
+    }
+}
+
+impl Packable for Diff {
+    type Error = Error;
+
+    fn packed_len(&self) -> usize {
+        self.spent_outputs.packed_len() + self.created_outputs.packed_len()
+    }
+
+    fn pack<W: Write>(&self, writer: &mut W) -> Result<(), Self::Error> {
+        (self.spent_outputs.len() as u32).pack(writer)?;
+        for spent_output in self.spent_outputs.iter() {
+            spent_output.pack(writer)?;
+        }
+        (self.created_outputs.len() as u32).pack(writer)?;
+        for created_output in self.created_outputs.iter() {
+            created_output.pack(writer)?;
+        }
+
+        Ok(())
+    }
+
+    fn unpack<R: Read + ?Sized>(reader: &mut R) -> Result<Self, Self::Error>
+    where
+        Self: Sized,
+    {
+        let spent_outputs_len = u32::unpack(reader)? as usize;
+        let mut spent_outputs = Vec::with_capacity(spent_outputs_len);
+        for _ in 0..spent_outputs_len {
+            spent_outputs.push(MessageId::unpack(reader)?);
+        }
+
+        let created_outputs_len = u32::unpack(reader)? as usize;
+        let mut created_outputs = Vec::with_capacity(created_outputs_len);
+        for _ in 0..created_outputs_len {
+            created_outputs.push(MessageId::unpack(reader)?);
+        }
+
+        Ok(Self {
+            spent_outputs,
+            created_outputs,
+        })
+    }
+}
