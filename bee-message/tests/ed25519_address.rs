7487c2dd4a42ce32b15d09a9135ce583c5d8eb50
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2021-01-18 14:24:32 +0100

    Add Bech32Address (#179)
    * Add Bech32Address and remove WOTS
    
    * fix to_bech32_testnet
    
    * Make HRP configurable
    
    * Add Bech32Address and remove WOTS
    
    * add test
    
    * remove iota atoi check

--- bee-message/tests/ed25519_address.rs
+++ bee-message/tests/ed25519_address.rs
@@ -22,10 +22,23 @@ fn generate_bech32_string() {
     let vec = hex::decode(ED25519_ADDRESS).unwrap();
     bytes.copy_from_slice(&vec);
     let address = Ed25519Address::new(bytes);
-    let bech32_string = address.to_bech32();
+    let bech32_string = address.to_bech32("iota");
     assert_eq!(
         bech32_string,
-        "iot1q9f0mlq8yxpx2nck8a0slxnzr4ef2ek8f5gqxlzd0wasgp73utryjtzcp98"
+        "iota1q9f0mlq8yxpx2nck8a0slxnzr4ef2ek8f5gqxlzd0wasgp73utryj0w6qwt"
+    );
+}
+
+#[test]
+fn generate_bech32_testnet_string() {
+    let mut bytes = [0; 32];
+    let vec = hex::decode(ED25519_ADDRESS).unwrap();
+    bytes.copy_from_slice(&vec);
+    let address = Ed25519Address::new(bytes);
+    let bech32_string = address.to_bech32("atoi");
+    assert_eq!(
+        bech32_string,
+        "atoi1q9f0mlq8yxpx2nck8a0slxnzr4ef2ek8f5gqxlzd0wasgp73utryjgqtp5x"
     );
 }
 
@@ -34,7 +47,16 @@ fn bech32_string_to_address() {
     let mut bytes = [0; 32];
     let vec = hex::decode(ED25519_ADDRESS).unwrap();
     bytes.copy_from_slice(&vec);
-    let address = Address::try_from_bech32(&Ed25519Address::new(bytes).to_bech32()).unwrap();
+    let address = Address::try_from_bech32(&Ed25519Address::new(bytes).to_bech32("iota")).unwrap();
+    if let Address::Ed25519(ed) = address {
+        assert_eq!(
+            ed.to_string(),
+            "52fdfc072182654f163f5f0f9a621d729566c74d10037c4d7bbb0407d1e2c649"
+        );
+    } else {
+        panic!("Expecting an Ed25519 address");
+    }
+    let address = Address::try_from_bech32(&Ed25519Address::new(bytes).to_bech32("atoi")).unwrap();
     if let Address::Ed25519(ed) = address {
         assert_eq!(
             ed.to_string(),
