ea4ae6dd897e3684c941dc509232ba5f0616102f
Thibault Martinez <thibault@iota.org>
2021-01-19 11:33:02 +0100

    Add MerkleTree test example from RFC

--- bee-ledger/src/merkle_hasher.rs
+++ bee-ledger/src/merkle_hasher.rs
@@ -83,71 +83,33 @@ impl MerkleHasher {
 #[cfg(test)]
 mod tests {
 
-    // use super::*;
-    //
-    // use bee_ternary::{T1B1Buf, TryteBuf};
-    //
-    // use blake2::Blake2b;
-    //
-    // #[test]
-    // fn empty() {
-    //     let hash = MerkleHasher::<Blake2b>::new().digest(&[]);
-    //
-    //     assert_eq!(
-    //         hex::encode(hash),
-    //         "786a02f742015903c6c6fd852552d272912f4740e15847618a86e217f71f5419d25e1031afee585313896444934eb04b903a685b14\
-    //         48b755d56f701afe9be2ce"
-    //     )
-    // }
-    //
-    // #[test]
-    // fn null_leaf() {
-    //     let hash = MerkleHasher::<Blake2b>::new().digest(&[Hash::zeros()]);
-    //
-    //     assert_eq!(
-    //         hex::encode(hash),
-    //         "0c18f7cbf23c3c8eda01ab64c79379ff0bf0d854125cbdf7dba43ca7630171d84c042673b731cb9f92cf937d738152306a8db092d9\
-    //         413d531dd8a4299c05278f"
-    //     )
-    // }
-    //
-    // #[test]
-    // fn null_node() {
-    //     let hash = MerkleHasher::<Blake2b>::new().digest(&[Hash::zeros(), Hash::zeros()]);
-    //
-    //     assert_eq!(
-    //         hex::encode(hash),
-    //         "876b38297f865de8b89fa69d7daa4da0fc31f562228ac4b5b71009ec10e878a7aec06f48ddf98a16460b742673ed47f308ff577684\
-    //         26bf72a6aee27d1c4ba5fd"
-    //     )
-    // }
-    //
-    // #[test]
-    // fn tree() {
-    //     let mut hashes = Vec::new();
-    //
-    //     for hash in [
-    //         "NOBKDFGZMOWYUKDZITTWBRWA9YPSXCVFENCQFPC9GMJIAIPSSURYIOMYZLGNZXLUAQHHNBSRHNOIJDYZO",
-    //         "IPATPTEZSBMFJRDCRPTCVUQWBAVCAXAVZIDEDL9TSILDFWDMIIFPZIYHKRFFZDYQNKBQBVGYSKMLCYBMR",
-    //         "MXOIOFOGLIHCHMDRCWAIYCWIUCMGEZWXFJZFWBRCNSNBWIGFJXBCACPKMLLANYNXSGYKANYFTVGTLFXXX",
-    //         "EXZTJAXJMZJBBIZGUTMBOEUQDNVHJPXCLFUXNLPLSBATDMKYUZOFMHCOBWUABYDMNGMKIXLIUFXNVY9PN",
-    //         "SJXYVFUDCDPPAOALVXDQUKAWLLOQO99OSJQT9TUNILQ9VLFLCZMLZAKUTIZFHOLPMGPYHKMMUUSURIOCF",
-    //         "Q9GHMAITEZCWKFIESJARYQYMF9XWFPQTTFRXULLHQDWEZLYBSFYHSLPXEHBORDDFYZRFYFGDCM9VJKEFR",
-    //         "GMNECTSPSLSPPEITCHBXSN9KZD9OZPVPOET9TVQJDZMFGN9SGPRPMUQARNXUVKMWAFAKLKWBZLWZCTPCP",
-    //     ]
-    //     .iter()
-    //     {
-    //         hashes.push(Hash::from_inner_unchecked(
-    //             TryteBuf::try_from_str(hash).unwrap().as_trits().encode::<T1B1Buf>(),
-    //         ));
-    //     }
-    //
-    //     let hash = MerkleHasher::<Blake2b>::new().digest(&hashes);
-    //
-    //     assert_eq!(
-    //         hex::encode(hash),
-    //         "d07161bdb535afb7dbb3f5b2fb198ecf715cbd9dfca133d2b48d67b1e11173c6f92bed2f4dca92c36e8d1ef279a0c19ca9e40a113e\
-    //         9f5526090342988f86e53a"
-    //     )
-    // }
+    use super::*;
+
+    use std::str::FromStr;
+
+    #[test]
+    fn tree() {
+        let mut hashes = Vec::new();
+
+        for hash in [
+            "52fdfc072182654f163f5f0f9a621d729566c74d10037c4d7bbb0407d1e2c649",
+            "81855ad8681d0d86d1e91e00167939cb6694d2c422acd208a0072939487f6999",
+            "eb9d18a44784045d87f3c67cf22746e995af5a25367951baa2ff6cd471c483f1",
+            "5fb90badb37c5821b6d95526a41a9504680b4e7c8b763a1b1d49d4955c848621",
+            "6325253fec738dd7a9e28bf921119c160f0702448615bbda08313f6a8eb668d2",
+            "0bf5059875921e668a5bdf2c7fc4844592d2572bcd0668d2d6c52f5054e2d083",
+            "6bf84c7174cb7476364cc3dbd968b0f7172ed85794bb358b0c3b525da1786f9f",
+        ]
+        .iter()
+        {
+            hashes.push(MessageId::from_str(hash).unwrap());
+        }
+
+        let hash = MerkleHasher::new().digest(&hashes);
+
+        assert_eq!(
+            hex::encode(hash),
+            "bf67ce7ba23e8c0951b5abaec4f5524360d2c26d971ff226d3359fa70cdb0beb"
+        )
+    }
 }
