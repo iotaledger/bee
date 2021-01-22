7cfab06eaaaeaba414cc50d5f60bc609841b51a0
Joshua Barretto <joshua.barretto@iota.org>
2021-01-22 23:03:05 +0000

    Fixed timings

--- bee-protocol/src/worker/message/processor.rs
+++ bee-protocol/src/worker/message/processor.rs
@@ -84,6 +84,8 @@ where
             let mut receiver = ShutdownStream::new(shutdown, UnboundedReceiverStream::new(rx));
 
             use std::sync::atomic::{AtomicU64, Ordering};
+            static TIME_TOTAL: AtomicU64 = AtomicU64::new(0);
+
             static ITER_TOTAL: AtomicU64 = AtomicU64::new(0);
             static TIME_UNPACK: AtomicU64 = AtomicU64::new(0);
             static TIME_NETWORKID: AtomicU64 = AtomicU64::new(0);
@@ -95,6 +97,7 @@ where
             static TIME_PROPAGATE: AtomicU64 = AtomicU64::new(0);
             static TIME_REMOVEREQ: AtomicU64 = AtomicU64::new(0);
             static TIME_SEND: AtomicU64 = AtomicU64::new(0);
+            const SAMPLE_ITERS: u64 = 250;
 
             while let Some(ProcessorWorkerEvent {
                 pow_score,
@@ -105,9 +108,10 @@ where
             {
                 fn start() -> Instant { Instant::now() }
                 fn end(i: Instant, total: &AtomicU64, s: &str) {
-                    let total = total.fetch_add(i.elapsed().as_micros() as u64, Ordering::Relaxed);
-                    if ITER_TOTAL.load(Ordering::Relaxed) == 1000 {
-                        println!("Avg time for {}: {}", s, total as f32 / 1000.0);
+                    let t = total.fetch_add(i.elapsed().as_micros() as u64, Ordering::Relaxed);
+                    if ITER_TOTAL.load(Ordering::Relaxed) == SAMPLE_ITERS {
+                        println!("Avg time for {}: {} ({}% of total)", s, t as f32 / 1000.0, 100.0 * t as f32 / TIME_TOTAL.load(Ordering::Relaxed) as f32);
+                        total.store(0, Ordering::Relaxed);
                     }
                 }
 
@@ -245,10 +249,9 @@ where
                 }
                 end(s, &TIME_SEND, "send");
 
-                static TIME_TOTAL: AtomicU64 = AtomicU64::new(0);
                 let time = TIME_TOTAL.fetch_add(now.elapsed().as_micros() as u64, Ordering::Relaxed);
                 let iter = ITER_TOTAL.fetch_add(1, Ordering::Relaxed);
-                if iter == 1000 {
+                if iter == SAMPLE_ITERS {
                     println!("---- Processor body timings ----");
                     println!("Iterations = {}", iter);
                     println!("Time = {}us", time);
