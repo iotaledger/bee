35635d3952b9f66a7966ec862b06c3ce9557fb10
Thibault Martinez <thibault@iota.org>
2021-01-26 13:05:01 +0100

    Remove Vertex trait

--- bee-message/src/lib.rs
+++ bee-message/src/lib.rs
@@ -8,7 +8,6 @@ mod serde;
 mod error;
 mod message;
 mod message_id;
-mod vertex;
 
 pub mod ledger_index;
 pub mod milestone;
@@ -19,4 +18,3 @@ pub mod solid_entry_point;
 pub use error::Error;
 pub use message::{Message, MessageBuilder, MESSAGE_LENGTH_MAX};
 pub use message_id::{MessageId, MESSAGE_ID_LENGTH};
-pub use vertex::Vertex;
