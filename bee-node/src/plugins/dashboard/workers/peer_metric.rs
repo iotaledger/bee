dfd8beceaf05e29c6dffefc68abb960b3179cb4f
rusfam <samuel.rufinatscha@iota.org>
2021-01-26 12:31:30 +0100

    adjust peer metric response of dashboard

--- bee-node/src/plugins/dashboard/workers/peer_metric.rs
+++ bee-node/src/plugins/dashboard/workers/peer_metric.rs
@@ -11,6 +11,10 @@ use crate::{
 };
 
 use bee_protocol::PeerManager;
+use bee_rest_api::{
+    handlers::peers::PeersResponse,
+    types::{peer_to_peer_dto, PeerDto},
+};
 use bee_runtime::{node::Node, shutdown_stream::ShutdownStream};
 
 use futures::StreamExt;
@@ -33,7 +37,7 @@ where
     let users = users.clone();
 
     node.spawn::<Dashboard, _, _>(|shutdown| async move {
-        debug!("Ws NodeStatus topic handler running.");
+        debug!("Ws `peer_metric_worker`is running.");
 
         let mut ticker = ShutdownStream::new(
             shutdown,
@@ -41,113 +45,13 @@ where
         );
 
         while ticker.next().await.is_some() {
-            let mut metrics = Vec::new();
-
+            let mut peers_dtos = Vec::new();
             for peer in peer_manager.get_all().await {
-                let peer_metric_dto = PeerMetric {
-                    identity: peer.id().to_string(),
-                    alias: peer.alias().to_string(),
-                    origin_addr: peer.address().to_string(),
-                    connection_origin: 0,
-                    protocol_version: 0,
-                    bytes_read: 0,
-                    bytes_written: 0,
-                    heartbeat: PeerHeartbeat {
-                        solid_milestone_index: 0,
-                        pruned_milestone_index: 0,
-                        latest_milestone_index: 0,
-                        connected_neighbors: 0,
-                        synced_neighbors: 0,
-                    },
-                    info: PeerInfo {
-                        address: peer.address().to_string(),
-                        port: 0,
-                        domain: String::from(""),
-                        number_of_all_messages: peer.metrics().messages_received(),
-                        number_of_new_messages: peer.metrics().new_messages(),
-                        number_of_known_messages: peer.metrics().known_messages(),
-                        number_of_received_message_req: peer.metrics().message_requests_received(),
-                        number_of_received_milestone_req: peer.metrics().milestone_requests_received(),
-                        number_of_received_heartbeats: peer.metrics().heartbeats_received(),
-                        number_of_sent_messages: peer.metrics().messages_sent(),
-                        number_of_sent_messages_req: peer.metrics().message_requests_sent(),
-                        number_of_sent_milestone_req: peer.metrics().milestone_requests_sent(),
-                        number_of_sent_heartbeats: peer.metrics().heartbeats_sent(),
-                        number_of_dropped_sent_packets: peer.metrics().invalid_packets(),
-                        connection_type: String::from(""),
-                        autopeering_id: String::from(""),
-                        connected: peer_manager.is_connected(peer.id()).await,
-                    },
-                    connected: peer_manager.is_connected(peer.id()).await,
-                    ts: 0,
-                };
-                metrics.push(peer_metric_dto);
+                peers_dtos.push(peer_to_peer_dto(peer, &peer_manager).await);
             }
-
-            broadcast(peer_metric::forward(PeerMetrics(metrics)), &users).await;
+            broadcast(peer_metric::forward(PeersResponse(peers_dtos)), &users).await;
         }
 
-        debug!("Ws NodeStatus topic handler stopped.");
+        debug!("Ws `peer_metric_worker` stopped.");
     });
-}
-
-#[derive(Clone, Debug, Serialize)]
-pub struct PeerMetrics(pub Vec<PeerMetric>);
-
-#[derive(Clone, Debug, Serialize)]
-pub struct PeerMetric {
-    pub identity: String,
-    pub alias: String,
-    pub origin_addr: String,
-    pub connection_origin: usize,
-    pub protocol_version: usize,
-    pub bytes_read: u64,
-    pub bytes_written: u64,
-    pub heartbeat: PeerHeartbeat,
-    pub info: PeerInfo,
-    pub connected: bool,
-    pub ts: u64,
-}
-
-#[derive(Clone, Debug, Serialize)]
-pub struct PeerHeartbeat {
-    pub solid_milestone_index: usize,
-    pub pruned_milestone_index: usize,
-    pub latest_milestone_index: usize,
-    pub connected_neighbors: usize,
-    pub synced_neighbors: usize,
-}
-
-#[derive(Clone, Debug, Serialize)]
-pub struct PeerInfo {
-    pub address: String,
-    pub port: usize,
-    pub domain: String,
-    #[serde(rename = "numberOfAllTransactions")]
-    pub number_of_all_messages: u64,
-    #[serde(rename = "numberOfNewTransactions")]
-    pub number_of_new_messages: u64,
-    #[serde(rename = "numberOfKnownTransactions")]
-    pub number_of_known_messages: u64,
-    #[serde(rename = "numberOfReceivedTransactionReq")]
-    pub number_of_received_message_req: u64,
-    #[serde(rename = "numberOfReceivedMilestoneReq")]
-    pub number_of_received_milestone_req: u64,
-    #[serde(rename = "numberOfReceivedHeartbeats")]
-    pub number_of_received_heartbeats: u64,
-    #[serde(rename = "numberOfSentTransactions")]
-    pub number_of_sent_messages: u64,
-    #[serde(rename = "numberOfSentTransactionsReq")]
-    pub number_of_sent_messages_req: u64,
-    #[serde(rename = "numberOfSentMilestoneReq")]
-    pub number_of_sent_milestone_req: u64,
-    #[serde(rename = "numberOfSentHeartbeats")]
-    pub number_of_sent_heartbeats: u64,
-    #[serde(rename = "numberOfDroppedSentPackets")]
-    pub number_of_dropped_sent_packets: u64,
-    #[serde(rename = "connectionType")]
-    pub connection_type: String,
-    #[serde(rename = "autopeeringId")]
-    pub autopeering_id: String,
-    pub connected: bool,
 }
