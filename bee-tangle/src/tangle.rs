ef69aa82c3e5ccb1d0ee7a9f0c58ff7ef1fd4966
Joshua Barretto <joshua.barretto@iota.org>
2021-01-27 16:33:03 +0000

    Increased tangle cache size

--- bee-tangle/src/tangle.rs
+++ bee-tangle/src/tangle.rs
@@ -20,7 +20,7 @@ use std::{
     sync::atomic::{AtomicU64, Ordering},
 };
 
-const CACHE_LEN: usize = 100_000;
+const CACHE_LEN: usize = 1_000_000;
 
 /// A trait used to provide hooks for a tangle. The tangle acts as an in-memory cache and will use hooks to extend its
 /// effective volume. When an entry doesn't exist in the tangle cache and needs fetching, or when an entry gets
