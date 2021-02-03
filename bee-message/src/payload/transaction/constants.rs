2a3c4352201ac1918f043d212fd4e1df20c138d3
Thibault Martinez <thibault@iota.org>
2021-02-03 12:18:44 +0100

    UTXOInput tests

--- bee-message/src/payload/transaction/constants.rs
+++ bee-message/src/payload/transaction/constants.rs
@@ -1,9 +1,9 @@
 // Copyright 2020 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use core::ops::Range;
+use core::ops::{Range, RangeInclusive};
 
 pub const IOTA_SUPPLY: u64 = 2_779_530_283_277_761;
 pub const INPUT_OUTPUT_COUNT_MAX: usize = 127;
-pub const INPUT_OUTPUT_COUNT_RANGE: Range<usize> = 1..INPUT_OUTPUT_COUNT_MAX + 1;
+pub const INPUT_OUTPUT_COUNT_RANGE: RangeInclusive<usize> = 1..=INPUT_OUTPUT_COUNT_MAX;
 pub const INPUT_OUTPUT_INDEX_RANGE: Range<u16> = 0..INPUT_OUTPUT_COUNT_MAX as u16;
--- bee-message/tests/utxo_input.rs
+++ bee-message/tests/utxo_input.rs
@@ -1,13 +1,49 @@
 // Copyright 2020 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+use bee_common::packable::Packable;
 use bee_message::prelude::*;
 
 use core::str::FromStr;
 
-const UTXO_INPUT: &str = "52fdfc072182654f163f5f0f9a621d729566c74d10037c4d7bbb0407d1e2c6492a00";
+const OUTPUT_ID: &str = "52fdfc072182654f163f5f0f9a621d729566c74d10037c4d7bbb0407d1e2c6492a00";
 
 #[test]
-fn from_to_str() {
-    assert_eq!(UTXO_INPUT, UTXOInput::from_str(UTXO_INPUT).unwrap().to_string());
+fn valid_new() {
+    let output_id = OutputId::from_str(OUTPUT_ID).unwrap();
+    let input = UTXOInput::new(*output_id.transaction_id(), output_id.index()).unwrap();
+
+    assert_eq!(*input.output_id(), output_id);
+}
+
+#[test]
+fn valid_from() {
+    let output_id = OutputId::from_str(OUTPUT_ID).unwrap();
+    let input: UTXOInput = output_id.into();
+
+    assert_eq!(*input.output_id(), output_id);
+}
+
+#[test]
+fn valid_from_str() {
+    let output_id = OutputId::from_str(OUTPUT_ID).unwrap();
+    let input = UTXOInput::from_str(OUTPUT_ID).unwrap();
+
+    assert_eq!(*input.output_id(), output_id);
+}
+
+#[test]
+fn from_str_to_str() {
+    let input = UTXOInput::from_str(OUTPUT_ID).unwrap();
+
+    assert_eq!(input.to_string(), OUTPUT_ID);
+}
+
+#[test]
+fn pack_unpack() {
+    let output_id = OutputId::from_str(OUTPUT_ID).unwrap();
+    let input_1 = UTXOInput::new(*output_id.transaction_id(), output_id.index()).unwrap();
+    let input_2 = UTXOInput::unpack(&mut input_1.pack_new().as_slice()).unwrap();
+
+    assert_eq!(input_1, input_2);
 }
