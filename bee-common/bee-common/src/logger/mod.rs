9f2790e766966af525c5fa27b859db9a02be7e16
Alexander Schmidt <alexander.schmidt@iota.org>
2021-02-10 00:49:12 +0100

    Aligns log output to make more readable

--- bee-common/bee-common/src/logger/mod.rs
+++ bee-common/bee-common/src/logger/mod.rs
@@ -31,11 +31,13 @@ pub enum Error {
 macro_rules! log_format {
     ($target:expr, $level:expr, $message:expr) => {
         format_args!(
-            "{}[{}][{}] {}",
+            "{} {:target_width$} {:^level_width$} {}",
             chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
             $target,
             $level,
-            $message
+            $message,
+            level_width = 5,
+            target_width = 40
         )
     };
 }
