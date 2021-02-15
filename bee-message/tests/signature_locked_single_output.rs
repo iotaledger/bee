33abc104a1b84c2d1351dd19e4d3614128bf2cd1
Thibault Martinez <thibault@iota.org>
2021-02-15 18:49:12 +0100

    Add SignatureLockedSingleOutput test

--- bee-message/tests/signature_locked_single_output.rs
+++ bee-message/tests/signature_locked_single_output.rs
@@ -9,12 +9,12 @@ use std::str::FromStr;
 const ED25519_ADDRESS: &str = "52fdfc072182654f163f5f0f9a621d729566c74d10037c4d7bbb0407d1e2c649";
 
 #[test]
-fn valid() {
+fn valid_min_amount() {
     let address = Address::from(Ed25519Address::from_str(ED25519_ADDRESS).unwrap());
-    let output = SignatureLockedSingleOutput::new(address, 1_000).unwrap();
+    let output = SignatureLockedSingleOutput::new(address, 1).unwrap();
 
     assert_eq!(*output.address(), address);
-    assert_eq!(output.amount(), 1_000);
+    assert_eq!(output.amount(), 1);
 }
 
 #[test]
@@ -27,8 +27,9 @@ fn valid_supply_amount() {
 }
 
 #[test]
-fn invalid_null_amount() {
+fn invalid_less_than_min_amount() {
     let address = Address::from(Ed25519Address::from_str(ED25519_ADDRESS).unwrap());
+
     assert!(matches!(
         SignatureLockedSingleOutput::new(address, 0),
         Err(Error::InvalidAmount(0))
@@ -36,8 +37,9 @@ fn invalid_null_amount() {
 }
 
 #[test]
-fn invalid_big_amount() {
+fn invalid_more_than_max_amount() {
     let address = Address::from(Ed25519Address::from_str(ED25519_ADDRESS).unwrap());
+
     assert!(matches!(
         SignatureLockedSingleOutput::new(address, 3_333_333_333_333_333),
         Err(Error::InvalidAmount(3_333_333_333_333_333))
@@ -45,10 +47,23 @@ fn invalid_big_amount() {
 }
 
 #[test]
-fn pack_unpack() {
+fn pack_unpack_valid() {
     let address = Address::from(Ed25519Address::from_str(ED25519_ADDRESS).unwrap());
     let output_1 = SignatureLockedSingleOutput::new(address, 1_000).unwrap();
     let output_2 = SignatureLockedSingleOutput::unpack(&mut output_1.pack_new().as_slice()).unwrap();
 
     assert_eq!(output_1, output_2);
+}
+
+#[test]
+fn pack_unpack_invalid() {
+    let bytes = vec![
+        1, 82, 253, 252, 7, 33, 130, 101, 79, 22, 63, 95, 15, 154, 98, 29, 114, 149, 102, 199, 77, 16, 3, 124, 77, 123,
+        187, 4, 7, 209, 226, 198, 73, 0, 0, 0, 0, 0, 0, 0, 0,
+    ];
+
+    assert!(matches!(
+        SignatureLockedSingleOutput::unpack(&mut bytes.as_slice()),
+        Err(Error::InvalidAmount(0))
+    ));
 }
