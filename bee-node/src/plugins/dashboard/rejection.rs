4dea17a32013b48d3a8c05f5b1d9da37de6a6ba8
Thibault Martinez <thibault@iota.org>
2021-02-22 23:37:53 +0100

    Add auth and JWT

---
+++ bee-node/src/plugins/dashboard/rejection.rs
@@ -0,0 +1,14 @@
+// Copyright 2020 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+#[derive(Debug, Clone)]
+pub(crate) enum CustomRejection {
+    InvalidCredentials,
+    InvalidJWT,
+    InternalError,
+    NoAuthHeader,
+    InvalidAuthHeader,
+    BadRequest(&'static str),
+}
+
+impl warp::reject::Reject for CustomRejection {}
