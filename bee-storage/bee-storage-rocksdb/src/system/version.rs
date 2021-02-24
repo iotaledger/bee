9afa03251da7f37c7f86434dfc91f101745d52a3
Thibault Martinez <thibault@iota.org>
2021-02-24 15:58:13 +0100

    Bump storage version

--- bee-storage/bee-storage-rocksdb/src/system/version.rs
+++ bee-storage/bee-storage-rocksdb/src/system/version.rs
@@ -4,7 +4,7 @@
 use bee_common::packable::{Packable, Read, Write};
 
 pub(crate) const STORAGE_VERSION_KEY: u8 = 0;
-pub(crate) const STORAGE_VERSION: StorageVersion = StorageVersion(2);
+pub(crate) const STORAGE_VERSION: StorageVersion = StorageVersion(3);
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub struct StorageVersion(pub u16);
