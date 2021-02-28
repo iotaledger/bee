5c558b2d0c8fad83b4e7064f002462885ea8184f
Thibault Martinez <thibault@iota.org>
2021-02-28 11:33:53 +0100

    SignatureLockedSingleOutput tests

--- bee-message/tests/signature_locked_single_output.rs
+++ bee-message/tests/signature_locked_single_output.rs
@@ -28,28 +28,38 @@ fn new_valid_max_amount() {
 
 #[test]
 fn new_invalid_less_than_min_amount() {
-    let address = Address::from(Ed25519Address::from_str(ED25519_ADDRESS).unwrap());
-
     assert!(matches!(
-        SignatureLockedSingleOutput::new(address, 0),
+        SignatureLockedSingleOutput::new(Address::from(Ed25519Address::from_str(ED25519_ADDRESS).unwrap()), 0),
         Err(Error::InvalidAmount(0))
     ));
 }
 
 #[test]
 fn new_invalid_more_than_max_amount() {
-    let address = Address::from(Ed25519Address::from_str(ED25519_ADDRESS).unwrap());
-
     assert!(matches!(
-        SignatureLockedSingleOutput::new(address, 3_333_333_333_333_333),
+        SignatureLockedSingleOutput::new(
+            Address::from(Ed25519Address::from_str(ED25519_ADDRESS).unwrap()),
+            3_333_333_333_333_333
+        ),
         Err(Error::InvalidAmount(3_333_333_333_333_333))
     ));
 }
 
 #[test]
+fn packed_len() {
+    assert_eq!(
+        SignatureLockedSingleOutput::new(Address::from(Ed25519Address::from_str(ED25519_ADDRESS).unwrap()), 1)
+            .unwrap()
+            .packed_len(),
+        1 + 32 + 8
+    );
+}
+
+#[test]
 fn pack_unpack_valid() {
-    let address = Address::from(Ed25519Address::from_str(ED25519_ADDRESS).unwrap());
-    let output_1 = SignatureLockedSingleOutput::new(address, 1_000).unwrap();
+    let output_1 =
+        SignatureLockedSingleOutput::new(Address::from(Ed25519Address::from_str(ED25519_ADDRESS).unwrap()), 1_000)
+            .unwrap();
     let output_2 = SignatureLockedSingleOutput::unpack(&mut output_1.pack_new().as_slice()).unwrap();
 
     assert_eq!(output_1, output_2);
@@ -57,13 +67,14 @@ fn pack_unpack_valid() {
 
 #[test]
 fn pack_unpack_invalid() {
-    let bytes = vec![
-        0, 82, 253, 252, 7, 33, 130, 101, 79, 22, 63, 95, 15, 154, 98, 29, 114, 149, 102, 199, 77, 16, 3, 124, 77, 123,
-        187, 4, 7, 209, 226, 198, 73, 0, 0, 0, 0, 0, 0, 0, 0,
-    ];
-
     assert!(matches!(
-        SignatureLockedSingleOutput::unpack(&mut bytes.as_slice()),
+        SignatureLockedSingleOutput::unpack(
+            &mut vec![
+                0, 82, 253, 252, 7, 33, 130, 101, 79, 22, 63, 95, 15, 154, 98, 29, 114, 149, 102, 199, 77, 16, 3, 124,
+                77, 123, 187, 4, 7, 209, 226, 198, 73, 0, 0, 0, 0, 0, 0, 0, 0,
+            ]
+            .as_slice()
+        ),
         Err(Error::InvalidAmount(0))
     ));
 }
