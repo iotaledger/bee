117ab6a72c57e5900c2b800216721a84f11221c5
Thibault Martinez <thibault@iota.org>
2021-03-04 12:47:47 +0100

    Fix milestone payload tests

--- bee-message/tests/milestone_payload.rs
+++ bee-message/tests/milestone_payload.rs
@@ -2,19 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use bee_message::prelude::*;
-use bee_test::rand::message::rand_message_ids;
+use bee_test::rand::parents::rand_parents;
 
 #[test]
 fn new_invalid_no_public_key() {
     assert!(matches!(
-        MilestonePayloadEssence::new(
-            0,
-            0,
-            rand_message_ids(2),
-            [0; MILESTONE_MERKLE_PROOF_LENGTH],
-            vec![],
-            None,
-        ),
+        MilestonePayloadEssence::new(0, 0, rand_parents(), [0; MILESTONE_MERKLE_PROOF_LENGTH], vec![], None,),
         Err(Error::MilestoneNoPublicKey)
     ));
 }
@@ -24,7 +17,7 @@ fn new_valid_sorted_unique_public_keys() {
     assert!(MilestonePayloadEssence::new(
         0,
         0,
-        rand_message_ids(2),
+        rand_parents(),
         [0; MILESTONE_MERKLE_PROOF_LENGTH],
         vec![[0; 32], [1; 32], [2; 32], [3; 32], [4; 32], [5; 32], [6; 32], [7; 32], [8; 32], [9; 32]],
         None,
@@ -38,7 +31,7 @@ fn new_invalid_sorted_not_unique_public_keys() {
         MilestonePayloadEssence::new(
             0,
             0,
-            rand_message_ids(2),
+            rand_parents(),
             [0; MILESTONE_MERKLE_PROOF_LENGTH],
             vec![[0; 32], [1; 32], [2; 32], [3; 32], [4; 32], [4; 32], [6; 32], [7; 32], [8; 32], [9; 32]],
             None,
@@ -53,7 +46,7 @@ fn new_invalid_not_sorted_unique_public_keys() {
         MilestonePayloadEssence::new(
             0,
             0,
-            rand_message_ids(2),
+            rand_parents(),
             [0; MILESTONE_MERKLE_PROOF_LENGTH],
             vec![[0; 32], [1; 32], [3; 32], [2; 32], [4; 32], [5; 32], [6; 32], [7; 32], [8; 32], [9; 32]],
             None,
