910abc4c1fcccf01f6e05d2ce66492bff534c091
Adam-Gleave <adamg108@hotmail.co.uk>
2021-03-12 12:26:18 +0000

    Formatting

--- bee-api/bee-rest-api/src/endpoints/api/v1/receipts_at.rs
+++ bee-api/bee-rest-api/src/endpoints/api/v1/receipts_at.rs
@@ -2,8 +2,8 @@ use crate::{
     body::SuccessBody, config::ROUTE_RECEIPTS_AT, filters::with_storage, path_params::milestone_index,
     permission::has_permission, rejection::CustomRejection, storage::StorageBackend, types::ReceiptDto,
 };
-
 use super::receipt::ReceiptsResponse;
+
 use bee_ledger::types::Receipt;
 use bee_message::milestone::MilestoneIndex;
 use bee_runtime::resource::ResourceHandle;
