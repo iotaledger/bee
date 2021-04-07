ecd4711b5c771f13bc44837ea69c8a95731ab519
Thibault Martinez <thibault@iota.org>
2021-04-07 14:32:28 +0200

    Put tests under feature

--- bee-common/bee-common/tests/password.rs
+++ bee-common/bee-common/tests/password.rs
@@ -1,9 +1,11 @@
 // Copyright 2020-2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+#[cfg(feature = "password")]
 use bee_common::password;
 
 #[test]
+#[cfg(feature = "password")]
 fn correct_password() {
     let salt = password::generate_salt();
     let password_hash = password::password_hash(b"password", &salt).unwrap();
@@ -12,6 +14,7 @@ fn correct_password() {
 }
 
 #[test]
+#[cfg(feature = "password")]
 fn incorrect_password() {
     let salt = password::generate_salt();
     let password_hash = password::password_hash(b"password", &salt).unwrap();
