3ac945aba68a53a60f34f52141411f845b20c0ea
Thibault Martinez <thibault@iota.org>
2021-04-12 11:14:11 +0200

    Add RFC link

--- bee-pow/src/lib.rs
+++ bee-pow/src/lib.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 //! Contains traits and implementations to provide and score Proof of Work.
+//! RFC https://github.com/Wollac/protocol-rfcs/blob/message-pow/text/0024-message-pow/0024-message-pow.md.
 
 #![warn(missing_docs)]
 
