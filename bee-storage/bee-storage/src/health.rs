09a31f12b3e3d2580f1189fa337f97426c6a24ec
Thibault Martinez <thibault@iota.org>
2021-04-13 11:12:10 +0200

    Add storage health error

--- bee-storage/bee-storage/src/health.rs
+++ bee-storage/bee-storage/src/health.rs
@@ -5,7 +5,16 @@
 
 use bee_common::packable::{Packable, Read, Write};
 
-// TODO handle panic
+/// Errors related to storage health statuses.
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    /// I/O error happened.
+    #[error("I/O error happened: {0:?}")]
+    Io(#[from] std::io::Error),
+    /// Unknown storage health variant.
+    #[error("Unknown storage health variant: {0}")]
+    UnknownHealthVariant(u8),
+}
 
 /// Represents different health states for a `StorageBackend`.
 #[repr(u8)]
@@ -20,22 +29,24 @@ pub enum StorageHealth {
 }
 
 impl Packable for StorageHealth {
-    type Error = std::io::Error;
+    type Error = Error;
 
     fn packed_len(&self) -> usize {
         0u8.packed_len()
     }
 
     fn pack<W: Write>(&self, writer: &mut W) -> Result<(), Self::Error> {
-        (*self as u8).pack(writer)
+        (*self as u8).pack(writer)?;
+
+        Ok(())
     }
 
     fn unpack_inner<R: Read + ?Sized, const CHECK: bool>(reader: &mut R) -> Result<Self, Self::Error> {
-        Ok(match u8::unpack_inner::<R, CHECK>(reader)? {
-            0 => StorageHealth::Healthy,
-            1 => StorageHealth::Idle,
-            2 => StorageHealth::Corrupted,
-            _ => panic!("Unhandled"),
-        })
+        match u8::unpack_inner::<R, CHECK>(reader)? {
+            0 => Ok(StorageHealth::Healthy),
+            1 => Ok(StorageHealth::Idle),
+            2 => Ok(StorageHealth::Corrupted),
+            h => Err(Error::UnknownHealthVariant(h)),
+        }
     }
 }
