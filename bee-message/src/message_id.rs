d926eed76141eebbf29ea8e8f3de5ec820b4ace6
Thibault Martinez <thibault@iota.org>
2021-04-14 12:02:37 +0200

    More tests and documentations

---
+++ bee-message/src/message_id.rs
@@ -0,0 +1,89 @@
+// Copyright 2020-2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::Error;
+
+use bee_common::packable::{Packable, Read, Write};
+
+use core::{convert::TryInto, str::FromStr};
+
+/// The length of a message identifier.
+pub const MESSAGE_ID_LENGTH: usize = 32;
+
+/// A message identifier, the BLAKE2b-256 hash of the message bytes.
+/// See <https://www.blake2.net/> for more information.
+#[derive(Clone, Copy, Eq, Hash, PartialEq, Ord, PartialOrd)]
+pub struct MessageId([u8; MESSAGE_ID_LENGTH]);
+
+impl MessageId {
+    /// Creates a new `MessageId`.
+    pub fn new(bytes: [u8; MESSAGE_ID_LENGTH]) -> Self {
+        bytes.into()
+    }
+
+    /// Create a null `MessageId`.
+    pub fn null() -> Self {
+        Self([0u8; MESSAGE_ID_LENGTH])
+    }
+}
+
+#[cfg(feature = "serde")]
+string_serde_impl!(MessageId);
+
+impl From<[u8; MESSAGE_ID_LENGTH]> for MessageId {
+    fn from(bytes: [u8; MESSAGE_ID_LENGTH]) -> Self {
+        Self(bytes)
+    }
+}
+
+impl FromStr for MessageId {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let bytes: [u8; MESSAGE_ID_LENGTH] = hex::decode(s)
+            .map_err(|_| Self::Err::InvalidHexadecimalChar(s.to_owned()))?
+            .try_into()
+            .map_err(|_| Self::Err::InvalidHexadecimalLength(MESSAGE_ID_LENGTH * 2, s.len()))?;
+
+        Ok(MessageId::from(bytes))
+    }
+}
+
+impl AsRef<[u8]> for MessageId {
+    fn as_ref(&self) -> &[u8] {
+        &self.0
+    }
+}
+
+impl core::fmt::Display for MessageId {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "{}", hex::encode(self.0))
+    }
+}
+
+impl core::fmt::Debug for MessageId {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "MessageId({})", self)
+    }
+}
+
+impl Packable for MessageId {
+    type Error = Error;
+
+    fn packed_len(&self) -> usize {
+        MESSAGE_ID_LENGTH
+    }
+
+    fn pack<W: Write>(&self, writer: &mut W) -> Result<(), Self::Error> {
+        writer.write_all(&self.0)?;
+
+        Ok(())
+    }
+
+    fn unpack_inner<R: Read + ?Sized, const CHECK: bool>(reader: &mut R) -> Result<Self, Self::Error> {
+        let mut bytes = [0u8; MESSAGE_ID_LENGTH];
+        reader.read_exact(&mut bytes)?;
+
+        Ok(Self(bytes))
+    }
+}
---
+++ bee-message/src/milestone/index.rs
@@ -0,0 +1,86 @@
+// Copyright 2020-2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use bee_common::packable::{Packable, Read, Write};
+
+use core::ops::{Add, Deref, Sub};
+
+/// A wrapper around a `u32` that represents a milestone index.
+#[derive(Debug, Clone, Copy, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct MilestoneIndex(pub u32);
+
+impl MilestoneIndex {
+    /// Creates a new `MilestoneIndex`.
+    pub fn new(value: u32) -> Self {
+        Self(value)
+    }
+}
+
+impl std::fmt::Display for MilestoneIndex {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl Deref for MilestoneIndex {
+    type Target = u32;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl From<u32> for MilestoneIndex {
+    fn from(v: u32) -> Self {
+        Self(v)
+    }
+}
+
+impl Add for MilestoneIndex {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self(*self + *other)
+    }
+}
+
+impl Add<u32> for MilestoneIndex {
+    type Output = Self;
+
+    fn add(self, other: u32) -> Self {
+        Self(*self + other)
+    }
+}
+
+impl Sub for MilestoneIndex {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self(*self - *other)
+    }
+}
+
+impl Sub<u32> for MilestoneIndex {
+    type Output = Self;
+
+    fn sub(self, other: u32) -> Self {
+        Self(*self - other)
+    }
+}
+
+impl Packable for MilestoneIndex {
+    type Error = std::io::Error;
+
+    fn packed_len(&self) -> usize {
+        self.0.packed_len()
+    }
+
+    fn pack<W: Write>(&self, writer: &mut W) -> Result<(), Self::Error> {
+        self.0.pack(writer)
+    }
+
+    fn unpack_inner<R: Read + ?Sized, const CHECK: bool>(reader: &mut R) -> Result<Self, Self::Error> {
+        Ok(Self(u32::unpack_inner::<R, CHECK>(reader)?))
+    }
+}
