e78a0c7b152c9313ffbbc3037388a3400509ece6
Thibault Martinez <thibault@iota.org>
2021-04-14 11:37:51 +0200

    First set of documentation

---
+++ bee-message/src/output/mod.rs
@@ -0,0 +1,108 @@
+// Copyright 2020-2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+mod output_id;
+mod signature_locked_dust_allowance;
+mod signature_locked_single;
+mod storable;
+mod treasury;
+
+pub use output_id::{OutputId, OUTPUT_ID_LENGTH};
+pub use signature_locked_dust_allowance::SignatureLockedDustAllowanceOutput;
+pub use signature_locked_single::SignatureLockedSingleOutput;
+pub use storable::{ConsumedOutput, CreatedOutput};
+pub use treasury::{TreasuryOutput, TREASURY_OUTPUT_AMOUNT};
+
+use crate::Error;
+
+use bee_common::packable::{Packable, Read, Write};
+
+/// A generic output that can represent different types defining the deposit of funds.
+#[non_exhaustive]
+#[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(tag = "type", content = "data")
+)]
+pub enum Output {
+    /// A signature locked single output.
+    SignatureLockedSingle(SignatureLockedSingleOutput),
+    /// A signature locked dust allowance output.
+    SignatureLockedDustAllowance(SignatureLockedDustAllowanceOutput),
+    /// A treasury output.
+    Treasury(TreasuryOutput),
+}
+
+impl Output {
+    /// The kind of an `Output`.
+    pub fn kind(&self) -> u8 {
+        match self {
+            Self::SignatureLockedSingle(_) => SignatureLockedSingleOutput::KIND,
+            Self::SignatureLockedDustAllowance(_) => SignatureLockedDustAllowanceOutput::KIND,
+            Self::Treasury(_) => TreasuryOutput::KIND,
+        }
+    }
+}
+
+impl From<SignatureLockedSingleOutput> for Output {
+    fn from(output: SignatureLockedSingleOutput) -> Self {
+        Self::SignatureLockedSingle(output)
+    }
+}
+
+impl From<SignatureLockedDustAllowanceOutput> for Output {
+    fn from(output: SignatureLockedDustAllowanceOutput) -> Self {
+        Self::SignatureLockedDustAllowance(output)
+    }
+}
+
+impl From<TreasuryOutput> for Output {
+    fn from(output: TreasuryOutput) -> Self {
+        Self::Treasury(output)
+    }
+}
+
+impl Packable for Output {
+    type Error = Error;
+
+    fn packed_len(&self) -> usize {
+        match self {
+            Self::SignatureLockedSingle(output) => SignatureLockedSingleOutput::KIND.packed_len() + output.packed_len(),
+            Self::SignatureLockedDustAllowance(output) => {
+                SignatureLockedDustAllowanceOutput::KIND.packed_len() + output.packed_len()
+            }
+            Self::Treasury(output) => TreasuryOutput::KIND.packed_len() + output.packed_len(),
+        }
+    }
+
+    fn pack<W: Write>(&self, writer: &mut W) -> Result<(), Self::Error> {
+        match self {
+            Self::SignatureLockedSingle(output) => {
+                SignatureLockedSingleOutput::KIND.pack(writer)?;
+                output.pack(writer)?;
+            }
+            Self::SignatureLockedDustAllowance(output) => {
+                SignatureLockedDustAllowanceOutput::KIND.pack(writer)?;
+                output.pack(writer)?;
+            }
+            Self::Treasury(output) => {
+                TreasuryOutput::KIND.pack(writer)?;
+                output.pack(writer)?;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn unpack_inner<R: Read + ?Sized, const CHECK: bool>(reader: &mut R) -> Result<Self, Self::Error> {
+        Ok(match u8::unpack_inner::<R, CHECK>(reader)? {
+            SignatureLockedSingleOutput::KIND => SignatureLockedSingleOutput::unpack_inner::<R, CHECK>(reader)?.into(),
+            SignatureLockedDustAllowanceOutput::KIND => {
+                SignatureLockedDustAllowanceOutput::unpack_inner::<R, CHECK>(reader)?.into()
+            }
+            TreasuryOutput::KIND => TreasuryOutput::unpack_inner::<R, CHECK>(reader)?.into(),
+            k => return Err(Self::Error::InvalidOutputKind(k)),
+        })
+    }
+}
