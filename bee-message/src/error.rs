e1dcd16cb5c2faeb52c04d14549840efade9ab9f
Thibault Martinez <thibault@iota.org>
2021-04-15 16:25:33 +0200

    Order error enum

--- bee-message/src/error.rs
+++ bee-message/src/error.rs
@@ -9,53 +9,53 @@ use core::fmt;
 #[derive(Debug)]
 #[allow(missing_docs)]
 pub enum Error {
-    Io(std::io::Error),
+    CryptoError(CryptoError),
+    DuplicateError,
+    DuplicateSignature(usize),
+    InputUnlockBlockCountMismatch(usize, usize),
+    InvalidAccumulatedOutput(u128),
+    InvalidAddress,
+    InvalidAddressKind(u8),
     InvalidAmount(u64),
     InvalidDustAllowanceAmount(u64),
-    InvalidTreasuryAmount(u64),
-    InvalidMigratedFundsEntryAmount(u64),
-    InvalidInputOutputCount(usize),
-    InvalidUnlockBlockCount(usize),
-    InvalidInputOutputIndex(u16),
-    InvalidReferenceIndex(u16),
-    InvalidInputKind(u8),
-    InvalidOutputKind(u8),
     InvalidEssenceKind(u8),
-    InvalidPayloadKind(u32),
-    InvalidAddressKind(u8),
-    InvalidSignatureKind(u8),
-    InvalidUnlockBlockKind(u8),
-    InvalidAccumulatedOutput(u128),
-    InputUnlockBlockCountMismatch(usize, usize),
-    InvalidParentsCount(usize),
-    DuplicateError,
-    InvalidAddress,
-    MissingField(&'static str),
-    InvalidPayloadLength(usize, usize),
-    MissingPayload,
     InvalidHexadecimalChar(String),
     InvalidHexadecimalLength(usize, usize),
-    InvalidIndexationIndexLength(usize),
     InvalidIndexationDataLength(usize),
+    InvalidIndexationIndexLength(usize),
+    InvalidInputKind(u8),
+    InvalidInputOutputCount(usize),
+    InvalidInputOutputIndex(u16),
     InvalidMessageLength(usize),
+    InvalidMigratedFundsEntryAmount(u64),
+    InvalidOutputKind(u8),
+    InvalidParentsCount(usize),
+    InvalidPayloadKind(u32),
+    InvalidPayloadLength(usize, usize),
+    InvalidPowScoreValues(u32, u32),
     InvalidReceiptFundsCount(usize),
-    MilestonePublicKeysNotUniqueSorted,
+    InvalidReferenceIndex(u16),
+    InvalidSignature,
+    InvalidSignatureKind(u8),
+    InvalidTailTransactionHash,
+    InvalidTreasuryAmount(u64),
+    InvalidUnlockBlockCount(usize),
+    InvalidUnlockBlockKind(u8),
+    InvalidUnlockBlockReference(usize),
+    Io(std::io::Error),
+    MigratedFundsNotSorted,
     MilestoneInvalidPublicKeyCount(usize),
     MilestoneInvalidSignatureCount(usize),
+    MilestonePublicKeysNotUniqueSorted,
     MilestonePublicKeysSignaturesCountMismatch(usize, usize),
-    InvalidUnlockBlockReference(usize),
-    DuplicateSignature(usize),
+    MissingField(&'static str),
+    MissingPayload,
+    ParentsNotUniqueSorted,
+    RemainingBytesAfterMessage,
+    SignaturePublicKeyMismatch(String, String),
+    TailTransactionHashNotUnique(usize, usize),
     TransactionInputsNotSorted,
     TransactionOutputsNotSorted,
-    MigratedFundsNotSorted,
-    RemainingBytesAfterMessage,
-    ParentsNotUniqueSorted,
-    TailTransactionHashNotUnique(usize, usize),
-    SignaturePublicKeyMismatch(String, String),
-    InvalidSignature,
-    InvalidTailTransactionHash,
-    InvalidPowScoreValues(u32, u32),
-    CryptoError(CryptoError),
 }
 
 impl std::error::Error for Error {}
@@ -63,25 +63,11 @@ impl std::error::Error for Error {}
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Error::Io(e) => write!(f, "I/O error happened: {}.", e),
-            Error::InvalidAmount(amount) => write!(f, "Invalid amount: {}.", amount),
-            Error::InvalidDustAllowanceAmount(amount) => write!(f, "Invalid dust allowance amount: {}.", amount),
-            Error::InvalidTreasuryAmount(amount) => write!(f, "Invalid treasury amount: {}.", amount),
-            Error::InvalidMigratedFundsEntryAmount(amount) => {
-                write!(f, "Invalid migrated funds entry amount: {}.", amount)
+            Error::CryptoError(e) => write!(f, "Cryptographic error: {}", e),
+            Error::DuplicateError => write!(f, "The object in the set must be unique."),
+            Error::DuplicateSignature(index) => {
+                write!(f, "Duplicate signature at index: {0}", index)
             }
-            Error::InvalidInputOutputCount(count) => write!(f, "Invalid input or output count: {}.", count),
-            Error::InvalidUnlockBlockCount(count) => write!(f, "Invalid unlock block count: {}.", count),
-            Error::InvalidInputOutputIndex(index) => write!(f, "Invalid input or output index: {}.", index),
-            Error::InvalidReferenceIndex(index) => write!(f, "Invalid reference index: {}.", index),
-            Error::InvalidInputKind(k) => write!(f, "Invalid input kind: {}.", k),
-            Error::InvalidOutputKind(k) => write!(f, "Invalid output kind: {}.", k),
-            Error::InvalidEssenceKind(k) => write!(f, "Invalid essence kind: {}.", k),
-            Error::InvalidPayloadKind(k) => write!(f, "Invalid payload kind: {}.", k),
-            Error::InvalidAddressKind(k) => write!(f, "Invalid address kind: {}.", k),
-            Error::InvalidSignatureKind(k) => write!(f, "Invalid signature kind: {}.", k),
-            Error::InvalidUnlockBlockKind(k) => write!(f, "Invalid unlock block kind: {}.", k),
-            Error::InvalidAccumulatedOutput(value) => write!(f, "Invalid accumulated output balance: {}.", value),
             Error::InputUnlockBlockCountMismatch(input, block) => {
                 write!(
                     f,
@@ -89,36 +75,65 @@ impl fmt::Display for Error {
                     input, block
                 )
             }
-            Error::InvalidParentsCount(count) => {
-                write!(f, "Invalid parents count: {}.", count)
-            }
-            Error::DuplicateError => write!(f, "The object in the set must be unique."),
+            Error::InvalidAccumulatedOutput(value) => write!(f, "Invalid accumulated output balance: {}.", value),
             Error::InvalidAddress => write!(f, "Invalid address provided."),
-            Error::MissingField(s) => write!(f, "Missing required field: {}.", s),
-            Error::InvalidPayloadLength(expected, actual) => {
-                write!(f, "Invalid payload length: expected {}, got {}.", expected, actual)
-            }
-            Error::MissingPayload => write!(f, "Missing payload."),
+            Error::InvalidAddressKind(k) => write!(f, "Invalid address kind: {}.", k),
+            Error::InvalidAmount(amount) => write!(f, "Invalid amount: {}.", amount),
+            Error::InvalidDustAllowanceAmount(amount) => write!(f, "Invalid dust allowance amount: {}.", amount),
+            Error::InvalidEssenceKind(k) => write!(f, "Invalid essence kind: {}.", k),
             Error::InvalidHexadecimalChar(hex) => write!(f, "Invalid hexadecimal character: {}.", hex),
             Error::InvalidHexadecimalLength(expected, actual) => {
                 write!(f, "Invalid hexadecimal length: expected {} got {}.", expected, actual)
             }
-            Error::InvalidIndexationIndexLength(length) => {
-                write!(f, "Invalid indexation index length {}.", length)
-            }
             Error::InvalidIndexationDataLength(length) => {
                 write!(f, "Invalid indexation data length {}.", length)
             }
+            Error::InvalidIndexationIndexLength(length) => {
+                write!(f, "Invalid indexation index length {}.", length)
+            }
+            Error::InvalidInputKind(k) => write!(f, "Invalid input kind: {}.", k),
+            Error::InvalidInputOutputCount(count) => write!(f, "Invalid input or output count: {}.", count),
+            Error::InvalidInputOutputIndex(index) => write!(f, "Invalid input or output index: {}.", index),
             Error::InvalidMessageLength(length) => write!(f, "Invalid message length {}.", length),
+            Error::InvalidMigratedFundsEntryAmount(amount) => {
+                write!(f, "Invalid migrated funds entry amount: {}.", amount)
+            }
+            Error::InvalidOutputKind(k) => write!(f, "Invalid output kind: {}.", k),
+            Error::InvalidParentsCount(count) => {
+                write!(f, "Invalid parents count: {}.", count)
+            }
+            Error::InvalidPayloadKind(k) => write!(f, "Invalid payload kind: {}.", k),
+            Error::InvalidPayloadLength(expected, actual) => {
+                write!(f, "Invalid payload length: expected {}, got {}.", expected, actual)
+            }
+            Error::InvalidPowScoreValues(nps, npsmi) => write!(
+                f,
+                "Invalid pow score values: next pow score {} and next pow score milestone index {}.",
+                nps, npsmi
+            ),
             Error::InvalidReceiptFundsCount(count) => write!(f, "Invalid receipt funds count: {}.", count),
-            Error::MilestonePublicKeysNotUniqueSorted => {
-                write!(f, "Milestone public keys are not unique and/or sorted.")
+            Error::InvalidReferenceIndex(index) => write!(f, "Invalid reference index: {}.", index),
+            Error::InvalidSignature => write!(f, "Invalid signature provided."),
+            Error::InvalidSignatureKind(k) => write!(f, "Invalid signature kind: {}.", k),
+            Error::InvalidTailTransactionHash => write!(f, "Invalid tail transaction hash."),
+            Error::InvalidTreasuryAmount(amount) => write!(f, "Invalid treasury amount: {}.", amount),
+            Error::InvalidUnlockBlockCount(count) => write!(f, "Invalid unlock block count: {}.", count),
+            Error::InvalidUnlockBlockKind(k) => write!(f, "Invalid unlock block kind: {}.", k),
+            Error::InvalidUnlockBlockReference(index) => {
+                write!(f, "Invalid unlock block reference: {0}", index)
+            }
+            Error::Io(e) => write!(f, "I/O error happened: {}.", e),
+            Error::MigratedFundsNotSorted => {
+                write!(f, "Migrated funds are not sorted.")
             }
             Error::MilestoneInvalidPublicKeyCount(count) => {
                 write!(f, "Invalid milestone public key count: {}.", count)
             }
             Error::MilestoneInvalidSignatureCount(count) => {
                 write!(f, "Invalid milestone signature count: {}.", count)
+            }
+            Error::MilestonePublicKeysNotUniqueSorted => {
+                write!(f, "Milestone public keys are not unique and/or sorted.")
             }
             Error::MilestonePublicKeysSignaturesCountMismatch(kcount, scount) => {
                 write!(
@@ -127,33 +142,13 @@ impl fmt::Display for Error {
                     kcount, scount
                 )
             }
-            Error::InvalidUnlockBlockReference(index) => {
-                write!(f, "Invalid unlock block reference: {0}", index)
-            }
-            Error::DuplicateSignature(index) => {
-                write!(f, "Duplicate signature at index: {0}", index)
-            }
-            Error::TransactionInputsNotSorted => {
-                write!(f, "Transaction inputs are not sorted.")
-            }
-            Error::TransactionOutputsNotSorted => {
-                write!(f, "Transaction outputs are not sorted.")
-            }
-            Error::MigratedFundsNotSorted => {
-                write!(f, "Migrated funds are not sorted.")
-            }
-            Error::RemainingBytesAfterMessage => {
-                write!(f, "Remaining bytes after message.")
-            }
+            Error::MissingField(s) => write!(f, "Missing required field: {}.", s),
+            Error::MissingPayload => write!(f, "Missing payload."),
             Error::ParentsNotUniqueSorted => {
                 write!(f, "Parents not unique and/or sorted.")
             }
-            Error::TailTransactionHashNotUnique(previous, current) => {
-                write!(
-                    f,
-                    "Tail transaction hash is not unique at indices: {0} and {1}.",
-                    previous, current
-                )
+            Error::RemainingBytesAfterMessage => {
+                write!(f, "Remaining bytes after message.")
             }
             Error::SignaturePublicKeyMismatch(expected, actual) => {
                 write!(
@@ -162,14 +157,19 @@ impl fmt::Display for Error {
                     expected, actual
                 )
             }
-            Error::InvalidSignature => write!(f, "Invalid signature provided."),
-            Error::InvalidTailTransactionHash => write!(f, "Invalid tail transaction hash."),
-            Error::InvalidPowScoreValues(nps, npsmi) => write!(
-                f,
-                "Invalid pow score values: next pow score {} and next pow score milestone index {}.",
-                nps, npsmi
-            ),
-            Error::CryptoError(e) => write!(f, "Cryptographic error: {}", e),
+            Error::TailTransactionHashNotUnique(previous, current) => {
+                write!(
+                    f,
+                    "Tail transaction hash is not unique at indices: {0} and {1}.",
+                    previous, current
+                )
+            }
+            Error::TransactionInputsNotSorted => {
+                write!(f, "Transaction inputs are not sorted.")
+            }
+            Error::TransactionOutputsNotSorted => {
+                write!(f, "Transaction outputs are not sorted.")
+            }
         }
     }
 }
