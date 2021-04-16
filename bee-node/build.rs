4415f52197b1f02166bee4f6a750ce5e8301ec72
Alexander Schmidt <alexander.schmidt@iota.org>
2021-04-16 17:20:41 +0200

    Let build.rs not change the environment

--- bee-node/build.rs
+++ bee-node/build.rs
@@ -9,13 +9,12 @@ enum BuildError {
 }
 
 fn main() -> Result<(), BuildError> {
-    println!("build.rs running");
     match Command::new("git").args(&["rev-parse", "HEAD"]).output() {
         Ok(output) => {
-            println!(
-                "cargo:rustc-env=GIT_COMMIT={}",
-                String::from_utf8(output.stdout).unwrap()
-            );
+            // println!(
+            //     "cargo:rustc-env=GIT_COMMIT={}",
+            //     String::from_utf8(output.stdout).unwrap()
+            // );
             Ok(())
         }
         Err(_) => Err(BuildError::GitCommit),
--- bee-node/src/constants.rs
+++ bee-node/src/constants.rs
@@ -2,4 +2,4 @@
 // SPDX-License-Identifier: Apache-2.0
 
 pub(crate) const BEE_VERSION: &str = env!("CARGO_PKG_VERSION");
-pub(crate) const BEE_GIT_COMMIT: &str = "42"; //env!("GIT_COMMIT");
+pub(crate) const BEE_GIT_COMMIT: &str = "42424242"; //env!("GIT_COMMIT");
