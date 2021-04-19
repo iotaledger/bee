b02eb6926006a57667c0d1af6395fe4795c42a45
Alexander Schmidt <alexander.schmidt@iota.org>
2021-04-19 15:54:33 +0200

    Add bee-network crate with initial types

---
+++ bee-network/src/lib.rs
@@ -0,0 +1,28 @@
+// Copyright 2020-2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+//! Networking functionality and types for nodes and clients participating in the IOTA protocol built on top of
+//! `libp2p`.
+
+#![deny(missing_docs, warnings)]
+
+mod peer;
+
+// Exports
+pub use self::peer::{PeerInfo, PeerRelation};
+
+// Re-Exports
+#[doc(inline)]
+pub use libp2p::{
+    core::identity::{ed25519::Keypair, PublicKey},
+    multiaddr::Protocol,
+    Multiaddr, PeerId,
+};
+
+/// Creates a (shorter) peer alias from a peer id.
+#[macro_export]
+macro_rules! alias {
+    ($peer_id:expr) => {
+        &$peer_id.to_base58()[46..]
+    };
+}
---
+++ bee-network/src/peer/info.rs
@@ -0,0 +1,36 @@
+// Copyright 2020-2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use libp2p::Multiaddr;
+
+/// Additional information about a peer.
+#[derive(Clone, Debug)]
+pub struct PeerInfo {
+    /// The peer's address.
+    pub address: Multiaddr,
+    /// The peer's alias.
+    pub alias: String,
+    /// The type of relation we have with this peer.
+    pub relation: PeerRelation,
+}
+
+/// Describes the relation with a peer.
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum PeerRelation {
+    /// Represents a known peer.
+    Known,
+    /// Represents an unknown peer.
+    Unknown,
+}
+
+impl PeerRelation {
+    /// Returns whether the peer is known.
+    pub fn is_known(&self) -> bool {
+        self.eq(&Self::Known)
+    }
+
+    /// Returns whether the peer is unknown.
+    pub fn is_unknown(&self) -> bool {
+        self.eq(&Self::Unknown)
+    }
+}
