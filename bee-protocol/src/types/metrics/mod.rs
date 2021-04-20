b15176136ec7175ca796044eb66d937c292cd265
Thibault Martinez <thibault@iota.org>
2021-04-20 12:24:53 +0200

    Add protocol types (#522)
    * Add protocol types
    
    * Remove udeps
    
    * Update dependencies
    
    * Address review comments
    
    * Update copyright header
    
    * Address comment

---
+++ bee-protocol/src/types/metrics/mod.rs
@@ -0,0 +1,10 @@
+// Copyright 2020-2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+//! A module that provides types to hold metrics related to other components.
+
+pub mod node;
+pub mod peer;
+
+pub use node::NodeMetrics;
+pub use peer::PeerMetrics;
---
+++ bee-protocol/src/types/mod.rs
@@ -0,0 +1,9 @@
+// Copyright 2020-2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+//! A module that provides primitive types for the IOTA protocol.
+
+pub mod metrics;
+pub mod milestone_key_manager;
+pub mod milestone_key_range;
+pub mod peer;
