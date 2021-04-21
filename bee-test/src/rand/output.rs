4e475b97f63270f1e41a42b6fdae1e141ab8719a
Thibault Martinez <thibault@iota.org>
2021-04-21 11:28:15 +0200

    output_id_to_consumed_output access tests

--- bee-test/src/rand/output.rs
+++ bee-test/src/rand/output.rs
@@ -1,9 +1,9 @@
 // Copyright 2020-2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::rand::{integer::rand_integer_range, transaction::rand_transaction_id};
+use crate::rand::{integer::rand_integer_range, milestone::rand_milestone_index, transaction::rand_transaction_id};
 
-use bee_ledger::types::Unspent;
+use bee_ledger::types::{ConsumedOutput, Unspent};
 use bee_message::{constants::INPUT_OUTPUT_INDEX_RANGE, output::OutputId};
 
 pub fn rand_output_id() -> OutputId {
@@ -12,4 +12,8 @@ pub fn rand_output_id() -> OutputId {
 
 pub fn rand_unspent_output_id() -> Unspent {
     Unspent::new(rand_output_id())
+}
+
+pub fn rand_consumed_output() -> ConsumedOutput {
+    ConsumedOutput::new(rand_transaction_id(), rand_milestone_index())
 }
