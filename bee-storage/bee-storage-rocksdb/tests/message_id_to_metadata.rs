9c8b801aa70571da982401096590268cd62e145c
Thibault Martinez <thibault@iota.org>
2021-04-26 21:46:43 +0200

    Remove bee-test from cpt2

--- bee-storage/bee-storage-rocksdb/tests/message_id_to_metadata.rs
+++
@@ -1,7 +0,0 @@
-// Copyright 2020-2021 IOTA Stiftung
-// SPDX-License-Identifier: Apache-2.0
-
-#[macro_use]
-mod access;
-
-impl_access_test!(message_id_to_metadata_access);
--- bee-test/src/rand/bool.rs
+++
@@ -1,8 +0,0 @@
-// Copyright 2020-2021 IOTA Stiftung
-// SPDX-License-Identifier: Apache-2.0
-
-use rand::Rng;
-
-pub fn rand_bool() -> bool {
-    rand::thread_rng().gen::<f64>() < 0.5
-}
