b55bddf124b5972c906b7e85558c4a949905ff0c
Alexander Schmidt <alexander.schmidt@iota.org>
2021-04-28 02:12:20 +0200

    Add note to macro documentation about panicking

--- bee-network/src/alias.rs
+++ bee-network/src/alias.rs
@@ -1,7 +1,9 @@
 // Copyright 2020-2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-/// A macro that creates a shorter representation of a [`PeerId`](crate::PeerId). Mostly useful for logging.
+/// A macro that creates a shorter representation of a [`PeerId`](crate::PeerId). Mostly useful in logging scenarios.
+///
+/// **NOTE**: This macro can panic if not used with a valid [`PeerId`](crate::PeerId), or provided with a `len > 52`.
 #[macro_export]
 macro_rules! alias {
     ($peer_id:expr) => {
