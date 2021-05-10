97c55ed562b06e27dfb1a779a6c04eb380469030
Christian Poveda <christian.poveda@iota.org>
2021-05-10 11:31:43 -0500

    add test for unit structs

---
+++ bee-common/bee-common-derive/tests/packable/pass/unit_struct.rs
@@ -0,0 +1,16 @@
+// Copyright 2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+#![allow(unused_imports)]
+
+use bee_common::packable::Packable;
+use bee_common_derive::Packable;
+
+#[derive(Packable)]
+pub struct Unit;
+
+#[derive(Packable)]
+pub struct RoundUnit();
+
+fn main() {}
+
