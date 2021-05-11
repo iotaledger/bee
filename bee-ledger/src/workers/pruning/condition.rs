bf58675bab18689432918f11cf5735953f243e30
Alexander Schmidt <alexander.schmidt@iota.org>
2021-05-11 17:13:23 +0200

    Fix 'should_prune' conditions

--- bee-ledger/src/workers/pruning/condition.rs
+++ bee-ledger/src/workers/pruning/condition.rs
@@ -20,7 +20,7 @@ pub enum SkipReason {
     NotEnoughData,
 
     /// Pruning is skipped for the current confirmed milestone.
-    NotNecessary,
+    Delayed,
 }
 
 pub(crate) fn should_prune<B: StorageBackend>(
@@ -30,10 +30,10 @@ pub(crate) fn should_prune<B: StorageBackend>(
 ) -> Result<PruningTargetIndex, SkipReason> {
     if pruning_config.disabled() {
         Err(SkipReason::Disabled)
-    } else if *ledger_index < pruning_config.delay() {
+    } else if *ledger_index < *tangle.get_pruning_index() + pruning_config.delay() {
         Err(SkipReason::NotEnoughData)
-    } else if *ledger_index < *tangle.get_pruning_index() + pruning_config.interval() {
-        Err(SkipReason::NotNecessary)
+    } else if *ledger_index % pruning_config.interval() != 0 {
+        Err(SkipReason::Delayed)
     } else {
         // Subtracting unsigned:
         // We made sure that `confirmed_milestone_index` >= pruning_config.delay() always holds.
