16018167c071190b84384f88d4e5e1f7abf9e028
Thibault Martinez <thibault@iota.org>
2021-05-11 16:06:25 +0200

    Remove output clone

--- bee-ledger/src/workers/storage.rs
+++ bee-ledger/src/workers/storage.rs
@@ -467,12 +467,12 @@ pub async fn fetch_unspent_treasury_output<B: StorageBackend>(storage: &B) -> Re
         .await
         .map_err(|e| Error::Storage(Box::new(e)))?
     {
-        Some(outputs) => {
+        Some(mut outputs) => {
             match outputs.len() {
                 // There has to be an unspent treasury output at all time.
                 0 => panic!("No unspent treasury output found"),
                 // Indexing is fine since length is known
-                1 => Ok(outputs[0].clone()),
+                1 => Ok(outputs.remove(0)),
                 // There should be one and only one unspent treasury output at all time.
                 _ => panic!("More than one unspent treasury output found"),
             }
