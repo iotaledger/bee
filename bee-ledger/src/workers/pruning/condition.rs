635411e8a4e7c07b56eeecb5712f15d173a4686f
Alexander Schmidt <alexander.schmidt@iota.org>
2021-05-11 17:41:49 +0200

    Add 'remaining_milestones' parameter to skip reason variants

--- bee-ledger/src/workers/pruning/condition.rs
+++ bee-ledger/src/workers/pruning/condition.rs
@@ -17,10 +17,10 @@ pub enum SkipReason {
     Disabled,
 
     /// There is not enough data yet to be pruned according to the config settings.
-    NotEnoughData,
+    NotEnoughData { remaining_milestones: u32 },
 
     /// Pruning is deferred to a later milestone.
-    Deferred,
+    Deferred { remaining_milestones: u32 },
 }
 
 pub(crate) fn should_prune<B: StorageBackend>(
@@ -31,9 +31,13 @@ pub(crate) fn should_prune<B: StorageBackend>(
     if pruning_config.disabled() {
         Err(SkipReason::Disabled)
     } else if *ledger_index < *tangle.get_pruning_index() + pruning_config.delay() {
-        Err(SkipReason::NotEnoughData)
+        Err(SkipReason::NotEnoughData {
+            remaining_milestones: (*tangle.get_pruning_index() + pruning_config.delay()) - *ledger_index,
+        })
     } else if *ledger_index % pruning_config.interval() != 0 {
-        Err(SkipReason::Deferred)
+        Err(SkipReason::Deferred {
+            remaining_milestones: *ledger_index % pruning_config.interval(),
+        })
     } else {
         // Subtracting unsigned:
         // We made sure that `confirmed_milestone_index` >= pruning_config.delay() always holds.
