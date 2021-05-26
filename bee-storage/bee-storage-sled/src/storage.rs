36e4057528c52814701cebf8588e18e8c0bcb36c
Christian Poveda <christian.poveda@iota.org>
2021-05-26 11:16:24 -0500

    add `bee-storage-sled` crate

---
+++ bee-storage/bee-storage-sled/src/storage.rs
@@ -0,0 +1,70 @@
+// Copyright 2020-2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::trees::*;
+
+use bee_storage::{backend::StorageBackend, health::StorageHealth};
+
+use async_trait::async_trait;
+use sled;
+
+type Error = sled::Error;
+
+pub struct Storage {
+    pub(crate) inner: sled::Db,
+}
+
+impl Storage {
+    pub fn new() -> Result<Self, Error> {
+        let inner = sled::open("./db")?;
+
+        inner.open_tree(TREE_SYSTEM)?;
+        inner.open_tree(TREE_MESSAGE_ID_TO_MESSAGE)?;
+        inner.open_tree(TREE_MESSAGE_ID_TO_METADATA)?;
+        inner.open_tree(TREE_MESSAGE_ID_TO_MESSAGE_ID)?;
+        inner.open_tree(TREE_INDEX_TO_MESSAGE_ID)?;
+        inner.open_tree(TREE_OUTPUT_ID_TO_CREATED_OUTPUT)?;
+        inner.open_tree(TREE_OUTPUT_ID_TO_CONSUMED_OUTPUT)?;
+        inner.open_tree(TREE_OUTPUT_ID_UNSPENT)?;
+        inner.open_tree(TREE_ED25519_ADDRESS_TO_OUTPUT_ID)?;
+        inner.open_tree(TREE_LEDGER_INDEX)?;
+        inner.open_tree(TREE_MILESTONE_INDEX_TO_MILESTONE)?;
+        inner.open_tree(TREE_SNAPSHOT_INFO)?;
+        inner.open_tree(TREE_SOLID_ENTRY_POINT_TO_MILESTONE_INDEX)?;
+        inner.open_tree(TREE_MILESTONE_INDEX_TO_OUTPUT_DIFF)?;
+        inner.open_tree(TREE_ADDRESS_TO_BALANCE)?;
+        inner.open_tree(TREE_MILESTONE_INDEX_TO_UNREFERENCED_MESSAGE)?;
+        inner.open_tree(TREE_MILESTONE_INDEX_TO_RECEIPT)?;
+        inner.open_tree(TREE_SPENT_TO_TREASURY_OUTPUT)?;
+
+        Ok(Self { inner })
+    }
+}
+
+#[async_trait]
+impl StorageBackend for Storage {
+    type ConfigBuilder = ();
+    type Config = ();
+    type Error = Error;
+
+    async fn start(config: Self::Config) -> Result<Self, Self::Error> {
+        Self::new()
+    }
+
+    async fn shutdown(self) -> Result<(), Self::Error> {
+        self.inner.flush()?;
+        Ok(())
+    }
+
+    async fn size(&self) -> Result<Option<usize>, Self::Error> {
+        Ok(Some(self.inner.size_on_disk()? as usize))
+    }
+
+    async fn get_health(&self) -> Result<Option<StorageHealth>, Self::Error> {
+        Ok(None)
+    }
+
+    async fn set_health(&self, health: StorageHealth) -> Result<(), Self::Error> {
+        Ok(())
+    }
+}
---
+++ bee-storage/bee-storage-sled/src/trees.rs
@@ -0,0 +1,21 @@
+// Copyright 2020-2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+pub const TREE_SYSTEM: &str = "system";
+pub const TREE_MESSAGE_ID_TO_MESSAGE: &str = "message_id_to_message";
+pub const TREE_MESSAGE_ID_TO_METADATA: &str = "message_id_to_metadata";
+pub const TREE_MESSAGE_ID_TO_MESSAGE_ID: &str = "message_id_to_message_id";
+pub const TREE_INDEX_TO_MESSAGE_ID: &str = "index_to_message_id";
+pub const TREE_OUTPUT_ID_TO_CREATED_OUTPUT: &str = "output_id_to_created_output";
+pub const TREE_OUTPUT_ID_TO_CONSUMED_OUTPUT: &str = "output_id_to_consumed_output";
+pub const TREE_OUTPUT_ID_UNSPENT: &str = "output_id_unspent";
+pub const TREE_ED25519_ADDRESS_TO_OUTPUT_ID: &str = "ed25519_address_to_output_id";
+pub const TREE_LEDGER_INDEX: &str = "ledger_index";
+pub const TREE_MILESTONE_INDEX_TO_MILESTONE: &str = "milestone_index_to_milestone";
+pub const TREE_SNAPSHOT_INFO: &str = "snapshot_info";
+pub const TREE_SOLID_ENTRY_POINT_TO_MILESTONE_INDEX: &str = "solid_entry_point_to_milestone_index";
+pub const TREE_MILESTONE_INDEX_TO_OUTPUT_DIFF: &str = "milestone_index_to_output_diff";
+pub const TREE_ADDRESS_TO_BALANCE: &str = "address_to_balance";
+pub const TREE_MILESTONE_INDEX_TO_UNREFERENCED_MESSAGE: &str = "milestone_index_to_unreferenced_message";
+pub const TREE_MILESTONE_INDEX_TO_RECEIPT: &str = "milestone_index_to_receipt";
+pub const TREE_SPENT_TO_TREASURY_OUTPUT: &str = "spent_to_treasury_output";
