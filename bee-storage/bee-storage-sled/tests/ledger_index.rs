fd4a41ff1c61a0f8f830af1e5b3cb4a1fd175273
Christian Poveda <christian.poveda@iota.org>
2021-05-26 19:55:56 -0500

    add tests

---
+++ bee-storage/bee-storage-sled/tests/ledger_index.rs
@@ -0,0 +1,7 @@
+// Copyright 2020-2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+#[macro_use]
+mod access;
+
+impl_access_test!(ledger_index_access);
---
+++ bee-storage/bee-storage-sled/tests/milestone_index_to_output_diff.rs
@@ -0,0 +1,7 @@
+// Copyright 2020-2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+#[macro_use]
+mod access;
+
+impl_access_test!(milestone_index_to_output_diff_access);
