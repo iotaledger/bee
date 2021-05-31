d5d70cb662d27513913b9a0cca1d26d33d2f6e5a
rusfam <samuel.rufinatscha@iota.org>
2021-05-31 17:47:42 +0200

    use map_err in path_params.rs

--- bee-api/bee-rest-api/src/endpoints/path_params.rs
+++ bee-api/bee-rest-api/src/endpoints/path_params.rs
@@ -16,77 +16,56 @@ use warp::{reject, Filter, Rejection};
 
 pub(super) fn output_id() -> impl Filter<Extract = (OutputId,), Error = Rejection> + Copy {
     warp::path::param().and_then(|value: String| async move {
-        match value.parse::<OutputId>() {
-            Ok(id) => Ok(id),
-            Err(_) => Err(reject::custom(CustomRejection::BadRequest(
-                "invalid output id".to_string(),
-            ))),
-        }
+        value.parse::<OutputId>().map_err(|_| reject::custom(CustomRejection::BadRequest(
+            "invalid output id".to_string(),
+        )))
     })
 }
 
 pub(super) fn message_id() -> impl Filter<Extract = (MessageId,), Error = Rejection> + Copy {
     warp::path::param().and_then(|value: String| async move {
-        match value.parse::<MessageId>() {
-            Ok(id) => Ok(id),
-            Err(_) => Err(reject::custom(CustomRejection::BadRequest(
-                "invalid message id".to_string(),
-            ))),
-        }
+        value.parse::<MessageId>().map_err(|_| reject::custom(CustomRejection::BadRequest(
+            "invalid message id".to_string(),
+        )))
     })
 }
 
 pub(super) fn transaction_id() -> impl Filter<Extract = (TransactionId,), Error = Rejection> + Copy {
     warp::path::param().and_then(|value: String| async move {
-        match value.parse::<TransactionId>() {
-            Ok(id) => Ok(id),
-            Err(_) => Err(reject::custom(CustomRejection::BadRequest(
-                "invalid transaction id".to_string(),
-            ))),
-        }
+        value.parse::<TransactionId>().map_err(|_| reject::custom(CustomRejection::BadRequest(
+            "invalid transaction id".to_string(),
+        )))
     })
 }
 
 pub(super) fn milestone_index() -> impl Filter<Extract = (MilestoneIndex,), Error = Rejection> + Copy {
     warp::path::param().and_then(|value: String| async move {
-        match value.parse::<u32>() {
-            Ok(i) => Ok(MilestoneIndex(i)),
-            Err(_) => Err(reject::custom(CustomRejection::BadRequest(
-                "invalid milestone index".to_string(),
-            ))),
-        }
+        value.parse::<u32>().map_err(|_| reject::custom(CustomRejection::BadRequest(
+            "invalid milestone index".to_string(),
+        ))).map(|index| MilestoneIndex(index))
     })
 }
 
 pub(super) fn bech32_address() -> impl Filter<Extract = (Address,), Error = Rejection> + Copy {
     warp::path::param().and_then(|value: String| async move {
-        match Address::try_from_bech32(&value) {
-            Ok(addr) => Ok(addr),
-            Err(_) => Err(reject::custom(CustomRejection::BadRequest(
-                "invalid address".to_string(),
-            ))),
-        }
+        Address::try_from_bech32(&value).map_err(|_| reject::custom(CustomRejection::BadRequest(
+            "invalid address".to_string(),
+        )))
     })
 }
 
 pub(super) fn ed25519_address() -> impl Filter<Extract = (Ed25519Address,), Error = Rejection> + Copy {
     warp::path::param().and_then(|value: String| async move {
-        match value.parse::<Ed25519Address>() {
-            Ok(addr) => Ok(addr),
-            Err(_) => Err(reject::custom(CustomRejection::BadRequest(
-                "invalid Ed25519 address".to_string(),
-            ))),
-        }
+        value.parse::<Ed25519Address>().map_err(|_| reject::custom(CustomRejection::BadRequest(
+            "invalid Ed25519 address".to_string(),
+        )))
     })
 }
 
 pub(super) fn peer_id() -> impl Filter<Extract = (PeerId,), Error = Rejection> + Copy {
     warp::path::param().and_then(|value: String| async move {
-        match value.parse::<PeerId>() {
-            Ok(id) => Ok(id),
-            Err(_) => Err(reject::custom(CustomRejection::BadRequest(
-                "invalid peer id".to_string(),
-            ))),
-        }
+        value.parse::<PeerId>().map_err(|_| reject::custom(CustomRejection::BadRequest(
+            "invalid peer id".to_string(),
+        )))
     })
 }
