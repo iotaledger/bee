3905b2f161e335d2c394d16459d8384c5b4a32e4
Thibault Martinez <thibault@iota.org>
2021-06-10 09:50:14 +0200

    Add missing ord tests

--- bee-common/bee-ord/tests/ord.rs
+++ bee-common/bee-ord/tests/ord.rs
@@ -6,6 +6,12 @@ extern crate alloc;
 use alloc::vec::Vec;
 
 #[test]
+fn empty_is_sorted() {
+    let vec = Vec::<u8>::new();
+    assert!(bee_ord::is_sorted(vec.iter()));
+}
+
+#[test]
 fn is_sorted() {
     let vec = Vec::from([0, 1, 2, 3, 5, 7, 10, 12, 29]);
     assert!(bee_ord::is_sorted(vec.iter()));
@@ -15,6 +21,12 @@ fn is_sorted() {
 fn not_sorted() {
     let vec = Vec::from([0, 1, 2, 3, 5, 7, 10, 29, 12]);
     assert!(!bee_ord::is_sorted(vec.iter()));
+}
+
+#[test]
+fn empty_is_unique_sorted() {
+    let vec = Vec::<u8>::new();
+    assert!(bee_ord::is_unique_sorted(vec.iter()));
 }
 
 #[test]
