b741d2616c60770f68f199ed434e2bb797acb1d1
Thibault Martinez <thibault@iota.org>
2021-06-10 16:26:20 +0200

    Add #![warn(missing_docs)]

--- bee-node/src/lib.rs
+++ bee-node/src/lib.rs
@@ -1,2 +1,6 @@
 // Copyright 2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
+
+//! Crate containing types and functionalities to build nodes for the IOTA networks.
+
+#![warn(missing_docs)]
