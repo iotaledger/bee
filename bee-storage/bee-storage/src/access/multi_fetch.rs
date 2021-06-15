02a7a87bfec20d8f838351bc25593869bf5ff027
Thibault Martinez <thibault@iota.org>
2021-06-15 17:35:20 +0200

    Remove clippy allow

--- bee-storage/bee-storage/src/access/multi_fetch.rs
+++ bee-storage/bee-storage/src/access/multi_fetch.rs
@@ -5,10 +5,10 @@ use crate::backend::StorageBackend;
 
 /// `MultiFetch<K, V>` trait extends the `StorageBackend` with `multi_fetch` operation for the (key: K, value: V pair);
 /// therefore, it should be explicitly implemented for the corresponding `StorageBackend`.
-#[allow(clippy::type_complexity)]
 pub trait MultiFetch<'a, K, V>: StorageBackend {
     /// The iterator associated type over values.
     type Iter: 'a + Iterator<Item = Result<Option<V>, Self::Error>>;
+
     /// Fetches the values associated with the keys from the storage.
     fn multi_fetch(&'a self, keys: &'a [K]) -> Result<Self::Iter, Self::Error>;
 }
