f5b7062b5873bb4d5c3ee78f87a830e3e21c244e
Thibault Martinez <thibault@iota.org>
2021-06-15 17:36:56 +0200

    Fix comment

--- bee-storage/bee-storage/src/access/multi_fetch.rs
+++ bee-storage/bee-storage/src/access/multi_fetch.rs
@@ -3,8 +3,8 @@
 
 use crate::backend::StorageBackend;
 
-/// `MultiFetch<K, V>` trait extends the `StorageBackend` with `multi_fetch` operation for the (key: K, value: V pair);
-/// therefore, it should be explicitly implemented for the corresponding `StorageBackend`.
+/// `MultiFetch<'a, K, V>` trait extends the `StorageBackend` with `multi_fetch` operation for the (key: K, value: V)
+/// pair; therefore, it should be explicitly implemented for the corresponding `StorageBackend`.
 pub trait MultiFetch<'a, K, V>: StorageBackend {
     /// The iterator associated type over values.
     type Iter: 'a + Iterator<Item = Result<Option<V>, Self::Error>>;
