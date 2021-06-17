e1e53adcaacea03f5d1294dad57c001e96d5301a
Thibault Martinez <thibault@iota.org>
2021-06-17 18:36:29 +0200

    Clean doc

--- bee-common/bee-event-bus/src/lib.rs
+++ bee-common/bee-event-bus/src/lib.rs
@@ -1,7 +1,7 @@
 // Copyright 2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-//! A module that provides a generic, type-safe and thread-safe event bus for arbitrary event types.
+//! A crate that provides a generic, type-safe and thread-safe event bus for arbitrary event types.
 
 #![warn(missing_docs)]
 
@@ -27,23 +27,16 @@ impl<'a> Default for EventBus<'a> {
     }
 }
 
+// TODO unwrap
+
 impl<'a> EventBus<'a> {
     /// Creates a new `EventBus`.
     pub fn new() -> Self {
         Self::default()
     }
 
-    /// Dispatch an event via this event bus.
-    ///
-    /// All active listeners registered for this event will be invoked.
-    pub fn dispatch<E: Any>(&self, event: E) {
-        if let Some(ls) = self.listeners.lock().unwrap().get_mut(&TypeId::of::<E>()) {
-            ls.iter_mut().for_each(|(l, _)| l(&event))
-        }
-    }
-
-    /// Add an event listener bound to a specific event type, `E`, and registered with the given type ID.
-    pub fn add_listener_raw<E: Any, F: Fn(&E) + Send + Sync + 'a>(&self, id: TypeId, handler: F) {
+    /// Adds an event listener bound to a specific event type `E` and registered with the given `TypeId`.
+    pub fn add_listener_with_id<E: Any, F: Fn(&E) + Send + Sync + 'a>(&self, handler: F, id: TypeId) {
         self.listeners
             .lock()
             .unwrap()
@@ -55,33 +48,37 @@ impl<'a> EventBus<'a> {
             ));
     }
 
-    /// Add an event listener bound to a specific event type, `E`, and bound to a type `T`.
-    ///
-    /// This event listener will be removed when [`EventBus::remove_listeners_by_id`] is called with the `TypeId` of
-    /// `T`.
+    /// Adds an event listener bound to a specific event type `E` and registered with the type `T`.
     pub fn add_listener<T: Any, E: Any, F: Fn(&E) + Send + Sync + 'a>(&self, handler: F) {
-        self.add_listener_raw(TypeId::of::<T>(), handler);
+        self.add_listener_with_id(handler, TypeId::of::<T>());
     }
 
-    /// Add an event listener bound to a specific event type, `E`, registered using a hidden type that will prevent its
-    /// removal until the event bus is dropped.
+    /// Adds an event listener bound to a specific event type `E` and registered with a hidden type that will prevent
+    /// its removal until the event bus is dropped.
     pub fn add_static_listener<E: Any, F: Fn(&E) + Send + Sync + 'a>(&self, handler: F) {
         struct Static;
 
-        self.add_listener_raw(TypeId::of::<Static>(), handler);
+        self.add_listener_with_id(handler, TypeId::of::<Static>());
     }
 
-    ///
-    pub fn remove_listeners<T: Any>(&self) {
-        self.remove_listeners_by_id(TypeId::of::<T>());
+    /// Dispatches an event via the event bus. All active listeners registered for this event will be invoked.
+    pub fn dispatch<E: Any>(&self, event: E) {
+        if let Some(listeners) = self.listeners.lock().unwrap().get_mut(&TypeId::of::<E>()) {
+            listeners.iter_mut().for_each(|(listener, _)| listener(&event))
+        }
     }
 
-    /// Remove all event listeners registered with the given ID, dropping them in the process.
-    pub fn remove_listeners_by_id(&self, id: TypeId) {
+    /// Removes all event listeners registered with the given `TypeId`.
+    pub fn remove_listeners_with_id(&self, id: TypeId) {
         self.listeners
             .lock()
             .unwrap()
             .iter_mut()
             .for_each(|(_, listeners)| listeners.retain(|(_, listener_id)| listener_id != &id));
+    }
+
+    /// Removes all event listeners registered with the type `T`.
+    pub fn remove_listeners<T: Any>(&self) {
+        self.remove_listeners_with_id(TypeId::of::<T>());
     }
 }
