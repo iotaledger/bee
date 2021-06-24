8cd3021969caacb9532dc33f64ee0122c4237a1f
Jean Pierre Dudey <jeandudey@hotmail.com>
2021-06-24 13:44:46 +0200

    Re-run build script if git commit changes (#644)
    * Re-run build script if git commit changes
    
    Signed-off-by: Jean Pierre Dudey <me@jeandudey.tech>
    
    * fixup! Re-run build script if git commit changes

--- bee-node/build.rs
+++ bee-node/build.rs
@@ -6,6 +6,7 @@ use std::process::Command;
 #[derive(Debug)]
 enum BuildError {
     GitCommit,
+    GitBranch,
 }
 
 fn main() -> Result<(), BuildError> {
@@ -15,8 +16,20 @@ fn main() -> Result<(), BuildError> {
                 "cargo:rustc-env=GIT_COMMIT={}",
                 String::from_utf8(output.stdout).unwrap()
             );
-            Ok(())
         }
-        Err(_) => Err(BuildError::GitCommit),
+        Err(_) => return Err(BuildError::GitCommit),
     }
+
+    match Command::new("git").args(&["rev-parse", "--abbrev-ref", "HEAD"]).output() {
+        Ok(output) => {
+            println!("cargo:rerun-if-changed=../.git/HEAD");
+            println!(
+                "cargo:rerun-if-changed=../.git/refs/heads/{}",
+                String::from_utf8(output.stdout).unwrap(),
+            );
+        }
+        Err(_) => return Err(BuildError::GitBranch),
+    }
+
+    Ok(())
 }
