3a66f4afe2afdd221a4ad0d078b3ec37ddb6e5b6
Adam-Gleave <adamg108@hotmail.co.uk>
2021-06-29 09:51:29 +0100

    Add bee-test crate

---
+++ bee-test/src/lib.rs
@@ -0,0 +1,9 @@
+// Copyright 2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+//! A crate providing utilities for testing purposes.
+
+#![deny(missing_docs, warnings)]
+
+/// A module providing random generation of types of testing purposes.
+pub mod rand;
