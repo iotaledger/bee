e5ec0729677853a8c16caf19b9d469f077e07cbe
Adam Gleave <adamg108@hotmail.co.uk>
2021-06-29 10:50:35 +0100

    Add bee-test crate (#648)
    * Add bee-test crate
    
    * rand_bytes_array
    
    * Formatting + description
    
    * Update doc-comment
    
    * rand v0.8.4 -> v0.8
    
    * Words!

---
+++ bee-test/src/rand/bytes.rs
@@ -0,0 +1,14 @@
+// Copyright 2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use rand::Rng;
+
+/// Generates a vector of random bytes with a given length.
+pub fn rand_bytes(len: usize) -> Vec<u8> {
+    (0..len).map(|_| rand::random::<u8>()).collect()
+}
+
+/// Generates an array of random bytes of length N.
+pub fn rand_bytes_array<const N: usize>() -> [u8; N] {
+    rand::thread_rng().gen::<[u8; N]>()
+}
---
+++ bee-test/src/rand/mod.rs
@@ -0,0 +1,13 @@
+// Copyright 2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+/// Module providing random boolean generation utilities.
+pub mod bool;
+/// Module providing random byte generation utilities.
+pub mod bytes;
+/// Module providing random number generation utilities.
+pub mod number;
+/// Module providing random option generation utilities.
+pub mod option;
+/// Module providing random string generation utilities.
+pub mod string;
