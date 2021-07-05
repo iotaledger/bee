d280cbbe77b8c6bc4e284a961465633d8c236a69
Thibault Martinez <thibault@iota.org>
2021-07-05 15:16:04 +0200

    impl<T: Packable> Packable for Box<T> (#654)

--- bee-common/bee-packable/tests/box.rs
+++ bee-common/bee-packable/tests/box.rs
@@ -6,6 +6,18 @@ mod common;
 #[test]
 fn packable_box() {
     assert_eq!(
+        common::generic_test(&(Box::new(42u64))).0.len(),
+        core::mem::size_of::<u64>()
+    );
+    assert_eq!(
+        common::generic_test(&(Box::new(Some([0u8; 5])))).0.len(),
+        (core::mem::size_of::<u8>() + 5 * core::mem::size_of::<u8>())
+    );
+}
+
+#[test]
+fn packable_box_slice() {
+    assert_eq!(
         common::generic_test(&(Box::new([]) as Box::<[u32]>)).0.len(),
         core::mem::size_of::<u64>()
     );
