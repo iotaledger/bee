06078617e95a50fdf9fb1dd124a04951778ccdc7
Adam-Gleave <adamg108@hotmail.co.uk>
2021-07-07 17:01:30 +0100

    fmt

--- bee-common/bee-packable/tests/vec_prefix.rs
+++ bee-common/bee-packable/tests/vec_prefix.rs
@@ -12,13 +12,18 @@ macro_rules! impl_packable_test_for_vec_prefix {
         #[test]
         fn $name() {
             assert_eq!(
-                common::generic_test(&VecPrefix::<u32, $ty, MAX_LENGTH>::new()).0.len(),
+                common::generic_test(&VecPrefix::<u32, $ty, MAX_LENGTH>::new())
+                    .0
+                    .len(),
                 core::mem::size_of::<$ty>()
             );
             assert_eq!(
-                common::generic_test(&VecPrefix::<Option<u32>, $ty, MAX_LENGTH>::from(vec![Some(0u32), None]))
-                    .0
-                    .len(),
+                common::generic_test(&VecPrefix::<Option<u32>, $ty, MAX_LENGTH>::from(vec![
+                    Some(0u32),
+                    None
+                ]))
+                .0
+                .len(),
                 core::mem::size_of::<$ty>()
                     + (core::mem::size_of::<u8>() + core::mem::size_of::<u32>())
                     + core::mem::size_of::<u8>()
