4659a58637e71e652960ce9baa4ad82dc5a3e024
Thibault Martinez <thibault@iota.org>
2021-07-07 14:32:24 +0200

    Allow excluding crates from missing files workflow

--- .github/workflows/scripts/missing_files.sh
+++ .github/workflows/scripts/missing_files.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 
 res=0
+excluded_crates=($1)
+
+exclude_crate () {
+  local array="$1[@]"
+  local seeking=$2
+  local in=1
+  for element in "${!array}"; do
+    if [[ "./$element" == "$seeking" ]]; then
+        in=0
+        break
+    fi
+  done
+  return $in
+}
 
 while read file
 do
   dir=$(dirname "$file")
+
+  if exclude_crate excluded_crates $dir; then
+    continue;
+  fi
 
   file="$dir/CHANGELOG.md"
   if [[ "$dir" != "." && ! -f $file ]]; then
