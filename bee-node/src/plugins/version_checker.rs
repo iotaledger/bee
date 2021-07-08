f8b562400194cb5f8e8e91ccb6262e1871b26dac
Matthieu Daumas <matthieu.daumas@iota.org>
2021-07-08 17:13:54 +0200

    Fix some coding style

--- bee-node/src/plugins/version_checker.rs
+++ bee-node/src/plugins/version_checker.rs
@@ -1,16 +1,17 @@
 // Copyright 2020-2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::constants::{BEE_GIT_API_LATEST, BEE_VERSION};
+
 use bee_runtime::{node::Node, shutdown_stream::ShutdownStream, worker::Worker};
 
 use async_trait::async_trait;
 use futures::StreamExt;
-use log::{info, warn, error};
+use log::{error, info, warn};
+use reqwest::{header::USER_AGENT, Client};
+use serde_json::Value;
 use tokio::time::interval;
 use tokio_stream::wrappers::IntervalStream;
-use crate::constants::{BEE_VERSION, BEE_GIT_API_LATEST};
-use reqwest::{Client, header::USER_AGENT};
-use serde_json::Value;
 
 use std::{convert::Infallible, time::Duration};
 
@@ -36,7 +37,6 @@ impl<N: Node> Worker<N> for VersionChecker {
             let client = reqwest::Client::new();
 
             while ticker.next().await.is_some() {
-
                 match is_latest(&client).await {
                     Ok(false) => warn!("A new version has been released. Please update the node."),
                     Err(e) => error!("Error while checking for new update. {:?}", e),
@@ -52,23 +52,20 @@ impl<N: Node> Worker<N> for VersionChecker {
 }
 
 async fn is_latest(client: &Client) -> Result<bool, reqwest::Error> {
-
-    let res = client.get(BEE_GIT_API_LATEST)
+    let res = client
+        .get(BEE_GIT_API_LATEST)
         .header(USER_AGENT, "curl")
         .send()
-        .await?.error_for_status()?;
+        .await?
+        .error_for_status()?;
 
     match res.text().await {
-        Ok(text) => {
-            match serde_json::from_str::<Value>(text.as_str()) {
-                Ok(value) => {
-                    match value.get("tag_name") {
-                        Some(tag_name) => return Ok(tag_name == format!("v{}", BEE_VERSION).as_str()),
-                        None => error!("No version field found."),
-                    }
-                }
-                Err(_err_parse_json) => error!("Error while getting update informations. {:?}", _err_parse_json),
-            }
+        Ok(text) => match serde_json::from_str::<Value>(text.as_str()) {
+            Ok(value) => match value.get("tag_name") {
+                Some(tag_name) => return Ok(tag_name == format!("v{}", BEE_VERSION).as_str()),
+                None => error!("No version field found."),
+            },
+            Err(_err_parse_json) => error!("Error while getting update informations. {:?}", _err_parse_json),
         },
         Err(_err_parse_text) => error!("Error while getting update informations. {:?}", _err_parse_text),
     }
