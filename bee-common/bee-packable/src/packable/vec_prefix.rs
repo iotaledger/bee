53ab203987611f3d86069bdbe95e6a6e735fbad5
Adam Gleave <adamg108@hotmail.co.uk>
2021-07-08 15:26:32 +0100

    Add maximum length parameter to `VecPrefix` (#661)
    * Add const length parameter to VecPrefix
    
    * fmt
    
    * Rename error variant
    
    * impl TryFrom<Vec<T>> for VecPrefix
    
    * Add tests
    
    * Fmt
    
    * Merge macros
    
    Co-authored-by: Thibault Martinez <thibault@iota.org>

--- bee-common/bee-packable/src/packable/vec_prefix.rs
+++ bee-common/bee-packable/src/packable/vec_prefix.rs
@@ -14,15 +14,26 @@ use crate::{
 use alloc::vec::Vec;
 use core::{convert::TryFrom, marker::PhantomData};
 
+/// Error encountered when attempting to convert a `Vec<T>` into a `VecPrefix`, where
+/// the length of the source vector exceeds the maximum length of the `VecPrefix`.
+#[derive(Debug, PartialEq, Eq)]
+pub struct PrefixedFromVecError {
+    /// Maximum length of the `VecPrefix`.
+    pub max_len: usize,
+    /// Actual length of the source vector.
+    pub actual_len: usize,
+}
+
 /// Wrapper type for `Vec<T>` where the length prefix is of type `P`.
+/// The `Vec<T>`'s maximum length is provided by `N`.
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[repr(transparent)]
-pub struct VecPrefix<T, P> {
+pub struct VecPrefix<T, P, const N: usize> {
     inner: Vec<T>,
     marker: PhantomData<P>,
 }
 
-impl<T, P> VecPrefix<T, P> {
+impl<T, P, const N: usize> VecPrefix<T, P, N> {
     /// Creates a new empty `VecPrefix<T, P>`.
     pub fn new() -> Self {
         Self {
@@ -40,30 +51,39 @@ impl<T, P> VecPrefix<T, P> {
     }
 }
 
-impl<T, P> Default for VecPrefix<T, P> {
+impl<T, P, const N: usize> Default for VecPrefix<T, P, N> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl<T, P> From<Vec<T>> for VecPrefix<T, P> {
-    fn from(vec: Vec<T>) -> Self {
-        Self {
-            inner: vec,
-            marker: PhantomData,
+impl<T, P, const N: usize> TryFrom<Vec<T>> for VecPrefix<T, P, N> {
+    type Error = PrefixedFromVecError;
+
+    fn try_from(vec: Vec<T>) -> Result<Self, Self::Error> {
+        if vec.len() > N {
+            Err(PrefixedFromVecError {
+                max_len: N,
+                actual_len: vec.len(),
+            })
+        } else {
+            Ok(Self {
+                inner: vec,
+                marker: PhantomData,
+            })
         }
     }
 }
 
 /// We cannot provide a `From` implementation because `Vec` is not from this crate.
 #[allow(clippy::from_over_into)]
-impl<T, P> Into<Vec<T>> for VecPrefix<T, P> {
+impl<T, P, const N: usize> Into<Vec<T>> for VecPrefix<T, P, N> {
     fn into(self) -> Vec<T> {
         self.inner
     }
 }
 
-impl<T, P> core::ops::Deref for VecPrefix<T, P> {
+impl<T, P, const N: usize> core::ops::Deref for VecPrefix<T, P, N> {
     type Target = Vec<T>;
 
     fn deref(&self) -> &Self::Target {
@@ -71,7 +91,7 @@ impl<T, P> core::ops::Deref for VecPrefix<T, P> {
     }
 }
 
-impl<T, P> core::ops::DerefMut for VecPrefix<T, P> {
+impl<T, P, const N: usize> core::ops::DerefMut for VecPrefix<T, P, N> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.inner
     }
@@ -79,13 +99,13 @@ impl<T, P> core::ops::DerefMut for VecPrefix<T, P> {
 
 macro_rules! impl_wrap_for_vec {
     ($ty:ty) => {
-        impl<T> Wrap<VecPrefix<T, $ty>> for Vec<T> {
-            fn wrap(&self) -> &VecPrefix<T, $ty> {
+        impl<T, const N: usize> Wrap<VecPrefix<T, $ty, N>> for Vec<T> {
+            fn wrap(&self) -> &VecPrefix<T, $ty, N> {
                 // SAFETY: `VecPrefix` has a transparent representation and it is composed of one
                 // `Vec` field and an additional zero-sized type field. This means that `VecPrefix`
                 // has the same layout as `Vec` and any valid `Vec` reference can be casted into a
                 // valid `VecPrefix` reference.
-                unsafe { &*(self as *const Vec<T> as *const VecPrefix<T, $ty>) }
+                unsafe { &*(self as *const Vec<T> as *const VecPrefix<T, $ty, N>) }
             }
         }
     };
@@ -100,7 +120,7 @@ impl_wrap_for_vec!(u128);
 
 macro_rules! impl_packable_for_vec_prefix {
     ($ty:ty) => {
-        impl<T: Packable> Packable for VecPrefix<T, $ty> {
+        impl<T: Packable, const N: usize> Packable for VecPrefix<T, $ty, N> {
             type PackError = PackPrefixError<T::PackError, $ty>;
             type UnpackError = UnpackPrefixError<T::UnpackError, $ty>;
 
@@ -125,10 +145,13 @@ macro_rules! impl_packable_for_vec_prefix {
             fn unpack<U: Unpacker>(unpacker: &mut U) -> Result<Self, UnpackError<Self::UnpackError, U::Error>> {
                 // The length of any dynamically-sized sequence must be prefixed.
                 let len = <$ty>::unpack(unpacker).map_err(UnpackError::infallible)?;
+                let len = usize::try_from(len).map_err(|err| UnpackError::Packable(UnpackPrefixError::Prefix(err)))?;
 
-                let mut vec = Self::with_capacity(
-                    usize::try_from(len).map_err(|err| UnpackError::Packable(UnpackPrefixError::Prefix(err)))?,
-                );
+                if len > N {
+                    return Err(UnpackError::Packable(UnpackPrefixError::InvalidPrefixLength(len)));
+                }
+
+                let mut vec = Self::with_capacity(len);
 
                 for _ in 0..len {
                     let item = T::unpack(unpacker).map_err(UnpackError::coerce)?;
