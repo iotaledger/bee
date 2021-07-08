fca04823b26b9eb26f5e834f5f2afe6fda8840ee
Thibault Martinez <thibault@iota.org>
2021-07-08 16:15:07 +0200

    Merge macros

--- bee-common/bee-packable/tests/vec_prefix.rs
+++ bee-common/bee-packable/tests/vec_prefix.rs
@@ -10,9 +10,9 @@ use core::convert::TryFrom;
 const MAX_LENGTH: usize = 128;
 
 macro_rules! impl_packable_test_for_vec_prefix {
-    ($name:ident, $ty:ty) => {
+    ($packable_vec_prefix:ident, $packable_vec_prefix_invalid_length:ident, $ty:ty) => {
         #[test]
-        fn $name() {
+        fn $packable_vec_prefix() {
             assert_eq!(
                 common::generic_test(&VecPrefix::<u32, $ty, MAX_LENGTH>::new())
                     .0
@@ -30,20 +30,9 @@ macro_rules! impl_packable_test_for_vec_prefix {
                     + core::mem::size_of::<u8>()
             );
         }
-    };
-}
 
-impl_packable_test_for_vec_prefix!(packable_vec_prefix_u8, u8);
-impl_packable_test_for_vec_prefix!(packable_vec_prefix_u16, u16);
-impl_packable_test_for_vec_prefix!(packable_vec_prefix_u32, u32);
-impl_packable_test_for_vec_prefix!(packable_vec_prefix_u64, u64);
-#[cfg(has_u128)]
-impl_packable_test_for_vec_prefix!(packable_vec_prefix_u128, u128);
-
-macro_rules! impl_test_for_invalid_prefix_length {
-    ($name:ident, $ty:ty) => {
         #[test]
-        fn $name() {
+        fn $packable_vec_prefix_invalid_length() {
             let mut bytes = vec![0u8; MAX_LENGTH + 2];
             bytes[0] = MAX_LENGTH as u8 + 1;
 
@@ -57,15 +46,15 @@ macro_rules! impl_test_for_invalid_prefix_length {
     };
 }
 
-impl_test_for_invalid_prefix_length!(invalid_prefix_length_u8, u8);
-impl_test_for_invalid_prefix_length!(invalid_prefix_length_u16, u16);
-impl_test_for_invalid_prefix_length!(invalid_prefix_length_u32, u32);
-impl_test_for_invalid_prefix_length!(invalid_prefix_length_u64, u64);
+impl_packable_test_for_vec_prefix!(packable_vec_prefix_u8, packable_vec_prefix_invalid_length_u8, u8);
+impl_packable_test_for_vec_prefix!(packable_vec_prefix_u16, packable_vec_prefix_invalid_length_u16, u16);
+impl_packable_test_for_vec_prefix!(packable_vec_prefix_u32, packable_vec_prefix_invalid_length_u32, u32);
+impl_packable_test_for_vec_prefix!(packable_vec_prefix_u64, packable_vec_prefix_invalid_length_u64, u64);
 #[cfg(has_u128)]
-impl_test_for_invalid_prefix_length!(invalid_prefix_length_u128, u128);
+impl_packable_test_for_vec_prefix!(packable_vec_prefix_u128, packable_vec_prefix_invalid_length_u128, u128);
 
 #[test]
-fn from_vec_error() {
+fn packable_vec_prefix_from_vec_error() {
     let vec = vec![0u8; 16];
     let prefixed = VecPrefix::<u8, u32, 8>::try_from(vec);
 
