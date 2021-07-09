b7bf741c76d1894ec3a945f1c21516f35122f4c2
Matthieu Daumas <matthieu.daumas@iota.org>
2021-07-09 15:07:33 +0200

    Moved user agent header to client builder

--- bee-node/src/plugins/version_checker.rs
+++ bee-node/src/plugins/version_checker.rs
@@ -8,7 +8,7 @@ use bee_runtime::{node::Node, shutdown_stream::ShutdownStream, worker::Worker};
 use async_trait::async_trait;
 use futures::StreamExt;
 use log::{error, info, warn};
-use reqwest::{header::USER_AGENT, Client};
+use reqwest::Client;
 use serde_json::Value;
 use tokio::time::interval;
 use tokio_stream::wrappers::IntervalStream;
@@ -34,14 +34,21 @@ impl<N: Node> Worker<N> for VersionChecker {
                 IntervalStream::new(interval(Duration::from_secs(CHECK_INTERVAL_SEC))),
             );
 
-            let client = reqwest::Client::new();
+            let client = reqwest::Client::builder()
+                .user_agent("curl")
+                .build();
 
-            while ticker.next().await.is_some() {
-                match is_latest(&client).await {
-                    Ok(false) => warn!("A new version has been released. Please update the node at https://github.com/iotaledger/bee/releases"),
-                    Err(e) => error!("error while checking for new update. {:?}", e),
-                    _ => (),
-                }
+            match client {
+                Ok(cl) => {
+                    while ticker.next().await.is_some() {
+                        match is_latest(&cl).await {
+                            Ok(false) => warn!("A new version has been released. Please update the node at https://github.com/iotaledger/bee/releases"),
+                            Err(e) => error!("error while checking for new update. {:?}", e),
+                            _ => (),
+                        }
+                    }
+                },
+                Err(e) => error!("a problem has occured in client building. {:?}", e),
             }
 
             info!("Stopped.");
@@ -52,12 +59,7 @@ impl<N: Node> Worker<N> for VersionChecker {
 }
 
 async fn is_latest(client: &Client) -> Result<bool, reqwest::Error> {
-    let res = client
-        .get(BEE_GIT_API_LATEST)
-        .header(USER_AGENT, "curl")
-        .send()
-        .await?
-        .error_for_status()?;
+    let res = client.get(BEE_GIT_API_LATEST).send().await?.error_for_status()?;
 
     match res.text().await {
         Ok(text) => match serde_json::from_str::<Value>(&text) {
