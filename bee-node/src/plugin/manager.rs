0340c3bfab707e8ec618dbc3399577d6f54281c8
Christian Poveda <christian.poveda@iota.org>
2021-07-16 17:53:04 -0500

    document the plugin system

--- bee-node/src/plugin/manager.rs
+++ bee-node/src/plugin/manager.rs
@@ -7,13 +7,18 @@ use bee_event_bus::EventBus;
 
 use std::{collections::HashMap, error::Error, process::Command, sync::Arc};
 
+/// The bee node plugin manager.
 pub(crate) struct PluginManager {
+    /// Counter to create new and unique `PluginId` values.
     count: usize,
+    /// Handlers for each plugin.
     handlers: HashMap<PluginId, PluginHandler>,
+    /// Reference to the event bus.
     bus: Arc<EventBus<'static, UniqueId>>,
 }
 
 impl PluginManager {
+    /// Creates a new and empty plugin manager.
     pub(crate) fn new(bus: Arc<EventBus<'static, UniqueId>>) -> Self {
         Self {
             count: 0,
@@ -22,19 +27,20 @@ impl PluginManager {
         }
     }
 
+    /// Loads a new plugin from the specified command and returns the `PluginId` for that plugin.
     pub(crate) fn load_plugin(&mut self, mut command: Command) -> Result<PluginId, Box<dyn Error>> {
         let process = command.spawn()?;
 
         // FIXME: do the handshake and retrieve the event IDs.
         let event_ids: Vec<EventId> = vec![];
 
-        let mut handler = PluginHandler::new(process);
+        let id = PluginId(self.count);
+        let mut handler = PluginHandler::new(id, process);
 
         self.count += 1;
-        let id = PluginId(self.count);
 
         for event_id in event_ids {
-            handler.register_callback(id, event_id, &self.bus);
+            handler.register_callback(event_id, &self.bus);
         }
 
         self.handlers.insert(id, handler);
@@ -42,9 +48,10 @@ impl PluginManager {
         Ok(id)
     }
 
+    /// UNloads a plugin with the specified identifier.
     pub(crate) fn unload_plugin(&mut self, id: PluginId) {
         if let Some(handler) = self.handlers.remove(&id) {
-            handler.shutdown(id, &self.bus);
+            handler.shutdown(&self.bus);
         }
     }
 }
