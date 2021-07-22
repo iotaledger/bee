ea98989f19ec2dfe68d319845e4dcbecd143dffa
Christian Poveda <christian.poveda@iota.org>
2021-07-22 09:12:28 -0500

    use `async-trait` directly in example

--- bee-plugin/examples/counter.rs
+++ bee-plugin/examples/counter.rs
@@ -18,7 +18,7 @@ struct Counter {
     inner: Arc<AtomicUsize>,
 }
 
-#[tonic::async_trait]
+#[async_trait::async_trait]
 impl Plugin for Counter {
     fn handshake() -> Vec<EventId> {
         vec![EventId::Dummy]
@@ -45,5 +45,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let counter = Counter {
         inner: Arc::new(0.into()),
     };
-    serve_plugin(counter).await
+
+    serve_plugin(counter).await?;
+
+    Ok(())
 }
