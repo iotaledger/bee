eac4bb6061696cbf32a9dac515ad14cec7235fec
Christian Poveda <christian.poveda@iota.org>
2021-07-22 09:38:34 -0500

    use grpc streams to send events

--- bee-plugin/src/streamer.rs
+++ bee-plugin/src/streamer.rs
@@ -1,43 +1,38 @@
 // Copyright 2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::{
-    error::PluginError,
-    grpc::{plugin_client::PluginClient, DummyEvent},
-};
+use crate::grpc::{plugin_client::PluginClient, DummyEvent};
 
 use tokio::{
     select,
     sync::{mpsc::UnboundedReceiver, oneshot::Receiver},
 };
+
+use tokio_stream::wrappers::UnboundedReceiverStream;
+
 use tonic::transport::Channel;
 
 pub(crate) struct PluginStreamer<T> {
-    rx: UnboundedReceiver<T>,
+    stream: UnboundedReceiverStream<T>,
     shutdown: Receiver<()>,
     client: PluginClient<Channel>,
 }
 
 impl<T> PluginStreamer<T> {
     pub(crate) fn new(rx: UnboundedReceiver<T>, shutdown: Receiver<()>, client: PluginClient<Channel>) -> Self {
-        Self { rx, shutdown, client }
+        Self {
+            stream: UnboundedReceiverStream::new(rx),
+            shutdown,
+            client,
+        }
     }
 }
 
 impl PluginStreamer<DummyEvent> {
-    pub(crate) async fn run(&mut self) {
-        loop {
-            select! {
-                _ = &mut self.shutdown => break,
-                event = self.rx.recv() => match event {
-                    Some(event) => {
-                        if let Err(err) = self.client.process_dummy_event(event).await {
-                            eprintln!("{}", PluginError::from(err))
-                        }
-                    },
-                    None => break,
-                }
-            }
+    pub(crate) async fn run(mut self) {
+        select! {
+            _ = &mut self.shutdown => (),
+            _ = self.client.process_dummy_event(self.stream) => (),
         }
     }
 }
