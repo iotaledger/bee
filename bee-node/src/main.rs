e8439dd2a25f6107218a62c6c4541172ca19ddc2
Christian Poveda <christian.poveda@iota.org>
2021-07-29 11:28:25 -0500

    move `bee-node` example to examples

--- bee-node/src/main.rs
+++ bee-node/src/main.rs
@@ -1,21 +1,12 @@
 // Copyright 2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use bee_event_bus::EventBus;
 use bee_logger::logger_init;
 use bee_node::{
     banner::print_logo_and_version,
     cli::NodeCliArgs,
     config::{NodeConfigBuilder, DEFAULT_NODE_CONFIG_PATH},
 };
-use bee_plugin::{
-    event::{DummyEvent, SillyEvent},
-    hotloading::Hotloader,
-    UniqueId,
-};
-use tokio::time::{sleep, Duration};
-
-use std::sync::Arc;
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -35,31 +26,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     if cli.version() {
         return Ok(());
     }
-
-    let event_bus = Arc::new(EventBus::<UniqueId>::new());
-
-    let hotloader = Hotloader::new("./plugins", Arc::clone(&event_bus));
-
-    let handle = tokio::spawn(async move { hotloader.run().await });
-
-    {
-        let event_bus = Arc::clone(&event_bus);
-        tokio::spawn(async move {
-            loop {
-                sleep(Duration::from_millis(1)).await;
-                event_bus.dispatch(DummyEvent {})
-            }
-        });
-    }
-
-    tokio::spawn(async move {
-        for _ in 0..1000 {
-            sleep(Duration::from_millis(1)).await;
-            event_bus.dispatch(SillyEvent {})
-        }
-    });
-
-    handle.await??;
 
     Ok(())
 }
---
+++ bee-plugin/examples/counter_node.rs
@@ -0,0 +1,51 @@
+// Copyright 2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use bee_event_bus::EventBus;
+use bee_logger::{logger_init, LoggerConfig, LoggerOutputConfigBuilder};
+use bee_plugin::{
+    event::{DummyEvent, SillyEvent},
+    hotloading::Hotloader,
+    UniqueId,
+};
+
+use tokio::time::{sleep, Duration};
+
+use std::{io::ErrorKind, sync::Arc};
+
+#[tokio::main(flavor = "multi_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    logger_init(LoggerConfig::build().output(LoggerOutputConfigBuilder::new()).finish())?;
+
+    match tokio::fs::create_dir("./plugins").await {
+        Err(err) if err.kind() != ErrorKind::AlreadyExists => return Err(err.into()),
+        _ => (),
+    }
+
+    let event_bus = Arc::new(EventBus::<UniqueId>::new());
+
+    let hotloader = Hotloader::new("./plugins", Arc::clone(&event_bus));
+
+    let handle = tokio::spawn(async move { hotloader.run().await });
+
+    {
+        let event_bus = Arc::clone(&event_bus);
+        tokio::spawn(async move {
+            loop {
+                sleep(Duration::from_millis(1)).await;
+                event_bus.dispatch(DummyEvent {})
+            }
+        });
+    }
+
+    tokio::spawn(async move {
+        loop {
+            sleep(Duration::from_millis(1)).await;
+            event_bus.dispatch(SillyEvent {})
+        }
+    });
+
+    handle.await??;
+
+    Ok(())
+}
