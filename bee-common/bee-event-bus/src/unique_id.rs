d14295a08d62c3a721c52dc8b6e3b1bf92b81e60
Thibault Martinez <thibault@iota.org>
2021-08-12 17:14:48 +0200

    Split EventBus and UniqueId + tests (#697)
    * Split EventBus and UniqueId + tests
    
    * Nit
    
    * Nits
    
    * Clippy
    
    * Fix docs

---
+++ bee-common/bee-event-bus/src/unique_id.rs
@@ -0,0 +1,19 @@
+// Copyright 2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use std::any::TypeId;
+
+/// A unique identifier for [`EventBus`](crate::EventBus) callbacks.
+#[derive(Clone, Debug, Copy, PartialEq, Eq)]
+pub enum UniqueId<T> {
+    /// Identifier for types.
+    Type(TypeId),
+    /// Identifier for objects.
+    Object(T),
+}
+
+impl<T> From<TypeId> for UniqueId<T> {
+    fn from(id: TypeId) -> Self {
+        Self::Type(id)
+    }
+}
