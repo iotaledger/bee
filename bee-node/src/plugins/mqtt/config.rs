7bb7e93b3ecfad9b9b16f49f7706e8afed8d226c
rufsam <samuel.rufinatscha@iota.org>
2021-08-18 17:30:33 +0200

    add console settings to config.rs

--- bee-node/src/plugins/mqtt/config.rs
+++ bee-node/src/plugins/mqtt/config.rs
@@ -10,7 +10,8 @@ pub(crate) const DEFAULT_BIND_ADDR: &str = "/ip4/0.0.0.0/tcp/1883";
 
 #[derive(Default, Deserialize)]
 pub struct MqttConfigBuilder {
-    bind_addr: Option<Multiaddr>,
+    server_bind_addr: Option<Multiaddr>,
+    console_bind_addr: Option<Multiaddr>,
 }
 
 impl MqttConfigBuilder {
@@ -19,11 +20,22 @@ impl MqttConfigBuilder {
         Self::default()
     }
 
-    /// Sets the binding address for the MQTT plugin.
-    pub fn bind_addr(mut self, addr: &str) -> Self {
+    /// Sets the binding address for the MQTT server.
+    pub fn server_bind_addr(mut self, addr: &str) -> Self {
         match addr.parse() {
             Ok(addr) => {
-                self.bind_addr.replace(addr);
+                self.server_bind_addr.replace(addr);
+            }
+            Err(e) => panic!("Error parsing bind address: {:?}", e),
+        }
+        self
+    }
+
+    /// Sets the binding address for the MQTT console.
+    pub fn console_bind_addr(mut self, addr: &str) -> Self {
+        match addr.parse() {
+            Ok(addr) => {
+                self.console_bind_addr.replace(addr);
             }
             Err(e) => panic!("Error parsing bind address: {:?}", e),
         }
@@ -31,41 +43,81 @@ impl MqttConfigBuilder {
     }
 
     pub fn finish(self) -> MqttConfig {
-        let multi_addr = self
-            .bind_addr
-            // We made sure that the default value is valid and therefore parseable.
-            .unwrap_or_else(|| DEFAULT_BIND_ADDR.parse().unwrap());
-        let address = multi_addr
-            .iter()
-            .find_map(|x| match x {
-                Protocol::Dns(address) => Some(
-                    (address.to_string(), 0)
-                        .to_socket_addrs()
-                        .unwrap_or_else(|error| panic!("error resolving '{}':{}", address, error))
-                        .next()
-                        // Unwrapping here is fine, because to_socket-addrs() didn't return an error,
-                        // thus we can be sure that the iterator contains at least 1 element.
-                        .unwrap()
-                        .ip(),
-                ),
-                Protocol::Ip4(ip) => Some(IpAddr::V4(ip)),
-                Protocol::Ip6(ip) => Some(IpAddr::V6(ip)),
-                _ => None,
-            })
-            .expect("Unsupported address");
+        // Get server address and port.
+        let (server_addr, server_port) = {
+            let multi_addr = self
+                .server_bind_addr
+                // We made sure that the default value is valid and therefore parseable.
+                .unwrap_or_else(|| DEFAULT_BIND_ADDR.parse().unwrap());
+            let address = multi_addr
+                .iter()
+                .find_map(|x| match x {
+                    Protocol::Dns(address) => Some(
+                        (address.to_string(), 0)
+                            .to_socket_addrs()
+                            .unwrap_or_else(|error| panic!("error resolving '{}':{}", address, error))
+                            .next()
+                            // Unwrapping here is fine, because to_socket-addrs() didn't return an error,
+                            // thus we can be sure that the iterator contains at least 1 element.
+                            .unwrap()
+                            .ip(),
+                    ),
+                    Protocol::Ip4(ip) => Some(IpAddr::V4(ip)),
+                    Protocol::Ip6(ip) => Some(IpAddr::V6(ip)),
+                    _ => None,
+                })
+                .expect("Unsupported address");
 
-        let port = multi_addr
-            .iter()
-            .find_map(|x| if let Protocol::Tcp(port) = x { Some(port) } else { None })
-            .unwrap_or_else(|| panic!("Unsupported protocol"));
+            let port = multi_addr
+                .iter()
+                .find_map(|x| if let Protocol::Tcp(port) = x { Some(port) } else { None })
+                .unwrap_or_else(|| panic!("Unsupported protocol"));
+
+            (address, port)
+        };
+
+        // Get console address and port.
+        let (console_addr, console_port) = {
+            let multi_addr = self
+                .console_bind_addr
+                // We made sure that the default value is valid and therefore parseable.
+                .unwrap_or_else(|| DEFAULT_BIND_ADDR.parse().unwrap());
+            let address = multi_addr
+                .iter()
+                .find_map(|x| match x {
+                    Protocol::Dns(address) => Some(
+                        (address.to_string(), 0)
+                            .to_socket_addrs()
+                            .unwrap_or_else(|error| panic!("error resolving '{}':{}", address, error))
+                            .next()
+                            // Unwrapping here is fine, because to_socket-addrs() didn't return an error,
+                            // thus we can be sure that the iterator contains at least 1 element.
+                            .unwrap()
+                            .ip(),
+                    ),
+                    Protocol::Ip4(ip) => Some(IpAddr::V4(ip)),
+                    Protocol::Ip6(ip) => Some(IpAddr::V6(ip)),
+                    _ => None,
+                })
+                .expect("Unsupported address");
+
+            let port = multi_addr
+                .iter()
+                .find_map(|x| if let Protocol::Tcp(port) = x { Some(port) } else { None })
+                .unwrap_or_else(|| panic!("Unsupported protocol"));
+
+            (address, port)
+        };
 
         MqttConfig {
-            bind_addr: SocketAddr::new(address, port),
+            server_bind_addr: SocketAddr::new(server_addr, server_port),
+            console_bind_addr: SocketAddr::new(console_addr, console_port),
         }
     }
 }
 
 #[derive(Clone)]
 pub struct MqttConfig {
-    pub(crate) bind_addr: SocketAddr,
+    pub(crate) server_bind_addr: SocketAddr,
+    pub(crate) console_bind_addr: SocketAddr,
 }
