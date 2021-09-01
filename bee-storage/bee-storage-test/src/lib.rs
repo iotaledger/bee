d3f1474e1f95dfd9b65a029778ef5e3c41cee448
Thibault Martinez <thibault@iota.org>
2021-09-01 18:36:08 +0200

    Bring back storage tests

--- bee-storage/bee-storage-test/src/lib.rs
+++ bee-storage/bee-storage-test/src/lib.rs
@@ -1,22 +1,6 @@
 // Copyright 2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-mod address_to_balance;
-mod ed25519_address_to_output_id;
-mod index_to_message_id;
-mod ledger_index;
 mod message_id_to_message;
-mod message_id_to_message_id;
-mod message_id_to_metadata;
-mod milestone_index_to_milestone;
-mod milestone_index_to_output_diff;
-mod milestone_index_to_receipt;
-mod milestone_index_to_unreferenced_message;
-mod output_id_to_consumed_output;
-mod output_id_to_created_output;
-mod output_id_unspent;
-mod snapshot_info;
-mod solid_entry_point_to_milestone_index;
-mod spent_to_treasury_output;
 
 pub use message_id_to_message::message_id_to_message_access;
