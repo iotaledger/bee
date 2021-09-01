a10630d4fef4f26cbf6c64185f45d84b22d9ce36
Thibault Martinez <thibault@iota.org>
2021-09-01 18:23:34 +0200

    Some nits

--- bee-storage/bee-storage-rocksdb/src/column_families.rs
+++ bee-storage/bee-storage-rocksdb/src/column_families.rs
@@ -1,6 +1,12 @@
 // Copyright 2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+//! Identifiers for each column family.
+//!
+//! Rocksdb allows creating new, isolated keyspaces by adding new column families to the database.
+//! Each column family can be accessed using the [`cf_handle`](https://docs.rs/rocksdb/latest/rocksdb/struct.DBWithThreadMode.html#method.cf_handle)
+//! method with one of the identifiers found here.
+
 /// Identifier for the `System` column family.
 pub const CF_SYSTEM: &str = "system";
 /// Identifier for the `MessageId` to `Message` column family.
--- bee-storage/bee-storage/src/system/version.rs
+++ bee-storage/bee-storage/src/system/version.rs
@@ -1,30 +1,8 @@
 // Copyright 2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use core::convert::Infallible;
-
-use bee_packable::{
-    error::{PackError, UnpackError},
-    packable::{Packable, Packer, Unpacker},
-};
+use bee_packable::packable::Packable;
 
 /// Version of the storage.
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Packable)]
 pub struct StorageVersion(pub u64);
-
-impl Packable for StorageVersion {
-    type PackError = Infallible;
-    type UnpackError = Infallible;
-
-    fn packed_len(&self) -> usize {
-        self.0.packed_len()
-    }
-
-    fn pack<P: Packer>(&self, packer: &mut P) -> Result<(), PackError<Self::PackError, P::Error>> {
-        self.0.pack(packer)
-    }
-
-    fn unpack<U: Unpacker>(unpacker: &mut U) -> Result<Self, UnpackError<Self::UnpackError, U::Error>> {
-        Ok(Self(u64::unpack(unpacker)?))
-    }
-}
