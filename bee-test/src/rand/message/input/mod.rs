10197177ba7e9736d19db6e3a4bf06a2d11fdf8d
Thibault Martinez <thibault@iota.org>
2021-09-06 11:32:04 +0200

    Proper input module

---
+++ bee-test/src/rand/message/input/mod.rs
@@ -0,0 +1,18 @@
+// Copyright 2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+mod utxo;
+
+use crate::rand::number::rand_number;
+
+pub use utxo::rand_utxo_input;
+
+use bee_message::input::{Input, UtxoInput};
+
+/// Generates a random [`Input`].
+pub fn rand_input() -> Input {
+    match rand_number::<u8>() % 1 {
+        UtxoInput::KIND => rand_utxo_input().into(),
+        _ => unreachable!(),
+    }
+}
