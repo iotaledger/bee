0c2536e5686a0352bb2f3cd8951b9e19a46572f8
Thibault Martinez <thibault@iota.org>
2021-09-07 11:41:52 +0200

    Fix rand_parents_block and add rand_parents_blocks

--- bee-test/src/rand/message/parents.rs
+++ bee-test/src/rand/message/parents.rs
@@ -1,7 +1,11 @@
 // Copyright 2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::rand::{message::rand_message_id, number::rand_number_range, vec::rand_vec};
+use crate::rand::{
+    message::rand_message_id,
+    number::{rand_number, rand_number_range},
+    vec::rand_vec,
+};
 
 use bee_message::parents::{ParentsBlock, ParentsKind, PREFIXED_PARENTS_LENGTH_MAX, PREFIXED_PARENTS_LENGTH_MIN};
 
@@ -14,12 +18,19 @@ pub fn rand_parents_block(block_type: ParentsKind) -> ParentsBlock {
 
     parent_ids.sort();
 
-    ParentsBlock::new(
-        block_type,
-        rand_vec(
-            rand_message_id,
-            rand_number_range(PREFIXED_PARENTS_LENGTH_MIN as usize..=PREFIXED_PARENTS_LENGTH_MAX as usize),
-        ),
-    )
-    .unwrap()
+    ParentsBlock::new(block_type, parent_ids).unwrap()
+}
+
+/// Generates a random [`Vec`] of [`ParentsBlock`]s.
+pub fn rand_parents_blocks() -> Vec<ParentsBlock> {
+    let mut parents = Vec::new();
+    let parents_kinds = [ParentsKind::Weak, ParentsKind::Disliked, ParentsKind::Liked];
+
+    parents.push(rand_parents_block(ParentsKind::Strong));
+
+    for i in 0..rand_number::<usize>() % 3 {
+        parents.push(rand_parents_block(parents_kinds[i]))
+    }
+
+    parents
 }
