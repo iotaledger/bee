8164c8c5abbbea0312049e13fbc9685edeff11cd
Thibault Martinez <thibault@iota.org>
2021-09-08 11:31:28 +0200

    Test errors consistency

--- bee-message/tests/unlock_blocks.rs
+++ bee-message/tests/unlock_blocks.rs
@@ -92,33 +92,32 @@ fn new_invalid_too_many_blocks() {
 
 #[test]
 fn new_valid() {
-    assert!(
-        UnlockBlocks::new(vec![
-            SignatureUnlock::from(Signature::from(Ed25519Signature::new([0; 32], [0; 64]))).into(),
-            ReferenceUnlock::new(0).unwrap().into(),
-            ReferenceUnlock::new(0).unwrap().into(),
-            SignatureUnlock::from(Signature::from(Ed25519Signature::new([1; 32], [1; 64]))).into(),
-            SignatureUnlock::from(Signature::from(Ed25519Signature::new([2; 32], [2; 64]))).into(),
-            SignatureUnlock::from(Signature::from(Ed25519Signature::new([3; 32], [3; 64]))).into(),
-            ReferenceUnlock::new(3).unwrap().into(),
-            ReferenceUnlock::new(4).unwrap().into(),
-            ReferenceUnlock::new(3).unwrap().into(),
-            ReferenceUnlock::new(4).unwrap().into(),
-            ReferenceUnlock::new(5).unwrap().into(),
-            SignatureUnlock::from(Signature::from(Ed25519Signature::new([4; 32], [4; 64]))).into(),
-            ReferenceUnlock::new(11).unwrap().into(),
-            SignatureUnlock::from(Signature::from(Ed25519Signature::new([5; 32], [5; 64]))).into(),
-        ])
-        .is_ok()
-    );
+    assert!(UnlockBlocks::new(vec![
+        SignatureUnlock::from(Signature::from(Ed25519Signature::new([0; 32], [0; 64]))).into(),
+        ReferenceUnlock::new(0).unwrap().into(),
+        ReferenceUnlock::new(0).unwrap().into(),
+        SignatureUnlock::from(Signature::from(Ed25519Signature::new([1; 32], [1; 64]))).into(),
+        SignatureUnlock::from(Signature::from(Ed25519Signature::new([2; 32], [2; 64]))).into(),
+        SignatureUnlock::from(Signature::from(Ed25519Signature::new([3; 32], [3; 64]))).into(),
+        ReferenceUnlock::new(3).unwrap().into(),
+        ReferenceUnlock::new(4).unwrap().into(),
+        ReferenceUnlock::new(3).unwrap().into(),
+        ReferenceUnlock::new(4).unwrap().into(),
+        ReferenceUnlock::new(5).unwrap().into(),
+        SignatureUnlock::from(Signature::from(Ed25519Signature::new([4; 32], [4; 64]))).into(),
+        ReferenceUnlock::new(11).unwrap().into(),
+        SignatureUnlock::from(Signature::from(Ed25519Signature::new([5; 32], [5; 64]))).into(),
+    ])
+    .is_ok());
 }
 
 #[test]
 fn get_none() {
     assert!(
-        UnlockBlocks::new(vec![
-            SignatureUnlock::from(Signature::from(Ed25519Signature::new([0; 32], [0; 64]))).into(),
-        ])
+        UnlockBlocks::new(vec![SignatureUnlock::from(Signature::from(Ed25519Signature::new(
+            [0; 32], [0; 64]
+        )))
+        .into(),])
         .unwrap()
         .get(42)
         .is_none()
@@ -161,8 +160,10 @@ fn unpack_invalid_unlock_block_kind() {
     let unlock_blocks = UnlockBlocks::unpack_from_slice(bytes);
 
     assert!(matches!(
-        unlock_blocks.err().unwrap(),
-        UnpackError::Packable(MessageUnpackError::UnlockBlock(UnlockBlockUnpackError::InvalidKind(2))),
+        unlock_blocks,
+        Err(UnpackError::Packable(MessageUnpackError::UnlockBlock(
+            UnlockBlockUnpackError::InvalidKind(2)
+        ))),
     ));
 }
 
