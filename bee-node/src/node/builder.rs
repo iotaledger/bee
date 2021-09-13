557ddf8a0f79cd9358ee4aa2d55648354b300b9a
Thibault Martinez <thibault@iota.org>
2021-09-13 17:20:15 +0200

    Don't corrupt the database if the private key was missing from the config file

--- bee-node/src/node/builder.rs
+++ bee-node/src/node/builder.rs
@@ -17,6 +17,7 @@ use bee_runtime::{
     node::{Node, NodeBuilder, NodeInfo},
     worker::Worker,
 };
+use bee_storage::system::StorageHealth;
 
 use anymap::Map;
 use async_trait::async_trait;
@@ -118,16 +119,6 @@ impl<B: StorageBackend> NodeBuilder<BeeNode<B>> for BeeNodeBuilder<B> {
     type Config = NodeConfig<B>;
 
     fn new(config: Self::Config) -> Result<Self, Self::Error> {
-        let version = if BEE_GIT_COMMIT.is_empty() {
-            BEE_VERSION.to_owned()
-        } else {
-            BEE_VERSION.to_owned() + "-" + &BEE_GIT_COMMIT[0..7]
-        };
-        let node_info = NodeInfo {
-            name: "Bee".to_owned(),
-            version,
-        };
-
         Ok(Self {
             deps: HashMap::default(),
             worker_starts: HashMap::default(),
@@ -135,11 +126,7 @@ impl<B: StorageBackend> NodeBuilder<BeeNode<B>> for BeeNodeBuilder<B> {
             worker_names: HashMap::default(),
             resource_registers: Vec::default(),
             config: config.clone(),
-        }
-        .with_resource(node_info)
-        // TODO block ? Make new async ?
-        .with_resource((B::start(config.storage)).map_err(|e| Error::StorageBackend(Box::new(e)))?)
-        .with_resource(Bus::<TypeId>::default()))
+        })
     }
 
     fn with_worker<W: Worker<BeeNode<B>> + 'static>(self) -> Self
@@ -186,23 +173,40 @@ impl<B: StorageBackend> NodeBuilder<BeeNode<B>> for BeeNodeBuilder<B> {
     }
 
     async fn finish(mut self) -> Result<BeeNode<B>, Error> {
-        if self.config.identity.2 {
-            return Err(Error::InvalidOrNoIdentityPrivateKey(self.config.identity.1));
+        let config = self.config.clone();
+        // TODO block ? Make new async ?
+        let storage = B::start(config.storage.clone()).map_err(|e| Error::StorageBackend(Box::new(e)))?;
+
+        let node_info = NodeInfo {
+            name: "Bee".to_owned(),
+            version: if BEE_GIT_COMMIT.is_empty() {
+                BEE_VERSION.to_owned()
+            } else {
+                BEE_VERSION.to_owned() + "-" + &BEE_GIT_COMMIT[0..7]
+            },
+        };
+
+        if config.identity.2 {
+            storage
+                .set_health(StorageHealth::Healthy)
+                .map_err(|e| Error::StorageBackend(Box::new(e)))?;
+            return Err(Error::InvalidOrNoIdentityPrivateKey(config.identity.1));
         }
+
+        let this = self
+            .with_resource(config.clone())
+            .with_resource(node_info)
+            .with_resource(storage)
+            .with_resource(Bus::<TypeId>::default());
 
         info!(
             "Joining network \"{}\"({}). Bech32 hrp \"{}\".",
-            self.config.network_id.0, self.config.network_id.1, self.config.bech32_hrp
+            config.network_id.0, config.network_id.1, config.bech32_hrp
         );
-
-        let config = self.config.clone();
 
         let network_config = config.network.clone();
         let network_id = config.network_id.1;
-
         let local_keys = config.identity.0.clone();
-
-        let this = self.with_resource(config.clone()); // TODO: Remove clone
 
         info!("Initializing network layer...");
         let (this, events) = bee_network::integrated::init::<BeeNode<B>>(network_config, local_keys, network_id, this)
