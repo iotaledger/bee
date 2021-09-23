36a407de25cb37229ce1ec04fbcb754481f7139b
Alexander Schmidt <alexander.schmidt@iota.org>
2021-09-23 12:44:53 +0200

    Add link to backstage

--- bee-network/src/backstage/mod.rs
+++ bee-network/src/backstage/mod.rs
@@ -1,7 +1,7 @@
 // Copyright 2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-//! A bee network layer API using backstage.
+//! A bee network layer API using [backstage](https://github.com/iotaledger/backstage.git).
 
 mod network;
 mod peer;
