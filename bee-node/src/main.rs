a13071d28f0b7b00255e4d3030c6ff5cdb5c9c47
Thibault Martinez <thibault@iota.org>
2021-09-27 15:38:47 +0200

    Fix console compilation

--- bee-node/src/main.rs
+++ bee-node/src/main.rs
@@ -5,6 +5,8 @@
 
 use bee_node::{plugins, print_banner_and_version, tools, CliArgs, NodeBuilder, NodeConfigBuilder};
 use bee_runtime::node::NodeBuilder as _;
+#[cfg(feature = "console")]
+use bee_runtime::task::{StandaloneSpawner, TaskSpawner};
 #[cfg(feature = "rocksdb")]
 use bee_storage_rocksdb::storage::Storage as RocksDb;
 #[cfg(all(feature = "sled", not(feature = "rocksdb")))]
