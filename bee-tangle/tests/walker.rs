6b50e20ddd480b4781cf781037bea3487af722e6
Christian Poveda <christian.poveda@iota.org>
2021-10-01 11:34:06 -0500

    add `tangle!` macro

--- bee-tangle/tests/walker.rs
+++ bee-tangle/tests/walker.rs
@@ -22,39 +22,35 @@ fn new_node(tangle: &Tangle, parents_ids: Vec<MessageId>) -> (Message, MessageMe
 }
 
 struct TangleBuilder {
-    nodes: Vec<usize>,
-    parents: HashMap<usize, Vec<usize>>,
+    graph: HashMap<usize, Vec<usize>>,
 }
 
 impl TangleBuilder {
     fn new() -> Self {
-        Self {
-            nodes: vec![],
-            parents: HashMap::new(),
-        }
+        Self { graph: HashMap::new() }
     }
 
     fn add_node<const M: usize>(&mut self, node: usize, parents: [usize; M]) -> &mut Self {
-        if let Err(i) = self.nodes.binary_search(&node) {
-            self.nodes.insert(i, node);
+        // Get the parents of the node or insert an empty list of parents if the node was not in
+        // the graph.
+        let existing_parents = self.graph.entry(node).or_default();
+
+        for parent in &parents {
+            // Check if the parent is not in the list of parents of the node and insert it.
+            if let Err(i) = existing_parents.binary_search(parent) {
+                existing_parents.insert(i, *parent);
+            }
         }
 
         for parent in parents {
-            if let Err(i) = self.nodes.binary_search(&parent) {
-                self.nodes.insert(i, parent);
-            }
-
-            let existing_parents = self.parents.entry(node).or_default();
-
-            if let Err(i) = existing_parents.binary_search(&parent) {
-                existing_parents.insert(i, parent);
-            }
+            // Insert the parent in the graph.
+            self.graph.entry(parent).or_default();
         }
 
         self
     }
 
-    fn build(&mut self) -> (Tangle, HashMap<usize, MessageId>) {
+    fn build(mut self) -> (Tangle, HashMap<usize, MessageId>) {
         // Check that the graph is a DAG and find a topological order so we can add messages to the
         // tangle in the correct order (parents before children). This `Vec` will hold the nodes in
         // such order.
@@ -68,47 +64,52 @@ impl TangleBuilder {
             parents: &HashMap<usize, Vec<usize>>,
             ordered_nodes: &mut Vec<usize>,
         ) {
+            // If the node is permanently marked as visited we skip it.
             if perms.binary_search(&node).is_ok() {
                 return;
             }
 
-            match temps.binary_search(&node) {
-                Ok(_) => panic!("not a DAG"),
-                Err(i) => temps.insert(i, node),
+            // If the node is temporarily marked as visited we have a cycle.
+            let i = temps.binary_search(&node).map(|i| temps[i]).expect_err("Found cycle");
+            // Mark the node as temporarily visited.
+            temps.insert(i, node);
+
+            // Visit each parent of the node.
+            for &edge in &parents[&node] {
+                visit(edge, perms, temps, parents, ordered_nodes);
             }
 
-            if let Some(edges) = parents.get(&node) {
-                for &edge in edges {
-                    visit(edge, perms, temps, parents, ordered_nodes);
-                }
-            }
+            // Remove the temporary mark for the node.
+            let j = temps
+                .binary_search(&node)
+                .map_err(|j| temps[j])
+                .expect("A temporarily marked node cannot be unmarked while visiting oter node");
+            temps.remove(j);
 
-            match temps.binary_search(&node) {
-                Ok(i) => {
-                    temps.remove(i);
-                    match perms.binary_search(&node) {
-                        Ok(_) => unreachable!(),
-                        Err(i) => perms.insert(i, node),
-                    }
-                }
-                Err(_) => unreachable!(),
-            }
+            // Mark the node as permanently visited.
+            let k = perms
+                .binary_search(&node)
+                .map(|k| perms[k])
+                .expect_err("A temporarily marked node cannot be permanently marked");
+            perms.insert(k, node);
 
+            // Insert the node at the beginning of the list.
             ordered_nodes.insert(0, node);
         }
 
         let mut perms = vec![];
         let mut temps = vec![];
+        let unmarked = self.graph.keys().copied().collect::<Vec<_>>();
 
-        for &node in self.nodes.iter() {
-            visit(node, &mut perms, &mut temps, &mut self.parents, &mut ordered_nodes);
+        for node in unmarked {
+            visit(node, &mut perms, &mut temps, &mut self.graph, &mut ordered_nodes);
         }
 
         let tangle = Tangle::new();
         let mut ids = HashMap::new();
 
         while let Some(node) = ordered_nodes.pop() {
-            let (_msg, _meta, id) = match self.parents.get(&node) {
+            let (_msg, _meta, id) = match self.graph.get(&node) {
                 Some(parents) if !parents.is_empty() => {
                     let parents = parents.iter().map(|node| ids[node]).collect::<Vec<MessageId>>();
                     new_node(&tangle, parents)
@@ -120,6 +121,26 @@ impl TangleBuilder {
         }
 
         (tangle, ids)
+    }
+}
+
+macro_rules! build_tangle {
+    ($builder:expr, $node:expr => $($parent:expr),* ; $($tail:tt)*) => {
+        $builder.add_node($node, [$($parent),*]);
+        build_tangle!{$builder, $($tail)*}
+    };
+
+    ($builder:expr,) => {
+        $builder.build()
+    }
+}
+
+macro_rules! tangle {
+    ($($tail:tt)*) => {
+        {
+            let mut builder = TangleBuilder::new();
+            build_tangle!{builder, $($tail)*}
+        }
     }
 }
 
@@ -141,41 +162,19 @@ fn walk() {
     //     | -- 11 --
     // 7 --
 
-    let (tangle, ids) = TangleBuilder::new()
-        .add_node(0, [])
-        .add_node(1, [])
-        .add_node(2, [])
-        .add_node(3, [])
-        .add_node(4, [])
-        .add_node(5, [])
-        .add_node(6, [])
-        .add_node(7, [])
-        .add_node(8, [0, 1])
-        .add_node(9, [2, 3])
-        .add_node(10, [4, 5])
-        .add_node(11, [6, 7])
-        .add_node(12, [8, 9])
-        .add_node(13, [10, 11])
-        .add_node(14, [12, 13])
-        .build();
+    let (tangle, ids) = tangle! {
+        8 => 0, 1;
+        9 => 2, 3;
+        10 => 4, 5;
+        11 => 6, 7;
+        12 => 8, 9;
+        13 => 10, 11;
+        14 => 12, 13;
+    };
 
-    let correct_order = vec![
-        ids[&14],
-        ids[&12],
-        ids[&8],
-        ids[&0],
-        ids[&1],
-        ids[&9],
-        ids[&2],
-        ids[&3],
-        ids[&13],
-        ids[&10],
-        ids[&4],
-        ids[&5],
-        ids[&11],
-        ids[&6],
-        ids[&7],
-    ];
+    let correct_order: Vec<MessageId> = IntoIterator::into_iter([14, 12, 8, 0, 1, 9, 2, 3, 13, 10, 4, 5, 11, 6, 7])
+        .map(|node| ids[&node])
+        .collect();
 
     let mut traversed_order = Vec::with_capacity(correct_order.len());
 
@@ -189,5 +188,5 @@ fn walk() {
         }
     }
 
-    // assert_eq!(correct_order, traversed_order);
+    assert_eq!(correct_order, traversed_order);
 }
