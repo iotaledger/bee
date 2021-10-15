280072550b3dcec55c1505168eaf52d886a9ac67
Thibault Martinez <thibault@iota.org>
2021-10-15 11:11:53 +0200

    Config nits

--- bee-tangle/src/config.rs
+++ bee-tangle/src/config.rs
@@ -1,12 +1,13 @@
 // Copyright 2020-2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use std::num::NonZeroUsize;
-
 use serde::Deserialize;
 
+use std::num::NonZeroUsize;
+
 const DEFAULT_BELOW_MAX_DEPTH: u32 = 15;
-const DEFAULT_NUM_PARTITIONS: Option<NonZeroUsize> = NonZeroUsize::new(100);
+// SAFETY: initialised with a non-zero value.
+const DEFAULT_NUM_PARTITIONS: NonZeroUsize = unsafe { NonZeroUsize::new_unchecked(16) };
 
 /// A builder type for a tangle configuration.
 #[derive(Default, Deserialize)]
@@ -16,16 +17,16 @@ pub struct TangleConfigBuilder {
 }
 
 impl TangleConfigBuilder {
-    /// Create a new `TangleConfigBuilder`.
+    /// Create a new [`TangleConfigBuilder`].
     pub fn new() -> Self {
         Self::default()
     }
 
-    /// Finish building tangle configuration, to create a `TangleConfig`.
+    /// Finish building tangle configuration, to create a [`TangleConfig`].
     pub fn finish(self) -> TangleConfig {
         TangleConfig {
             below_max_depth: self.below_max_depth.unwrap_or(DEFAULT_BELOW_MAX_DEPTH),
-            num_partitions: self.num_partitions.or(DEFAULT_NUM_PARTITIONS).unwrap(),
+            num_partitions: self.num_partitions.unwrap_or(DEFAULT_NUM_PARTITIONS),
         }
     }
 }
@@ -38,7 +39,7 @@ pub struct TangleConfig {
 }
 
 impl TangleConfig {
-    /// Begin building a new `TangleConfig`.
+    /// Begin building a new [`TangleConfig`].
     pub fn build() -> TangleConfigBuilder {
         TangleConfigBuilder::new()
     }
