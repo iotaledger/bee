70b2338d1003d0182ad21b4c87fea9e06c582c99
Thibault Martinez <thibault@iota.org>
2021-10-19 12:24:54 +0200

    Impl remaining feature blocks

--- bee-message/src/output/feature_block/indexation.rs
+++ bee-message/src/output/feature_block/indexation.rs
@@ -8,15 +8,26 @@ use bee_common::packable::{Packable, Read, Write};
 ///
 #[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
 #[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
-pub struct IndexationFeatureBlock {}
+pub struct IndexationFeatureBlock(Box<[u8]>);
+
+impl From<&[u8]> for IndexationFeatureBlock {
+    fn from(tag: &[u8]) -> Self {
+        IndexationFeatureBlock(tag.into())
+    }
+}
 
 impl IndexationFeatureBlock {
     /// The feature block kind of an `IndexationFeatureBlock`.
     pub const KIND: u8 = 7;
 
     /// Creates a new `IndexationFeatureBlock`.
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(tag: &[u8]) -> Self {
+        tag.into()
+    }
+
+    /// Returns the tag.
+    pub fn tag(&self) -> &[u8] {
+        &self.0
     }
 }
 
@@ -24,14 +35,21 @@ impl Packable for IndexationFeatureBlock {
     type Error = Error;
 
     fn packed_len(&self) -> usize {
-        0
+        0u32.packed_len() + self.0.len()
     }
 
-    fn pack<W: Write>(&self, _writer: &mut W) -> Result<(), Self::Error> {
+    fn pack<W: Write>(&self, writer: &mut W) -> Result<(), Self::Error> {
+        (self.0.len() as u32).pack(writer)?;
+        writer.write_all(&self.0)?;
+
         Ok(())
     }
 
-    fn unpack_inner<R: Read + ?Sized, const CHECK: bool>(_reader: &mut R) -> Result<Self, Self::Error> {
-        Ok(Self::new())
+    fn unpack_inner<R: Read + ?Sized, const CHECK: bool>(reader: &mut R) -> Result<Self, Self::Error> {
+        let tag_len = u32::unpack_inner::<R, CHECK>(reader)? as usize;
+        let mut tag = vec![0u8; tag_len];
+        reader.read_exact(&mut tag)?;
+
+        Ok(Self(tag.into()))
     }
 }
--- bee-message/src/output/feature_block/metadata.rs
+++ bee-message/src/output/feature_block/metadata.rs
@@ -8,15 +8,26 @@ use bee_common::packable::{Packable, Read, Write};
 ///
 #[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
 #[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
-pub struct MetadataFeatureBlock {}
+pub struct MetadataFeatureBlock(Box<[u8]>);
+
+impl From<&[u8]> for MetadataFeatureBlock {
+    fn from(data: &[u8]) -> Self {
+        MetadataFeatureBlock(data.into())
+    }
+}
 
 impl MetadataFeatureBlock {
     /// The feature block kind of a `MetadataFeatureBlock`.
     pub const KIND: u8 = 8;
 
     /// Creates a new `MetadataFeatureBlock`.
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(data: &[u8]) -> Self {
+        data.into()
+    }
+
+    /// Returns the data.
+    pub fn data(&self) -> &[u8] {
+        &self.0
     }
 }
 
@@ -24,14 +35,21 @@ impl Packable for MetadataFeatureBlock {
     type Error = Error;
 
     fn packed_len(&self) -> usize {
-        0
+        0u32.packed_len() + self.0.len()
     }
 
-    fn pack<W: Write>(&self, _writer: &mut W) -> Result<(), Self::Error> {
+    fn pack<W: Write>(&self, writer: &mut W) -> Result<(), Self::Error> {
+        (self.0.len() as u32).pack(writer)?;
+        writer.write_all(&self.0)?;
+
         Ok(())
     }
 
-    fn unpack_inner<R: Read + ?Sized, const CHECK: bool>(_reader: &mut R) -> Result<Self, Self::Error> {
-        Ok(Self::new())
+    fn unpack_inner<R: Read + ?Sized, const CHECK: bool>(reader: &mut R) -> Result<Self, Self::Error> {
+        let data_len = u32::unpack_inner::<R, CHECK>(reader)? as usize;
+        let mut data = vec![0u8; data_len];
+        reader.read_exact(&mut data)?;
+
+        Ok(Self(data.into()))
     }
 }
