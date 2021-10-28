b4d655009484e18ff40287f8f48c578b6fd2fd88
Thibault Martinez <thibault@iota.org>
2021-10-28 16:24:01 +0200

    Reverse fields of ExtendedOutput

--- bee-message/src/output/extended.rs
+++ bee-message/src/output/extended.rs
@@ -11,19 +11,19 @@ use bee_common::packable::{Packable, Read, Write};
 
 ///
 pub struct ExtendedOutputBuilder {
+    address: Address,
     amount: u64,
     native_tokens: Vec<NativeToken>,
-    address: Address,
     feature_blocks: Vec<FeatureBlock>,
 }
 
 impl ExtendedOutputBuilder {
     ///
-    pub fn new(amount: u64, address: Address) -> Self {
+    pub fn new(address: Address, amount: u64) -> Self {
         Self {
+            address,
             amount,
             native_tokens: Vec::new(),
-            address,
             feature_blocks: Vec::new(),
         }
     }
@@ -55,9 +55,9 @@ impl ExtendedOutputBuilder {
     ///
     pub fn build(self) -> ExtendedOutput {
         ExtendedOutput {
+            address: self.address,
             amount: self.amount,
             native_tokens: self.native_tokens.into_boxed_slice(),
-            address: self.address,
             feature_blocks: self.feature_blocks.into_boxed_slice(),
         }
     }
@@ -67,9 +67,9 @@ impl ExtendedOutputBuilder {
 #[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
 #[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
 pub struct ExtendedOutput {
+    address: Address,
     amount: u64,
     native_tokens: Box<[NativeToken]>,
-    address: Address,
     feature_blocks: Box<[FeatureBlock]>,
 }
 
@@ -78,8 +78,13 @@ impl ExtendedOutput {
     pub const KIND: u8 = 1;
 
     /// Creates a new `ExtendedOutput`.
-    pub fn new(amount: u64, address: Address) -> Self {
-        ExtendedOutputBuilder::new(amount, address).build()
+    pub fn new(address: Address, amount: u64) -> Self {
+        ExtendedOutputBuilder::new(address, amount).build()
+    }
+
+    ///
+    pub fn address(&self) -> &Address {
+        &self.address
     }
 
     ///
@@ -93,11 +98,6 @@ impl ExtendedOutput {
     }
 
     ///
-    pub fn address(&self) -> &Address {
-        &self.address
-    }
-
-    ///
     pub fn feature_blocks(&self) -> &[FeatureBlock] {
         &self.feature_blocks
     }
@@ -107,21 +107,21 @@ impl Packable for ExtendedOutput {
     type Error = Error;
 
     fn packed_len(&self) -> usize {
-        self.amount.packed_len()
+        self.address.packed_len()
+            + self.amount.packed_len()
             + 0u16.packed_len()
             + self.native_tokens.iter().map(Packable::packed_len).sum::<usize>()
-            + self.address.packed_len()
             + 0u16.packed_len()
             + self.feature_blocks.iter().map(Packable::packed_len).sum::<usize>()
     }
 
     fn pack<W: Write>(&self, writer: &mut W) -> Result<(), Self::Error> {
+        self.address.pack(writer)?;
         self.amount.pack(writer)?;
         (self.native_tokens.len() as u16).pack(writer)?;
         for native_token in self.native_tokens.iter() {
             native_token.pack(writer)?
         }
-        self.address.pack(writer)?;
         (self.feature_blocks.len() as u16).pack(writer)?;
         for feature_block in self.feature_blocks.iter() {
             feature_block.pack(writer)?
@@ -131,6 +131,7 @@ impl Packable for ExtendedOutput {
     }
 
     fn unpack_inner<R: Read + ?Sized, const CHECK: bool>(reader: &mut R) -> Result<Self, Self::Error> {
+        let address = Address::unpack_inner::<R, CHECK>(reader)?;
         let amount = u64::unpack_inner::<R, CHECK>(reader)?;
         let native_tokens_len = u16::unpack_inner::<R, CHECK>(reader)? as usize;
         let mut native_tokens = Vec::with_capacity(native_tokens_len);
@@ -142,12 +143,11 @@ impl Packable for ExtendedOutput {
         for _ in 0..feature_blocks_len {
             feature_blocks.push(FeatureBlock::unpack_inner::<R, CHECK>(reader)?);
         }
-        let address = Address::unpack_inner::<R, CHECK>(reader)?;
 
         Ok(Self {
+            address,
             amount,
             native_tokens: native_tokens.into_boxed_slice(),
-            address,
             feature_blocks: feature_blocks.into_boxed_slice(),
         })
     }
