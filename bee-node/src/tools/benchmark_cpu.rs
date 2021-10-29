cd944c127cfdd3134dc31821d06752a905ce7ffe
Dr-Electron <dr-electr0n@protonmail.com>
2021-10-29 23:06:07 +0200

    Format code

--- bee-node/src/tools/benchmark_cpu.rs
+++ bee-node/src/tools/benchmark_cpu.rs
@@ -1,8 +1,18 @@
-use std::{sync::{Arc, atomic::{AtomicU64, Ordering}}, thread::{self}, time::{self, Duration}};
+use std::{
+    sync::{
+        atomic::{AtomicU64, Ordering},
+        Arc,
+    },
+    thread::{self},
+    time::{self, Duration},
+};
 
-use bee_crypto::ternary::{sponge::{BATCH_SIZE, CurlP, CurlPRounds, Sponge}};
+use bee_crypto::ternary::sponge::{CurlP, CurlPRounds, Sponge, BATCH_SIZE};
 use bee_pow::providers::miner::MinerCancel;
-use bee_ternary::{T1B1Buf, TritBuf, b1t6::{self}};
+use bee_ternary::{
+    b1t6::{self},
+    T1B1Buf, TritBuf,
+};
 use structopt::StructOpt;
 use thiserror::Error;
 
@@ -14,13 +24,13 @@ pub enum BenchmarkCPUError {
 
 #[derive(Clone, Debug, StructOpt)]
 pub struct BenchmarkCPUTool {
-    threads: Option<usize>
+    threads: Option<usize>,
 }
 
 pub fn exec(tool: &BenchmarkCPUTool) {
     let threads = match tool.threads {
         Some(threads) => threads,
-        None => num_cpus::get()
+        None => num_cpus::get(),
     };
     let duration = Duration::from_secs(60);
 
@@ -34,9 +44,9 @@ pub fn exec(tool: &BenchmarkCPUTool) {
 
     let time_start = std::time::Instant::now();
 
-    let pow_digest: [u8;32] = rand::random();
+    let pow_digest: [u8; 32] = rand::random();
 
-    let mut workers = Vec::with_capacity(threads+2);
+    let mut workers = Vec::with_capacity(threads + 2);
 
     //Stop if the timeout has exceeded
     let time_thread = thread::spawn(move || {
@@ -47,11 +57,16 @@ pub fn exec(tool: &BenchmarkCPUTool) {
     let process_thread = thread::spawn(move || {
         while !cancel_2.is_cancelled() {
             std::thread::sleep(Duration::from_secs(2));
-            
+
             let elapsed = time_start.elapsed();
-			let (percentage, remaining) = estimate_remaining_time(time_start, elapsed.as_millis() as i64, duration.as_millis() as i64);
-            let megahashes_per_second = counter.load(Ordering::Relaxed) as f64 / (elapsed.as_secs_f64() * 1_000_000 as f64);
-            println!("Average CPU speed: {:.2}MH/s ({} thread(s), {:.2}%. {:.2?} left...)", megahashes_per_second, threads, percentage, remaining);
+            let (percentage, remaining) =
+                estimate_remaining_time(time_start, elapsed.as_millis() as i64, duration.as_millis() as i64);
+            let megahashes_per_second =
+                counter.load(Ordering::Relaxed) as f64 / (elapsed.as_secs_f64() * 1_000_000 as f64);
+            println!(
+                "Average CPU speed: {:.2}MH/s ({} thread(s), {:.2}%. {:.2?} left...)",
+                megahashes_per_second, threads, percentage, remaining
+            );
         }
     });
 
@@ -75,12 +90,17 @@ pub fn exec(tool: &BenchmarkCPUTool) {
     }
 
     let megahashes_per_second = counter_2.load(Ordering::Relaxed) as f64 / (duration.as_secs_f64() * 1_000_000 as f64);
-    println!("Average CPU speed: {:.2}MH/s ({} thread(s), took {:.2?})", megahashes_per_second, threads, duration);
+    println!(
+        "Average CPU speed: {:.2}MH/s ({} thread(s), took {:.2?})",
+        megahashes_per_second, threads, duration
+    );
 }
 
 fn cpu_benchmark_worker(_pow_digest: &[u8], start_nonce: u64, cancel: MinerCancel, counter: Arc<AtomicU64>) {
     let mut pow_digest = TritBuf::<T1B1Buf>::new();
-    b1t6::encode::<T1B1Buf>(&_pow_digest).iter().for_each(|t| pow_digest.push(t));
+    b1t6::encode::<T1B1Buf>(&_pow_digest)
+        .iter()
+        .for_each(|t| pow_digest.push(t));
 
     let mut nonce = start_nonce;
     let mut curlp = CurlP::new(CurlPRounds::Rounds81);
@@ -104,12 +124,12 @@ fn cpu_benchmark_worker(_pow_digest: &[u8], start_nonce: u64, cancel: MinerCance
 
 // Calculates the remaining time for a running operation and returns the finished percentage.
 fn estimate_remaining_time(time_start: std::time::Instant, current: i64, total: i64) -> (f64, std::time::Duration) {
-	let ratio = current as f64 / total as f64;
+    let ratio = current as f64 / total as f64;
     let total_time = time::Duration::from_secs_f64(time_start.elapsed().as_secs_f64() / ratio);
     let time_now = std::time::Instant::now();
     if time_now > (time_start + total_time) {
         return (100.0, Duration::from_secs(0));
     }
     let remaining = (time_start + total_time).duration_since(time_now);
-	return (ratio * 100.0, remaining)
+    return (ratio * 100.0, remaining);
 }
