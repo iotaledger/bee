db9ab494c2b4844e832cc7605fd1a8721ad3c69f
Christian Poveda <31802960+pvdrz@users.noreply.github.com>
2021-11-08 11:39:36 -0500

    Add check script (#820)
    * add check script
    
    * rename `check.sh` to `all_checks.sh`
    
    * explain the `packages` variable
    
    * create `scripts` folder

---
+++ scripts/all_checks.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Print and run a command
+print_and_run() {
+    cmd=\`$@\`
+    echo Running $cmd...
+    $*
+    status_code=$?
+    if [[ $status_code -eq 0 ]] ; then
+        echo $cmd was succesful!
+    else
+        echo $cmd failed!
+    fi
+    return $status_code
+}
+
+# Check if any arguments were provided and use them as the packages to be
+# checked. Use the `--all` flag if there were no arguments to check the whole
+# workspace.
+if [[ $# -eq 0 ]] ; then
+    packages=--all
+else
+    packages=-p $*
+fi
+
+print_and_run cargo +nightly fmt $packages &&
+print_and_run cargo check --all-features $packages &&
+print_and_run cargo +nightly check --all-features $packages &&
+print_and_run cargo +nightly clippy --all-targets --all-features $packages -- -D warnings &&
+print_and_run cargo test --all-features --no-fail-fast $packages &&
+print_and_run cargo +nightly test --all-features --no-fail-fast $packages &&
+print_and_run cargo doc --all-features $packages &&
+print_and_run cargo +nightly udeps --all-features $packages
