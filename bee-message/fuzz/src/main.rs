79ff6a550a5e4235f5a6a5c96be9d898693b4b77
Thibault Martinez <thibault@iota.org>
2021-11-12 11:30:00 +0100

    Add error print in fuzzer sorter

--- bee-message/fuzz/src/main.rs
+++ bee-message/fuzz/src/main.rs
@@ -38,6 +38,7 @@ fn main() -> io::Result<()> {
                     .create(true)
                     .open(format!("./corpus/errors/{}", file_name))?;
                 file.write_all(&buffer)?;
+                println!("{:?}", err);
             }
         }
     }
