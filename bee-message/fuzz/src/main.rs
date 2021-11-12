ff934242ed3b8356c575b43b7f410b98019e33f4
Thibault Martinez <thibault@iota.org>
2021-11-12 11:06:33 +0100

    Write fuzzer sorted outputs to files

--- bee-message/fuzz/src/main.rs
+++ bee-message/fuzz/src/main.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use std::{
-    fs::{self, File},
+    fs::{self, File, OpenOptions},
     io::{self, prelude::*},
 };
 
@@ -11,17 +11,34 @@ use bee_message::{Error, Message};
 
 fn main() -> io::Result<()> {
     let paths = fs::read_dir("./corpus/fuzz_message")?;
+    std::fs::create_dir_all("./corpus/errors")?;
 
     for path in paths {
+        let file_name = format!(
+            "{}",
+            path.as_ref()
+                .unwrap()
+                .path()
+                .file_name()
+                .unwrap()
+                .to_os_string()
+                .into_string()
+                .unwrap()
+        );
         let mut file = File::open(path?.path())?;
         let mut buffer = Vec::new();
 
         file.read_to_end(&mut buffer)?;
 
-        let res = Message::unpack(&mut buffer.as_slice());
-
-        if !matches!(res, Err(Error::Io(..))) {
-            println!("{:?}", res);
+        if let Err(err) = Message::unpack(&mut buffer.as_slice()) {
+            if !matches!(err, Error::Io(..)) {
+                let mut file = OpenOptions::new()
+                    .write(true)
+                    .truncate(true)
+                    .create(true)
+                    .open(format!("./corpus/errors/{}", file_name))?;
+                file.write_all(&buffer)?;
+            }
         }
     }
 
