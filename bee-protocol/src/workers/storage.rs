06c16e14a1192bd4c576f66b11d4e1822ed5ed31
Thibault Martinez <thibault@iota.org>
2021-11-18 13:42:39 +0100

    Remove some storage bounds

--- bee-protocol/src/workers/storage.rs
+++ bee-protocol/src/workers/storage.rs
@@ -3,28 +3,21 @@
 
 use bee_ledger::workers::StorageBackend as LedgerStorageBackend;
 use bee_message::{milestone::MilestoneIndex, payload::indexation::PaddedIndex, MessageId};
-use bee_storage::{
-    access::{Batch, Insert},
-    backend,
-};
-use bee_tangle::{storage::StorageBackend as TangleStorageBackend, unreferenced_message::UnreferencedMessage};
+use bee_storage::{access::Insert, backend};
+use bee_tangle::unreferenced_message::UnreferencedMessage;
 
 pub trait StorageBackend:
     backend::StorageBackend
-    + Batch<(MilestoneIndex, UnreferencedMessage), ()>
     + Insert<(PaddedIndex, MessageId), ()>
     + Insert<(MilestoneIndex, UnreferencedMessage), ()>
-    + TangleStorageBackend
     + LedgerStorageBackend
 {
 }
 
 impl<T> StorageBackend for T where
     T: backend::StorageBackend
-        + Batch<(MilestoneIndex, UnreferencedMessage), ()>
         + Insert<(PaddedIndex, MessageId), ()>
         + Insert<(MilestoneIndex, UnreferencedMessage), ()>
-        + TangleStorageBackend
         + LedgerStorageBackend
 {
 }
