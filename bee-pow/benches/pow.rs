76405e29b80868da1177bfdb04531ef1c22df1b5
Jochen Görtler <jochen.goertler@iota.org>
2021-11-22 19:57:04 +0100

    :sparkles: Add throughput calculation

--- bee-pow/benches/pow.rs
+++ bee-pow/benches/pow.rs
@@ -10,11 +10,16 @@ use bee_pow::{
 };
 use bee_test::rand::message::rand_message;
 
-use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion, Throughput};
+
+const MINIMUM_POW_SCORE: f64 = 4000.0;
 
 fn scoring_benchmark(c: &mut Criterion) {
-    let mut pow = PoWScorer::new();
-    c.bench_function("scoring", |b| {
+    let mut g = c.benchmark_group("scoring");
+    g.throughput(Throughput::Elements(1));
+
+    g.bench_function("random message", |b| {
+        let mut pow = PoWScorer::new();
         b.iter_batched(
             rand_message,
             |msg| {
@@ -27,23 +32,25 @@ fn scoring_benchmark(c: &mut Criterion) {
 }
 
 fn mining_benchmark(c: &mut Criterion) {
-    let mut g = c.benchmark_group("mining");
-    g.measurement_time(Duration::from_secs(60));
+    let mut g = c.benchmark_group("find nonce");
+    g.measurement_time(Duration::from_secs(120));
+    g.throughput(Throughput::Elements(1));
 
-    g.bench_function("single-threaded", |b| {
+    g.bench_function("random message (single-threaded)", |b| {
         let miner = MinerBuilder::default().with_num_workers(1).finish();
         b.iter_batched(
-            rand::random::<[u8; 32]>,
-            |bytes| miner.nonce(&bytes, 4000.0),
+            // TODO: Something is odd here `rand_message` takes forever.
+            rand::random::<[u8; 32]>, 
+            |bytes| miner.nonce(&bytes, MINIMUM_POW_SCORE),
             BatchSize::SmallInput,
         )
     });
 
-    g.bench_function("multi-threaded", |b| {
+    g.bench_function("random message (multi-threaded)", |b| {
         let miner = MinerBuilder::default().with_num_workers(num_cpus::get()).finish();
         b.iter_batched(
             rand::random::<[u8; 32]>,
-            |bytes| miner.nonce(&bytes, 4000.0),
+            |bytes| miner.nonce(&bytes, MINIMUM_POW_SCORE),
             BatchSize::SmallInput,
         )
     });
