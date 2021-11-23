d01362f35df693c281f97e9ed94d6869f93dae02
Adam-Gleave <adam.gleave@iota.org>
2021-11-23 14:18:41 +0000

    Add license headers to examples

--- bee-trace/examples/flamegraph.rs
+++ bee-trace/examples/flamegraph.rs
@@ -1,3 +1,6 @@
+// Copyright 2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 use bee_trace::observe;
 
 use tokio::{task, time::timeout};
--- bee-trace/examples/logging.rs
+++ bee-trace/examples/logging.rs
@@ -1,3 +1,6 @@
+// Copyright 2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 use bee_common::logger::{LoggerConfig, LoggerOutputConfigBuilder};
 
 use std::path::PathBuf;
