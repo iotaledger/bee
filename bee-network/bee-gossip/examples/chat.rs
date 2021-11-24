660e1b266d957e6bd54d9a0aadd0c304e29eff37
Alexander Schmidt <alexander.schmidt@iota.org>
2021-11-24 12:03:45 +0100

    Fix bee-gossip example

--- bee-network/bee-gossip/examples/chat.rs
+++ bee-network/bee-gossip/examples/chat.rs
@@ -25,7 +25,7 @@ mod common;
 
 #[tokio::main]
 async fn main() {
-    use bee_network::{alias, standalone::init, Event, Multiaddr, NetworkConfig, Protocol};
+    use bee_gossip::{alias, standalone::init, Event, Multiaddr, NetworkConfig, Protocol};
     use common::keys_and_ids::{gen_constant_net_id, gen_deterministic_keys, gen_deterministic_peer_id};
     use std::{
         env,
