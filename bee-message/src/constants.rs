4930ec15fce7c64a3d3e8d5e5e54d21805603dcb
Thibault Martinez <thibault@iota.org>
2021-11-24 09:38:06 +0100

    Remove old dust constants

--- bee-message/src/constants.rs
+++ bee-message/src/constants.rs
@@ -4,19 +4,5 @@
 /// The total number of IOTA tokens in circulation.
 pub const IOTA_SUPPLY: u64 = 2_779_530_283_277_761;
 
-// TODO check if still needed.
-
 /// Amount of tokens below which an output is considered a dust output.
 pub const DUST_THRESHOLD: u64 = 1_000_000;
-/// Divisor used to compute the allowed dust outputs on an address.
-pub const DUST_ALLOWANCE_DIVISOR: u64 = 100_000;
-/// Maximum number of dust outputs for an address.
-pub const DUST_OUTPUTS_MAX: u64 = 100;
-
-/// The maximum number of allowed dust outputs on an address is `dust_allowance_sum` divided by `DUST_ALLOWANCE_DIVISOR`
-/// and rounded down, i.e. 10 outputs for each 1 Mi deposited. `dust_allowance_sum` is the sum of all the amounts of all
-/// unspent `SigLockedDustAllowanceOutputs` on this address. Regardless of `dust_allowance_sum`, the number of dust
-/// outputs must never exceed `DUST_OUTPUTS_MAX` per address.
-pub fn dust_outputs_max(dust_allowance_sum: u64) -> u64 {
-    DUST_OUTPUTS_MAX.min(dust_allowance_sum / DUST_ALLOWANCE_DIVISOR)
-}
