9a4126f3f1e9f5853c92186bdf9f30e5cb0fc6aa
Jochen Görtler <jochen.goertler@iota.org>
2021-12-06 11:37:04 +0100

    Small improvements

--- bee-ledger/src/workers/snapshot/download.rs
+++ bee-ledger/src/workers/snapshot/download.rs
@@ -11,7 +11,7 @@ use log::{info, warn};
 
 use std::{io::Read, path::Path};
 
-async fn download_snapshot_header(download_url: &str) -> Result<Option<(SnapshotHeader, &str)>, Error> {
+async fn download_snapshot_header(download_url: &str) -> Result<(SnapshotHeader, &str), Error> {
     info!("Downloading snapshot header {}...", download_url);
 
     match reqwest::get(download_url).await.and_then(|res| res.error_for_status()) {
@@ -29,35 +29,31 @@ async fn download_snapshot_header(download_url: &str) -> Result<Option<(Snapshot
                 if bytes.len() >= SnapshotHeader::LENGTH {
                     info!("Downloaded snapshot header from {}.", download_url);
 
-                    bytes.resize(SnapshotHeader::LENGTH, 0);
-                    let mut slice: &[u8] = &bytes;
+                    let mut slice: &[u8] = &bytes[..SnapshotHeader::LENGTH];
 
-                    return Ok(Some((SnapshotHeader::unpack(&mut slice)?, download_url)));
+                    return Ok((SnapshotHeader::unpack(&mut slice)?, download_url));
                 }
             }
         }
         Err(e) => warn!("Downloading snapshot header failed: {:?}.", e.to_string()),
     }
 
-    Ok(None)
+    Err(Error::DownloadingFailed)
 }
 
 async fn most_recent_snapshot_url<'a>(download_urls: impl Iterator<Item = &'a str>) -> Result<&'a str, Error> {
     let downloads = join_all(download_urls.map(download_snapshot_header)).await;
-    let snapshot_headers = downloads.into_iter().collect::<Result<Vec<_>, Error>>()?;
-    let mut snapshot_headers = snapshot_headers.iter().flatten().collect::<Vec<_>>();
+    let snapshot_headers = downloads.iter().flatten();
 
-    if snapshot_headers.is_empty() {
-        return Err(Error::NoDownloadSourceAvailable);
-    }
-
-    // Sort the headers so that the largest ledger index is in the last element.
-    snapshot_headers.sort_by(|(header_a, _), (header_b, _)| header_a.ledger_index().cmp(&header_b.ledger_index()));
+    // Find header with largest index.
+    let largest_index = snapshot_headers.max_by_key(|(header, _)| header.ledger_index());
 
     // We know `snapshot_headers` is not empty, so unwrapping here is fine.
-    let (_, url) = snapshot_headers.pop().unwrap();
-
-    Ok(url)
+    if let Some((_, url)) = largest_index {
+        Ok(url)
+    } else {
+        Err(Error::NoDownloadSourceAvailable)
+    }
 }
 
 pub(crate) async fn download_snapshot_file(
