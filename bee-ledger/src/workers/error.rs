b8be765c8ea34d7687225690bb76cca097b3bccb
Thibault Martinez <thibault@iota.org>
2021-12-15 14:15:44 +0100

    Change error capitalization

--- bee-ledger/src/workers/error.rs
+++ bee-ledger/src/workers/error.rs
@@ -14,66 +14,66 @@ use bee_message::{milestone::MilestoneIndex, Error as MessageError, MessageId};
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     /// Snapshot error.
-    #[error("Snapshot error: {0}")]
+    #[error("snapshot error: {0}")]
     Snapshot(#[from] SnapshotError),
     /// Types error.
-    #[error("Types error: {0}")]
+    #[error("types error: {0}")]
     Types(#[from] TypesError),
     /// Message error.
-    #[error("Message error: {0}")]
+    #[error("message error: {0}")]
     Message(#[from] MessageError),
     /// Missing message in the past cone of the milestone
-    #[error("Message {0} is missing in the past cone of the milestone")]
+    #[error("message {0} is missing in the past cone of the milestone")]
     MissingMessage(MessageId),
     /// Unsupported input kind.
-    #[error("Unsupported input kind: {0}")]
+    #[error("unsupported input kind: {0}")]
     UnsupportedInputKind(u8),
     /// Unsupported output kind.
-    #[error("Unsupported output kind: {0}")]
+    #[error("unsupported output kind: {0}")]
     UnsupportedOutputKind(u8),
     /// Unsupported payload kind.
-    #[error("Unsupported payload kind: {0}")]
+    #[error("unsupported payload kind: {0}")]
     UnsupportedPayloadKind(u32),
     /// Milestone message not found.
-    #[error("Milestone message not found: {0}")]
+    #[error("milestone message not found: {0}")]
     MilestoneMessageNotFound(MessageId),
     /// Message payload is not a milestone
-    #[error("Message payload is not a milestone")]
+    #[error("message payload is not a milestone")]
     NoMilestonePayload,
     /// Non contiguous milestones.
-    #[error("Non contiguous milestones: tried to confirm {0} on top of {1}")]
+    #[error("non contiguous milestones: tried to confirm {0} on top of {1}")]
     NonContiguousMilestones(u32, u32),
     /// Merkle proof mismatch.
-    #[error("Merkle proof mismatch on milestone {0}: computed {1} != provided {2}")]
+    #[error("merkle proof mismatch on milestone {0}: computed {1} != provided {2}")]
     MerkleProofMismatch(MilestoneIndex, String, String),
     /// Invalid messages count.
-    #[error("Invalid messages count: referenced ({0}) != no transaction ({1}) + conflicting ({2}) + included ({3})")]
+    #[error("invalid messages count: referenced ({0}) != no transaction ({1}) + conflicting ({2}) + included ({3})")]
     InvalidMessagesCount(usize, usize, usize, usize),
     /// Invalid ledger unspent state.
-    #[error("Invalid ledger unspent state: {0}")]
+    #[error("invalid ledger unspent state: {0}")]
     InvalidLedgerUnspentState(u64),
     /// Invalid ledger balance state.
-    #[error("Invalid ledger balance state: {0}")]
+    #[error("invalid ledger balance state: {0}")]
     InvalidLedgerBalanceState(u64),
     /// Consumed amount overflow.
-    #[error("Consumed amount overflow: {0}.")]
+    #[error("consumed amount overflow: {0}.")]
     ConsumedAmountOverflow(u128),
     /// Created amount overflow.
-    #[error("Created amount overflow: {0}.")]
+    #[error("created amount overflow: {0}.")]
     CreatedAmountOverflow(u128),
     /// Ledger state overflow.
-    #[error("Ledger state overflow: {0}")]
+    #[error("ledger state overflow: {0}")]
     LedgerStateOverflow(u128),
     /// Non zero balance diff sum.
-    #[error("Non zero balance diff sum: {0}.")]
+    #[error("non zero balance diff sum: {0}.")]
     NonZeroBalanceDiffSum(i64),
     /// Decreasing receipt migrated at index.
-    #[error("Decreasing receipt migrated at index: {0} < {1}")]
+    #[error("decreasing receipt migrated at index: {0} < {1}")]
     DecreasingReceiptMigratedAtIndex(MilestoneIndex, MilestoneIndex),
     /// Missing unspent output.
-    #[error("Missing unspent output {0}")]
+    #[error("missing unspent output {0}")]
     MissingUnspentOutput(Unspent),
     /// Storage backend error.
-    #[error("Storage backend error: {0}")]
+    #[error("storage backend error: {0}")]
     Storage(Box<dyn std::error::Error + Send>),
 }
