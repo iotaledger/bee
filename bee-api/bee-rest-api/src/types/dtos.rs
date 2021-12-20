7b64a3c27aad5a07cfcb3ccc4e3388d9faf84278
rufsam <samuel.rufinatscha@iota.org>
2021-12-20 17:48:42 +0100

    add output type to error message

--- bee-api/bee-rest-api/src/types/dtos.rs
+++ bee-api/bee-rest-api/src/types/dtos.rs
@@ -372,19 +372,19 @@ impl<'de> serde::Deserialize<'de> for OutputDto {
             {
                 SimpleOutput::KIND => OutputDto::Simple(
                     SimpleOutputDto::deserialize(value)
-                        .map_err(|e| serde::de::Error::custom(format!("can not deserialize output: {}", e)))?,
+                        .map_err(|e| serde::de::Error::custom(format!("can not deserialize simple output: {}", e)))?,
                 ),
                 TreasuryOutput::KIND => OutputDto::Treasury(
                     TreasuryOutputDto::deserialize(value)
-                        .map_err(|e| serde::de::Error::custom(format!("can not deserialize output: {}", e)))?,
+                        .map_err(|e| serde::de::Error::custom(format!("can not deserialize treasury output: {}", e)))?,
                 ),
                 ExtendedOutput::KIND => OutputDto::Extended(
                     ExtendedOutputDto::deserialize(value)
-                        .map_err(|e| serde::de::Error::custom(format!("can not deserialize output: {}", e)))?,
+                        .map_err(|e| serde::de::Error::custom(format!("can not deserialize extended output: {}", e)))?,
                 ),
                 AliasOutput::KIND => OutputDto::Alias(
                     AliasOutputDto::deserialize(value)
-                        .map_err(|e| serde::de::Error::custom(format!("can not deserialize output: {}", e)))?,
+                        .map_err(|e| serde::de::Error::custom(format!("can not deserialize alias output: {}", e)))?,
                 ),
                 _ => unimplemented!(),
             },
