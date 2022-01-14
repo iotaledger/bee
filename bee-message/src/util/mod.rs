7067b5b243392a7f2036929828ba96087cf4a0f1
Christian Poveda <christian.poveda@iota.org>
2022-01-14 10:31:59 -0500

    reorganize dependencies

--- bee-message/src/util/mod.rs
+++ bee-message/src/util/mod.rs
@@ -5,12 +5,12 @@ mod map;
 
 #[cfg(not(feature = "std"))]
 pub(crate) use map::{Map, Set};
-#[cfg(feature = "std")]
-pub(crate) use std::collections::{HashMap as Map, HashSet as Set};
 
 use crate::error::Error;
 
 use alloc::string::ToString;
+#[cfg(feature = "std")]
+pub(crate) use std::collections::{HashMap as Map, HashSet as Set};
 
 /// Tries to decode an hexadecimal encoded string to an array of bytes.
 pub fn hex_decode<const LENGTH: usize>(hex: &str) -> Result<[u8; LENGTH], Error> {
