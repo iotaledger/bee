39d116ac05ea17fe07480eff9dff8b7f1e7c29f4
Jochen Görtler <jochen.goertler@iota.org>
2022-01-20 15:42:00 +0100

    Make imports specific

--- bee-network/bee-gossip/src/lib.rs
+++ bee-network/bee-gossip/src/lib.rs
@@ -28,10 +28,7 @@ pub use libp2p_core::{
 // Exported only with "full" feature flag.
 #[cfg(feature = "full")]
 #[doc(inline)]
-pub use libp2p::core::identity::{
-    ed25519::{Keypair, SecretKey},
-    PublicKey,
-};
+pub use libp2p::core::identity::ed25519::{Keypair, PublicKey, SecretKey};
 
 #[cfg(feature = "full")]
 pub use crate::{
