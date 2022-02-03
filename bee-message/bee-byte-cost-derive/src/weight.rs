b7b98fc0ccfcfadf50b82b6c6384757f31e39a1c
Jochen Görtler <jochen.goertler@iota.org>
2022-02-03 16:44:07 +0100

    :page_facing_up: Unify copyright

--- bee-message/bee-byte-cost-derive/src/weight.rs
+++ bee-message/bee-byte-cost-derive/src/weight.rs
@@ -1,4 +1,4 @@
-// Copyright 2021 IOTA Stiftung
+// Copyright 2021-2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
 use syn::{spanned::Spanned, Attribute, Error, Meta, NestedMeta, Result};
--- bee-message/bee-byte-cost/src/lib.rs
+++ bee-message/bee-byte-cost/src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2020-2021 IOTA Stiftung
+// Copyright 2021-2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
 pub use bee_byte_cost_derive::ByteCost;
