974c8ad68e31e44e9360c43411dcd20da50ff273
Thibault Martinez <thibault@iota.org>
2022-02-04 17:32:26 +0100

    Move crate doc up

--- bee-api/bee-rest-api/src/lib.rs
+++ bee-api/bee-rest-api/src/lib.rs
@@ -1,12 +1,12 @@
 // Copyright 2020-2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-// #![deny(missing_docs, warnings)]
-
 //! Bee REST API
 //!
 //! ## Feature Flags
 //! - `cpt2`: Enable support for backwards compatible output and transaction payload types.
+
+// #![deny(missing_docs, warnings)]
 
 #![cfg_attr(doc_cfg, feature(doc_cfg))]
 
