db03764397c9d0f783a00771187edbafec473251
Jochen Görtler <grtlr@users.noreply.github.com>
2022-02-04 14:14:53 +0100

    Respect conditions during random `Output` generation (#1079)
    * Respect conditions during random `Output` generation
    
    * Review comment

--- bee-test/src/rand/output.rs
+++ bee-test/src/rand/output.rs
@@ -2,19 +2,29 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::rand::{
+    address::rand_alias_address,
     bytes::{rand_bytes, rand_bytes_array},
     message::rand_message_id,
     milestone::{rand_milestone_id, rand_milestone_index},
     number::{rand_number, rand_number_range},
-    output::feature_block::rand_allowed_feature_blocks,
+    output::{
+        feature_block::rand_allowed_feature_blocks,
+        unlock_condition::{
+            rand_address_unlock_condition, rand_address_unlock_condition_different_from,
+            rand_governor_address_unlock_condition_different_from,
+            rand_state_controller_address_unlock_condition_different_from,
+        },
+    },
     transaction::rand_transaction_id,
 };
 
 /// Module providing random feature block generation utilities.
 pub mod feature_block;
+/// Module providing random unlock condition generation utilities.
+pub mod unlock_condition;
 
 use bee_ledger::types::{ConsumedOutput, CreatedOutput, TreasuryOutput, Unspent};
-use bee_message::output::{self, Output, OutputId, OUTPUT_INDEX_RANGE};
+use bee_message::output::{self, unlock_condition::AddressUnlockCondition, Output, OutputId, OUTPUT_INDEX_RANGE};
 
 use primitive_types::U256;
 
@@ -40,6 +50,7 @@ pub fn rand_basic_output() -> output::BasicOutput {
     output::BasicOutput::build(rand_number_range(Output::AMOUNT_RANGE))
         .unwrap()
         .with_feature_blocks(feature_blocks)
+        .add_unlock_condition(rand_address_unlock_condition().into())
         .finish()
         .unwrap()
 }
@@ -54,6 +65,8 @@ pub fn rand_alias_output() -> output::AliasOutput {
     output::AliasOutput::build(rand_number_range(Output::AMOUNT_RANGE), alias_id)
         .unwrap()
         .with_feature_blocks(feature_blocks)
+        .add_unlock_condition(rand_state_controller_address_unlock_condition_different_from(&alias_id).into())
+        .add_unlock_condition(rand_governor_address_unlock_condition_different_from(&alias_id).into())
         .finish()
         .unwrap()
 }
@@ -62,8 +75,8 @@ pub fn rand_alias_output() -> output::AliasOutput {
 pub fn rand_foundry_output() -> output::FoundryOutput {
     let feature_blocks = rand_allowed_feature_blocks(output::FoundryOutput::ALLOWED_FEATURE_BLOCKS);
 
-    let max_supply = U256::from(rand_bytes_array()).saturating_add(1.into());
-    let circulating = U256::from(rand_bytes_array()) % max_supply.saturating_add(1.into());
+    let max_supply = U256::from(rand_bytes_array()).saturating_add(U256::one());
+    let circulating = U256::from(rand_bytes_array()) % max_supply.saturating_add(U256::one());
 
     output::FoundryOutput::build(
         rand_number_range(Output::AMOUNT_RANGE),
@@ -75,6 +88,7 @@ pub fn rand_foundry_output() -> output::FoundryOutput {
     )
     .unwrap()
     .with_feature_blocks(feature_blocks)
+    .add_unlock_condition(AddressUnlockCondition::new(rand_alias_address().into()).into())
     .finish()
     .unwrap()
 }
@@ -82,15 +96,25 @@ pub fn rand_foundry_output() -> output::FoundryOutput {
 /// Generates a random [`NftOutput`](output::NftOutput).
 pub fn rand_nft_output() -> output::NftOutput {
     let feature_blocks = rand_allowed_feature_blocks(output::NftOutput::ALLOWED_FEATURE_BLOCKS);
+
+    // We need to make sure that `NftId` and `Address` don't match.
+    let nft_id = output::NftId::from(rand_bytes_array());
+
     output::NftOutput::build(
         rand_number_range(Output::AMOUNT_RANGE),
-        output::NftId::new(rand_bytes_array()),
+        nft_id,
         rand_bytes(rand_number_range(0..output::NftOutput::IMMUTABLE_METADATA_LENGTH_MAX) as usize),
     )
     .unwrap()
     .with_feature_blocks(feature_blocks)
+    .add_unlock_condition(rand_address_unlock_condition_different_from(&nft_id).into())
     .finish()
     .unwrap()
+}
+
+/// Generates a random ledger [`TreasuryOutput`].
+pub fn rand_ledger_treasury_output() -> TreasuryOutput {
+    TreasuryOutput::new(rand_treasury_output(), rand_milestone_id())
 }
 
 /// Generates a random [`Output`].
@@ -112,19 +136,5 @@ pub fn rand_consumed_output() -> ConsumedOutput {
 
 /// Generates a random [`CreatedOutput`].
 pub fn rand_created_output() -> CreatedOutput {
-    CreatedOutput::new(
-        rand_message_id(),
-        rand_milestone_index(),
-        rand_number(),
-        match rand_number::<u64>() % 3 {
-            // 1 => todo!(),
-            _ => rand_treasury_output().into(),
-            // _ => unreachable!(),
-        },
-    )
-}
-
-/// Generates a random ledger [`TreasuryOutput`].
-pub fn rand_ledger_treasury_output() -> TreasuryOutput {
-    TreasuryOutput::new(rand_treasury_output(), rand_milestone_id())
+    CreatedOutput::new(rand_message_id(), rand_milestone_index(), rand_number(), rand_output())
 }
---
+++ bee-test/src/rand/output/unlock_condition.rs
@@ -0,0 +1,54 @@
+// Copyright 2022 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::rand::address::{rand_address, rand_alias_address, rand_nft_address};
+
+use bee_message::{
+    address::Address,
+    output::{
+        unlock_condition::{
+            AddressUnlockCondition, GovernorAddressUnlockCondition, StateControllerAddressUnlockCondition,
+        },
+        AliasId, NftId,
+    },
+};
+
+/// Generates a random [`AddressUnlockCondition`].
+pub fn rand_address_unlock_condition() -> AddressUnlockCondition {
+    rand_address().into()
+}
+
+/// Generates a random [`StateControllerAddressUnlockCondition`].
+pub fn rand_state_controller_address_unlock_condition_different_from(
+    alias_id: &AliasId,
+) -> StateControllerAddressUnlockCondition {
+    let mut address = rand_address();
+    if let Address::Alias(mut alias_address) = &mut address {
+        while alias_address.id() == alias_id {
+            alias_address = rand_alias_address();
+        }
+    }
+    address.into()
+}
+
+/// Generates a random [`GovernorAddressUnlockCondition`] that is different from `alias_id`.
+pub fn rand_governor_address_unlock_condition_different_from(alias_id: &AliasId) -> GovernorAddressUnlockCondition {
+    let mut address = rand_address();
+    if let Address::Alias(mut alias_address) = &mut address {
+        while alias_address.id() == alias_id {
+            alias_address = rand_alias_address();
+        }
+    }
+    address.into()
+}
+
+/// Generates a random [`AddressUnlockCondition`] that is different from `nft_id`.
+pub fn rand_address_unlock_condition_different_from(nft_id: &NftId) -> AddressUnlockCondition {
+    let mut address = rand_address();
+    if let Address::Nft(mut nft_address) = &mut address {
+        while nft_address.id() == nft_id {
+            nft_address = rand_nft_address();
+        }
+    }
+    address.into()
+}
