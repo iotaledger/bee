0db65bdaa1b09abaa57c762e52a32cdda90b0999
Thibault Martinez <thibault@iota.org>
2022-02-04 16:44:38 +0100

    Log peer_id of entrynode (#1086)
    * Log peer_id of entrynode
    
    * Fmt

--- bee-node/src/entrynode/builder.rs
+++ bee-node/src/entrynode/builder.rs
@@ -129,6 +129,9 @@ impl NodeBuilder<EntryNode> for EntryNodeBuilder {
             network_spec.hrp()
         );
 
+        // Print the local entity data.
+        log::info!("{}", builder.config().local());
+
         // Add the resources that are shared throughout the node.
         let builder = add_node_resources(builder)?;
 
