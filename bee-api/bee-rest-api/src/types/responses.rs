ad41f468956271182ff895f8b664e77092c4a023
Samuel Rufinatscha <samuel.rufinatscha@gmail.com>
2022-02-07 13:16:40 +0100

    Update bee-api/bee-rest-api/src/types/responses.rs
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>

--- bee-api/bee-rest-api/src/types/responses.rs
+++ bee-api/bee-rest-api/src/types/responses.rs
@@ -59,7 +59,7 @@ pub struct ProtocolResponse {
 pub struct RentStructureResponse {
     #[serde(rename = "vByteCost")]
     pub vbyte_cost: u64,
-    #[serde(rename = "vBFactorData")]
+    #[serde(rename = "vByteFactorData")]
     pub vbyte_factor_data: u64,
     #[serde(rename = "vBFactorKey")]
     pub vbyte_factor_key: u64,
