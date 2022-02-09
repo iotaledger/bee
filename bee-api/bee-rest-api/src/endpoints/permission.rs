f806f69093221693ff8b5f84297fe99c87fac53a
rufsam <samuel.rufinatscha@iota.org>
2022-02-09 14:25:47 +0100

    Base64 decode JWT payload to categorize JWT audience

--- bee-api/bee-rest-api/src/endpoints/permission.rs
+++ bee-api/bee-rest-api/src/endpoints/permission.rs
@@ -4,6 +4,7 @@
 use crate::endpoints::{config::route_to_regex, rejection::CustomRejection, storage::StorageBackend, ApiArgs};
 
 use auth_helper::jwt::{Claims, JsonWebToken, TokenData};
+use base64;
 
 use lazy_static::lazy_static;
 use regex::RegexSet;
@@ -47,16 +48,28 @@ pub(crate) fn check_permission<B: StorageBackend>(
                     return Ok(());
                 }
 
-                if let Ok(jwt) = validate_jwt(&headers, &args) {
-                    let aud_claim = jwt.claims.audience();
+                let jwt = extract_jwt(&headers)?;
+                let jwt_payload = {
+                    let jwt_string = jwt.to_string();
+                    let encoded = jwt_string.trim_start_matches(".").trim_end_matches(".");
+                    let decoded = base64::decode(encoded).map_err(|_| reject::custom(CustomRejection::Forbidden))?;
+                    String::from_utf8(decoded.clone()).map_err(|_| CustomRejection::Forbidden)?
+                };
 
-                    if aud_claim == API_AUDIENCE_CLAIM {
-                        if args.rest_api_config.protected_routes.is_match(path.as_str()) {
-                            return Ok(());
+                if jwt_payload.contains("api") {
+                    if let Ok(jwt) = validate_api_jwt(&jwt, &args) {
+                        if jwt.claims.audience() == API_AUDIENCE_CLAIM {
+                            if args.rest_api_config.protected_routes.is_match(path.as_str()) {
+                                return Ok(());
+                            }
                         }
-                    } else if aud_claim == DASHBOARD_AUDIENCE_CLAIM {
-                        if DASHBOARD_ROUTES.is_match(path.as_str()) {
-                            return Ok(());
+                    }
+                } else if jwt_payload.contains("dashboard") {
+                    if let Ok(jwt) = validate_dashboard_jwt(&jwt, &args) {
+                        if jwt.claims.audience() == DASHBOARD_AUDIENCE_CLAIM {
+                            if DASHBOARD_ROUTES.is_match(path.as_str()) {
+                                return Ok(());
+                            }
                         }
                     }
                 }
@@ -67,7 +80,7 @@ pub(crate) fn check_permission<B: StorageBackend>(
         .untuple_one()
 }
 
-fn extract_jwt(headers: &HeaderMap) -> Result<String, Rejection> {
+fn extract_jwt(headers: &HeaderMap) -> Result<JsonWebToken, Rejection> {
     let header = headers.get(AUTHORIZATION).ok_or(CustomRejection::Forbidden)?;
     let auth_header = std::str::from_utf8(header.as_bytes()).map_err(|_| CustomRejection::Forbidden)?;
 
@@ -75,18 +88,31 @@ fn extract_jwt(headers: &HeaderMap) -> Result<String, Rejection> {
         return Err(reject::custom(CustomRejection::Forbidden));
     }
 
-    Ok(auth_header.trim_start_matches(BEARER).to_owned())
+    Ok(JsonWebToken::from(auth_header.trim_start_matches(BEARER).to_owned()))
 }
 
-fn validate_jwt<B: StorageBackend>(
-    headers: &HeaderMap,
+fn validate_api_jwt<B: StorageBackend>(
+    jwt: &JsonWebToken,
     args: &Arc<ApiArgs<B>>,
 ) -> Result<TokenData<Claims>, Rejection> {
-    let jwt = JsonWebToken::from(extract_jwt(headers)?);
     jwt.validate(
         args.node_id.to_string(),
         args.rest_api_config.jwt_salt.to_owned(),
         API_AUDIENCE_CLAIM.to_owned(),
+        false,
+        args.node_key_pair.secret().as_ref(),
+    )
+    .map_err(|_| reject::custom(CustomRejection::Forbidden))
+}
+
+fn validate_dashboard_jwt<B: StorageBackend>(
+    jwt: &JsonWebToken,
+    args: &Arc<ApiArgs<B>>,
+) -> Result<TokenData<Claims>, Rejection> {
+    jwt.validate(
+        args.node_id.to_string(),
+        args.rest_api_config.jwt_salt.to_owned(), // Dashboard SALT MISSING
+        DASHBOARD_AUDIENCE_CLAIM.to_owned(),
         true,
         args.node_key_pair.secret().as_ref(),
     )
