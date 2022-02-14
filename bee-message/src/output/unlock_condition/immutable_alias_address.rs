249fa598dc16c2af515eec8938fb824278c9458f
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2022-02-15 00:24:31 +0100

    Fix ImmutableAliasAddressUnlockCondition (#1132)
    * FIx ImmutableAliasAddressUnlockCondition
    
    * Update ImmutableAliasAddress dto
    
    * Undo networkId change
    
    * Add verify_alias_address

--- bee-message/src/output/unlock_condition/immutable_alias_address.rs
+++ bee-message/src/output/unlock_condition/immutable_alias_address.rs
@@ -1,14 +1,17 @@
 // Copyright 2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::address::AliasAddress;
+use crate::{
+    address::{Address, AliasAddress},
+    Error,
+};
 
 use derive_more::From;
 
 /// Defines the permanent [`AliasAddress`] that owns this output.
 #[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd, From, packable::Packable)]
 #[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
-pub struct ImmutableAliasAddressUnlockCondition(AliasAddress);
+pub struct ImmutableAliasAddressUnlockCondition(#[packable(verify_with = verify_alias_address)] Address);
 
 impl ImmutableAliasAddressUnlockCondition {
     /// The [`UnlockCondition`](crate::output::UnlockCondition) kind of an [`ImmutableAliasAddressUnlockCondition`].
@@ -17,12 +20,29 @@ impl ImmutableAliasAddressUnlockCondition {
     /// Creates a new [`ImmutableAliasAddressUnlockCondition`].
     #[inline(always)]
     pub fn new(address: AliasAddress) -> Self {
-        Self(address)
+        Self(Address::Alias(address))
     }
 
     /// Returns the address of a [`ImmutableAliasAddressUnlockCondition`].
     #[inline(always)]
     pub fn address(&self) -> &AliasAddress {
-        &self.0
+        // An ImmutableAliasAddressUnlockCondition must have an AliasAddress.
+        if let Address::Alias(alias_address) = &self.0 {
+            alias_address
+        } else {
+            unreachable!();
+        }
+    }
+}
+
+fn verify_alias_address<const VERIFY: bool>(address: &Address) -> Result<(), Error> {
+    if VERIFY {
+        if let Address::Alias(_) = address {
+            Ok(())
+        } else {
+            Err(Error::InvalidAddressKind(address.kind()))
+        }
+    } else {
+        Ok(())
     }
 }
