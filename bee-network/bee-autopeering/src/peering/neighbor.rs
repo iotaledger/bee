1fede8d0e0a812f1a8689c8bdfaccd7c9547bea0
Alexander Schmidt <alexander.schmidt@iota.org>
2022-02-24 15:39:16 +0100

    Use maximum to find furthest neighbor

--- bee-network/bee-autopeering/src/peering/neighbor.rs
+++ bee-network/bee-autopeering/src/peering/neighbor.rs
@@ -199,7 +199,7 @@ impl<const N: usize, const INBOUND: bool> NeighborhoodInner<N, INBOUND> {
         }
     }
 
-    fn is_preferred(&mut self, candidate: &Neighbor) -> bool {
+    fn is_preferred(&self, candidate: &Neighbor) -> bool {
         if let Some(furthest) = self.find_furthest_if_full() {
             candidate < furthest
         } else {
@@ -223,10 +223,9 @@ impl<const N: usize, const INBOUND: bool> NeighborhoodInner<N, INBOUND> {
         }
     }
 
-    fn find_furthest_if_full(&mut self) -> Option<&Neighbor> {
+    fn find_furthest_if_full(&self) -> Option<&Neighbor> {
         if self.neighbors.len() >= N {
-            self.neighbors.sort_unstable();
-            self.neighbors.last()
+            self.neighbors.iter().max()
         } else {
             None
         }
