ed98841b801bd3f2facbd6e559a354f98b55f41a
Adam-Gleave <adam.gleave@iota.org>
2022-03-01 13:59:12 +0000

    Make build deps optional

--- bee-node/build.rs
+++ bee-node/build.rs
@@ -1,19 +1,9 @@
 // Copyright 2020-2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use serde::Deserialize;
-use zip::ZipArchive;
+use std::{fmt, process::Command};
 
-use std::{
-    env, fmt,
-    io::{self, BufReader, Cursor},
-    path::Path,
-    process::Command,
-};
-
-const ASSET_PREFIX: &str = "node-dashboard-bee-";
-const RELEASES_URL: &str = "https://api.github.com/repos/iotaledger/node-dashboard/releases/latest";
-
+#[allow(dead_code)]
 #[derive(Debug)]
 enum BuildError {
     DashboardDecode,
@@ -40,19 +30,29 @@ impl fmt::Display for BuildError {
     }
 }
 
-#[derive(Deserialize, Debug)]
-struct LatestReleaseAssets {
-    assets: Vec<ReleaseAsset>,
+#[cfg(not(feature = "dashboard"))]
+fn main() -> Result<(), BuildError> {
+    parse_git_information()
 }
 
-#[derive(Deserialize, Debug)]
-struct ReleaseAsset {
-    name: String,
-    browser_download_url: String,
-}
-
+#[cfg(feature = "dashboard")]
 #[tokio::main]
 async fn main() -> Result<(), BuildError> {
+    parse_git_information()?;
+
+    let should_fetch = std::env::var("FETCH_DASHBOARD").map(|val| val == "1").unwrap_or(false);
+    let dashboard_dir = std::env::var("OUT_DIR").unwrap() + "/dashboard";
+
+    println!("cargo:rustc-env=DASHBOARD_DIR={}", dashboard_dir);
+
+    if should_fetch || !std::path::Path::new(&dashboard_dir).exists() {
+        dashboard::fetch(dashboard_dir).await?;
+    }
+
+    Ok(())
+}
+
+fn parse_git_information() -> Result<(), BuildError> {
     match Command::new("git").args(&["rev-parse", "HEAD"]).output() {
         Ok(output) => {
             println!(
@@ -77,69 +77,85 @@ async fn main() -> Result<(), BuildError> {
         Err(_) => return Err(BuildError::GitBranch),
     }
 
-    let should_fetch = env::var("FETCH_DASHBOARD").map(|val| val == "1").unwrap_or(false);
-    let dashboard_dir = env::var("OUT_DIR").unwrap() + "/dashboard";
-
-    println!("cargo:rustc-env=DASHBOARD_DIR={}", dashboard_dir);
-
-    if should_fetch || !Path::new(&dashboard_dir).exists() {
-        fetch_dashboard(dashboard_dir).await?;
-    }
-
     Ok(())
 }
 
-async fn fetch_dashboard<P: AsRef<Path>>(dashboard_dir: P) -> Result<(), BuildError> {
-    println!("fetching latest dashboard release");
+#[cfg(feature = "dashboard")]
+mod dashboard
+{
+    use super::*;
 
-    let client = reqwest::Client::builder()
-        .user_agent("bee-fetch-dashboard")
-        .build()
-        .expect("could not create client");
+    use serde::Deserialize;
+    use zip::ZipArchive;
 
-    let response = client
-        .get(RELEASES_URL)
-        .send()
-        .await
-        .and_then(|resp| resp.error_for_status())
-        .map_err(|e| BuildError::DashboardRequest(e.status().map(|code| code.as_u16()), RELEASES_URL.to_string()))?;
+    use std::{io::{self, BufReader, Cursor}, path::Path};
 
-    let assets = response
-        .json::<LatestReleaseAssets>()
-        .await
-        .map_err(|_| BuildError::DashboardDecode)?
-        .assets;
+    const ASSET_PREFIX: &str = "node-dashboard-bee-";
+    const RELEASES_URL: &str = "https://api.github.com/repos/iotaledger/node-dashboard/releases/latest";
 
-    let release_asset = assets
-        .iter()
-        .find(|asset| asset.name.starts_with(ASSET_PREFIX))
-        .ok_or(BuildError::DashboardNoSuitableRelease)?;
+    #[derive(Deserialize, Debug)]
+    struct LatestReleaseAssets {
+        assets: Vec<ReleaseAsset>,
+    }
 
-    println!(
-        "downloading latest dashboard release `{}` from {}",
-        release_asset.name, release_asset.browser_download_url
-    );
+    #[derive(Deserialize, Debug)]
+    struct ReleaseAsset {
+        name: String,
+        browser_download_url: String,
+    }
 
-    let mut archive = tempfile::tempfile().expect("could not create temp file");
+    pub(super) async fn fetch<P: AsRef<Path>>(dashboard_dir: P) -> Result<(), BuildError> {
+        println!("fetching latest dashboard release");
 
-    let response = client
-        .get(&release_asset.browser_download_url)
-        .send()
-        .await
-        .and_then(|resp| resp.error_for_status())
-        .map_err(|e| {
-            BuildError::DashboardRequest(
-                e.status().map(|code| code.as_u16()),
-                release_asset.browser_download_url.clone(),
-            )
-        })?;
+        let client = reqwest::Client::builder()
+            .user_agent("bee-fetch-dashboard")
+            .build()
+            .expect("could not create client");
 
-    let mut content = Cursor::new(response.bytes().await.map_err(|_| BuildError::DashboardDownload)?);
-    io::copy(&mut content, &mut archive).expect("copying failed");
-    let mut archive = ZipArchive::new(BufReader::new(archive)).map_err(|_| BuildError::DashboardInvalidArchive)?;
+        let response = client
+            .get(RELEASES_URL)
+            .send()
+            .await
+            .and_then(|resp| resp.error_for_status())
+            .map_err(|e| BuildError::DashboardRequest(e.status().map(|code| code.as_u16()), RELEASES_URL.to_string()))?;
 
-    println!("extracting release archive to {}", dashboard_dir.as_ref().display());
-    archive.extract(dashboard_dir).map_err(|_| BuildError::DashboardInvalidArchive)?;
+        let assets = response
+            .json::<LatestReleaseAssets>()
+            .await
+            .map_err(|_| BuildError::DashboardDecode)?
+            .assets;
 
-    Ok(())
+        let release_asset = assets
+            .iter()
+            .find(|asset| asset.name.starts_with(ASSET_PREFIX))
+            .ok_or(BuildError::DashboardNoSuitableRelease)?;
+
+        println!(
+            "downloading latest dashboard release `{}` from {}",
+            release_asset.name, release_asset.browser_download_url
+        );
+
+        let mut archive = tempfile::tempfile().expect("could not create temp file");
+
+        let response = client
+            .get(&release_asset.browser_download_url)
+            .send()
+            .await
+            .and_then(|resp| resp.error_for_status())
+            .map_err(|e| {
+                BuildError::DashboardRequest(
+                    e.status().map(|code| code.as_u16()),
+                    release_asset.browser_download_url.clone(),
+                )
+            })?;
+
+        let mut content = Cursor::new(response.bytes().await.map_err(|_| BuildError::DashboardDownload)?);
+        io::copy(&mut content, &mut archive).expect("copying failed");
+        let mut archive = ZipArchive::new(BufReader::new(archive)).map_err(|_| BuildError::DashboardInvalidArchive)?;
+
+        println!("extracting release archive to {}", dashboard_dir.as_ref().display());
+        archive.extract(dashboard_dir).map_err(|_| BuildError::DashboardInvalidArchive)?;
+
+        Ok(())
+    }
 }
