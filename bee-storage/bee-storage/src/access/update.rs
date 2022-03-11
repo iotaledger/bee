52b8814e163b2589d752a96bb48a7a932d68a0b0
Christian Poveda <christian.poveda@iota.org>
2022-03-11 11:43:23 -0500

    fix punctuation

--- bee-storage/bee-storage/src/access/update.rs
+++ bee-storage/bee-storage/src/access/update.rs
@@ -6,6 +6,6 @@ use crate::backend::StorageBackend;
 /// `Update<K, V>` trait extends the `StorageBackend` with `update` operation for the (key: K, value: V) pair;
 /// therefore, it should be explicitly implemented for the corresponding `StorageBackend`.
 pub trait Update<K, V>: StorageBackend {
-    /// Fetches the value for the key `K` and updates it using `f`
+    /// Fetches the value for the key `K` and updates it using `f`.
     fn update(&self, key: &K, f: impl FnMut(&mut V)) -> Result<(), Self::Error>;
 }
