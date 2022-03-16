e3ecaa281fbe98a5293f1d33e17d2f9cc12f2b28
rufsam <samuel.rufinatscha@iota.org>
2022-03-16 14:29:15 +0100

    Document base64 decoding

--- bee-api/bee-rest-api/src/endpoints/permission.rs
+++ bee-api/bee-rest-api/src/endpoints/permission.rs
@@ -44,26 +44,32 @@ pub(crate) fn check_permission<B: StorageBackend>(
         .and_then(move |path: FullPath, headers| {
             let args = args.clone();
             async move {
-                // Check if the requested endpoint is for public use.
+                // Check if the requested endpoint is open for public use.
                 if args.rest_api_config.public_routes.is_match(path.as_str()) {
                     return Ok(());
                 }
 
-                // Else the requested endpoint is protected / or not even exposed.
+                // Else the requested endpoint is protected by JWT / or not even exposed.
                 let jwt = extract_jwt(&headers)?;
-                // Decode the JWT payload to find out how to validate it.
+                // Decode the JWT payload to find out how to validate it. The `aud` claim will indicate if it's an API
+                // JWT or a Dashboard JWT.
                 let jwt_payload = {
                     let jwt_string = jwt.to_string();
                     // Every JWT consists of 3 parts: 1) header, 2) payload, 3) signature.
                     // The different parts are separated by `.`.
                     let split = jwt_string.split('.').collect::<Vec<&str>>();
-                    // If there are less then 3 parts the given JWT is invalid.
-                    if split.len() < 3 {
+                    // If there are less or more then 3 parts the given JWT is invalid.
+                    if split.len() != 3 {
                         return Err(reject::custom(CustomRejection::Forbidden));
                     }
-                    let encoded = split[1];
-                    let decoded = base64::decode(encoded).map_err(|_| reject::custom(CustomRejection::Forbidden))?;
-                    String::from_utf8(decoded).map_err(|_| CustomRejection::Forbidden)?
+
+                    // Get the base64 encoded payload.
+                    let encoded_payload = split[1];
+                    // Base64-decode the payload to bytes.
+                    let decoded_payload_bytes =
+                        base64::decode(encoded_payload).map_err(|_| reject::custom(CustomRejection::Forbidden))?;
+                    // Create a UTF-8 string from the decoded bytes.
+                    String::from_utf8(decoded_payload_bytes).map_err(|_| CustomRejection::Forbidden)?
                 };
 
                 if jwt_payload.contains(&format!("\"aud\":\"{}\"", API_AUDIENCE_CLAIM)) {
