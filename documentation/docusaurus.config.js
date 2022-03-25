7f9e298075283b2affce684f65a507247ee666a7
Dr-Electron <dr-electr0n@protonmail.com>
2022-03-26 00:33:16 +0100

    Prepare for new wiki build system

---
+++ documentation/docusaurus.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    title: 'Bee',
+    url: '/',
+    baseUrl: '/',
+    themes: ['@docusaurus/theme-classic'],
+    plugins: [
+        [
+            '@docusaurus/plugin-content-docs',
+            {
+                id: 'bee',
+                path: path.resolve(__dirname, './docs'),
+                routeBasePath: 'bee',
+                sidebarPath: path.resolve(__dirname, './sidebars.js'),
+                remarkPlugins: [require('remark-code-import'), require('remark-import-partial')],
+            }
+        ],
+    ],
+    staticDirectories: [path.resolve(__dirname, './static')],
+};
