521a02bc0751fbe7e74f8c069a698280ba770167
rufsam <samuel.rufinatscha@iota.org>
2022-03-28 12:24:42 +0200

    Simplify the Duration parsing from `white_flag_solidification_timeout`

--- bee-api/bee-rest-api/src/endpoints/config.rs
+++ bee-api/bee-rest-api/src/endpoints/config.rs
@@ -156,10 +156,9 @@ impl RestApiConfigBuilder {
 
         let feature_proof_of_work = self.feature_proof_of_work.unwrap_or(DEFAULT_FEATURE_PROOF_OF_WORK);
 
-        let white_flag_solidification_timeout = match self.white_flag_solidification_timeout {
-            Some(timeout) => Duration::from_secs(timeout),
-            None => DEFAULT_WHITE_FLAG_SOLIDIFICATION_TIMEOUT,
-        };
+        let white_flag_solidification_timeout = self
+            .white_flag_solidification_timeout
+            .map_or_else(|| DEFAULT_WHITE_FLAG_SOLIDIFICATION_TIMEOUT, Duration::from_secs);
 
         RestApiConfig {
             bind_socket_addr: SocketAddr::new(address, port),
