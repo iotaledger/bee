5e4daf4f947bd4de94fbef2584341e7c88054205
Alexander Schmidt <alexander.schmidt@iota.org>
2022-03-28 12:26:43 +0200

    Replace unreachable with panic and message

--- bee-ledger/src/workers/consensus/worker.rs
+++ bee-ledger/src/workers/consensus/worker.rs
@@ -352,7 +352,8 @@ where
                                         .await
                                 {
                                     error!("Pruning failed: {:?}.", e);
-                                    unreachable!()
+                                    // TODO: consider initiating an emergency (but graceful) shutdown instead of just panicking.
+                                    panic!("Aborting due to an unexpected pruning error.");
                                 }
                             }
                             Err(reason) => {
