6e8ca9bfedffaa88ff5ee5b1409c89955338f9ef
rufsam <samuel.rufinatscha@iota.org>
2022-04-29 15:52:27 +0200

    Revisit info endpoint logic for future snapshot update

--- bee-api/bee-rest-api/src/endpoints/routes/api/v2/info.rs
+++ bee-api/bee-rest-api/src/endpoints/routes/api/v2/info.rs
@@ -99,6 +99,7 @@ pub(crate) fn info<B: StorageBackend>(
         version: node_info.version.clone(),
         status: StatusResponse {
             is_healthy: health::is_healthy(&tangle, &peer_manager),
+            // TODO: In future, the snapshot might make all data for the `latest_milestone` available.
             latest_milestone: LatestMilestoneResponse {
                 index: *latest_milestone_index,
                 timestamp: latest_milestone_metadata
@@ -109,6 +110,7 @@ pub(crate) fn info<B: StorageBackend>(
                     .map(|m| m.milestone_id().to_string())
                     .unwrap_or_default(),
             },
+            // TODO: In future, the snapshot might make all data for the `confirmed_milestone` available.
             confirmed_milestone: ConfirmedMilestoneResponse {
                 index: *confirmed_milestone_index,
                 timestamp: confirmed_milestone_metadata
