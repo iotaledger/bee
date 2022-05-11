557cc869548bccb59c84726528394e2ed77b587f
Dr-Electron <dr-electr0n@protonmail.com>
2022-05-11 14:26:08 +0000

    Add test action and fix broken links

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -5,6 +5,15 @@ module.exports = {
     url: '/',
     baseUrl: '/',
     themes: ['@docusaurus/theme-classic'],
+    themeConfig: {
+        navbar: {
+            // Workaround to disable broken logo href on test build
+            logo: {
+                src: 'img/logo/bee_logo.png',
+                href: 'https://wiki.iota.org/',
+            },
+        },
+    },
     plugins: [
         [
             '@docusaurus/plugin-content-docs',
