d755e50ea2bf6162b0e6c4d690b4a8fe6ba8f047
Thibault Martinez <thibault@iota.org>
2022-06-22 14:05:57 +0200

    Move REST API white flag endpoint to core group (#1445)

--- bee-api/bee-rest-api/src/endpoints/routes/api/mod.rs
+++ bee-api/bee-rest-api/src/endpoints/routes/api/mod.rs
@@ -2,12 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 pub mod core;
-pub mod plugins;
 
 use axum::Router;
 
 use crate::endpoints::storage::StorageBackend;
 
 pub(crate) fn filter<B: StorageBackend>() -> Router {
-    Router::new().nest("/api", plugins::filter::<B>().merge(core::filter::<B>()))
+    Router::new().nest("/api", core::filter::<B>())
 }
