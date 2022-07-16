99f4b35a94433a40e0bdb0d5db985b30a78db961
Thibault Martinez <thibault@iota.org>
2022-07-16 11:56:09 +0200

    Add bee-api-types crate

---
+++ bee-api/bee-api-types/src/dtos.rs
@@ -0,0 +1,153 @@
+// Copyright 2020-2022 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use bee_block::payload::milestone::option::dto::ReceiptMilestoneOptionDto;
+use bee_ledger::types::Receipt;
+#[cfg(feature = "peer")]
+use bee_protocol::types::peer::Peer;
+use serde::{Deserialize, Serialize};
+
+/// Describes a peer.
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub struct PeerDto {
+    pub id: String,
+    #[serde(rename = "multiAddresses")]
+    pub multi_addresses: Vec<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub alias: Option<String>,
+    pub relation: RelationDto,
+    pub connected: bool,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub gossip: Option<GossipDto>,
+}
+
+#[cfg(feature = "peer")]
+impl From<&Peer> for PeerDto {
+    fn from(peer: &Peer) -> Self {
+        PeerDto {
+            id: peer.id().to_string(),
+            alias: Some(peer.alias().to_string()),
+            multi_addresses: vec![peer.address().to_string()],
+            relation: {
+                if peer.relation().is_known() {
+                    RelationDto::Known
+                } else if peer.relation().is_unknown() {
+                    RelationDto::Unknown
+                } else {
+                    RelationDto::Autopeered
+                }
+            },
+            connected: peer.is_connected(),
+            gossip: Some(GossipDto {
+                heartbeat: HeartbeatDto {
+                    solid_milestone_index: *peer.solid_milestone_index(),
+                    pruned_milestone_index: *peer.pruned_index(),
+                    latest_milestone_index: *peer.latest_milestone_index(),
+                    connected_peers: peer.connected_peers(),
+                    synced_peers: peer.synced_peers(),
+                },
+                metrics: MetricsDto {
+                    new_blocks: peer.metrics().new_blocks(),
+                    received_blocks: peer.metrics().blocks_received(),
+                    known_blocks: peer.metrics().known_blocks(),
+                    received_block_requests: peer.metrics().block_requests_received(),
+                    received_milestone_requests: peer.metrics().milestone_requests_received(),
+                    received_heartbeats: peer.metrics().heartbeats_received(),
+                    sent_blocks: peer.metrics().blocks_sent(),
+                    sent_block_requests: peer.metrics().block_requests_sent(),
+                    sent_milestone_requests: peer.metrics().milestone_requests_sent(),
+                    sent_heartbeats: peer.metrics().heartbeats_sent(),
+                    dropped_packets: 0,
+                },
+            }),
+        }
+    }
+}
+
+/// Returns all information about the gossip stream with the peer.
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize, Default)]
+pub struct GossipDto {
+    pub heartbeat: HeartbeatDto,
+    pub metrics: MetricsDto,
+}
+
+/// Describes the relation with the peer.
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub enum RelationDto {
+    #[serde(rename = "known")]
+    Known,
+    #[serde(rename = "unknown")]
+    Unknown,
+    #[serde(rename = "autopeered")]
+    Autopeered,
+}
+
+/// Describes the heartbeat of a node.
+#[derive(Clone, Debug, Default, Eq, PartialEq, Serialize, Deserialize)]
+pub struct HeartbeatDto {
+    #[serde(rename = "solidMilestoneIndex")]
+    pub solid_milestone_index: u32,
+    #[serde(rename = "prunedMilestoneIndex")]
+    pub pruned_milestone_index: u32,
+    #[serde(rename = "latestMilestoneIndex")]
+    pub latest_milestone_index: u32,
+    #[serde(rename = "connectedPeers")]
+    pub connected_peers: u8,
+    #[serde(rename = "syncedPeers")]
+    pub synced_peers: u8,
+}
+
+/// Describes metrics of a gossip stream.
+#[derive(Clone, Debug, Default, Eq, PartialEq, Serialize, Deserialize)]
+pub struct MetricsDto {
+    #[serde(rename = "newBlocks")]
+    pub new_blocks: u64,
+    #[serde(rename = "receivedBlocks")]
+    pub received_blocks: u64,
+    #[serde(rename = "knownBlocks")]
+    pub known_blocks: u64,
+    #[serde(rename = "receivedBlockRequests")]
+    pub received_block_requests: u64,
+    #[serde(rename = "receivedMilestoneRequests")]
+    pub received_milestone_requests: u64,
+    #[serde(rename = "receivedHeartbeats")]
+    pub received_heartbeats: u64,
+    #[serde(rename = "sentBlocks")]
+    pub sent_blocks: u64,
+    #[serde(rename = "sentBlockRequests")]
+    pub sent_block_requests: u64,
+    #[serde(rename = "sentMilestoneRequests")]
+    pub sent_milestone_requests: u64,
+    #[serde(rename = "sentHeartbeats")]
+    pub sent_heartbeats: u64,
+    #[serde(rename = "droppedPackets")]
+    pub dropped_packets: u64,
+}
+
+/// Describes a receipt.
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub struct ReceiptDto {
+    pub receipt: ReceiptMilestoneOptionDto,
+    #[serde(rename = "milestoneIndex")]
+    pub milestone_index: u32,
+}
+
+impl From<Receipt> for ReceiptDto {
+    fn from(value: Receipt) -> Self {
+        ReceiptDto {
+            receipt: value.inner().into(),
+            milestone_index: **value.included_in(),
+        }
+    }
+}
+
+/// Describes the ledger inclusion state of a transaction.
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub enum LedgerInclusionStateDto {
+    #[serde(rename = "conflicting")]
+    Conflicting,
+    #[serde(rename = "included")]
+    Included,
+    #[serde(rename = "noTransaction")]
+    NoTransaction,
+}
---
+++ bee-api/bee-api-types/src/error.rs
@@ -0,0 +1,13 @@
+// Copyright 2020-2022 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use bee_block::Error as BlockError;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("invalid field \"{0}\"")]
+    InvalidField(&'static str),
+    #[error("{0}")]
+    Block(#[from] BlockError),
+}
