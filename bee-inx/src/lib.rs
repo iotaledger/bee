3f32e67112cff144becb2f67c01760758ff781bc
Jochen Görtler <jochen.goertler@iota.org>
2022-07-25 13:46:57 +0200

    Add testcase for macro

--- bee-inx/src/lib.rs
+++ bee-inx/src/lib.rs
@@ -26,3 +26,18 @@ macro_rules! field {
         $object.$field.ok_or(Self::Error::MissingField(stringify!($field)))?
     };
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn macro_missing_field() {
+        let proto = proto::TreasuryOutput{
+            milestone_id: None,
+            amount: 42,
+        };
+        let err = TreasuryOutput::try_from(proto).unwrap_err();
+        assert!(matches!(err, bee_block::InxError::MissingField("milestone_id")));
+    }
+}
